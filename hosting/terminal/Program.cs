bc16bde1a9671921c9e2eb53a3ec8ef06b16ca50
Popeye Zhong <9555843@qq.com>
2020-02-04 23:35:58 +0800

    初始化终端宿主程序项目。 :closed_lock_with_key:

---
+++ hosting/terminal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Zongsoft.Hosting.Terminal
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Zongsoft.Plugins.Application.Start(Zongsoft.Terminals.Plugins.ApplicationContext.Current, args);
+		}
+	}
+}
