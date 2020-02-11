feac987c09dd21524662829d6233a0c339ec2e62
Popeye Zhong <9555843@qq.com>
2020-02-11 20:03:24 +0800

    调整代码格式。 :frog:

--- Zongsoft.Core/src/Common/SequenceBase.cs
+++ Zongsoft.Core/src/Common/SequenceBase.cs
@@ -33,252 +33,253 @@ using System.Collections.Generic;
 
 namespace Zongsoft.Common
 {
-    public abstract class SequenceBase : ISequence, IDisposable
-    {
-        #region 内部结构
-        [System.Runtime.InteropServices.StructLayout(System.Runtime.InteropServices.LayoutKind.Sequential)]
-        private struct Entry
-        {
-            public long Value;      //当前序号值
-            public long Threshold;  //本地递增的上限值
-            public long Timestamp;  //最后更新时间，频率为秒
-            public int Count;       //成长因子，最小值为10
-            public int Flags;       //同步锁标记
-        }
-        #endregion
+	public abstract class SequenceBase : ISequence, IDisposable
+	{
+		#region 内部结构
+		[System.Runtime.InteropServices.StructLayout(System.Runtime.InteropServices.LayoutKind.Sequential)]
+		private struct Entry
+		{
+			public long Value;      //当前序号值
+			public long Threshold;  //本地递增的上限值
+			public long Timestamp;  //最后更新时间，频率为秒
+			public int Count;       //成长因子，最小值为10
+			public int Flags;       //同步锁标记
+		}
+		#endregion
 
-        #region 常量定义
-        private const int MINIMUM_FACTOR = 10;
-        private const int MAXIMUM_FACTOR = 10000;
+		#region 常量定义
+		private const int MINIMUM_FACTOR = 10;
+		private const int MAXIMUM_FACTOR = 10000;
 
-        private const int LOCKED_FLAG = 1;
-        private const int UNLOCK_FLAG = 0;
-        #endregion
+		private const int LOCKED_FLAG = 1;
+		private const int UNLOCK_FLAG = 0;
+		#endregion
 
-        #region 成员字段
-        private ReaderWriterLockSlim _locker;
-        private Dictionary<string, int> _map;
-        private Entry[] _entries;
-        #endregion
+		#region 成员字段
+		private ReaderWriterLockSlim _locker;
+		private Dictionary<string, int> _map;
+		private Entry[] _entries;
+		#endregion
 
-        #region 构造函数
-        protected SequenceBase() : this(64)
-        {
-        }
+		#region 构造函数
+		protected SequenceBase() : this(64)
+		{
+		}
 
-        protected SequenceBase(int capacity)
-        {
-            if(capacity < 8)
-                throw new ArgumentOutOfRangeException(nameof(capacity));
+		protected SequenceBase(int capacity)
+		{
+			if(capacity < 8)
+				throw new ArgumentOutOfRangeException(nameof(capacity));
 
-            _map = new Dictionary<string, int>(capacity);
-            _entries = new Entry[capacity];
-            _locker = new ReaderWriterLockSlim();
-        }
-        #endregion
+			_map = new Dictionary<string, int>(capacity);
+			_entries = new Entry[capacity];
+			_locker = new ReaderWriterLockSlim();
+		}
+		#endregion
 
-        #region 公共方法
-        public bool TryGetValue(string key, out long value)
-        {
-            if(string.IsNullOrEmpty(key))
-                throw new ArgumentNullException(nameof(key));
+		#region 公共方法
+		public bool TryGetValue(string key, out long value)
+		{
+			if(string.IsNullOrEmpty(key))
+				throw new ArgumentNullException(nameof(key));
 
-            if(_map.TryGetValue(key, out var index))
-            {
-                value = _entries[index].Value;
-                return true;
-            }
+			if(_map.TryGetValue(key, out var index))
+			{
+				value = _entries[index].Value;
+				return true;
+			}
 
-            value = 0;
-            return false;
-        }
+			value = 0;
+			return false;
+		}
 
-        public virtual long Decrement(string key, int interval = 1, int seed = 0)
-        {
-            return this.Increment(key, -interval, seed);
-        }
+		public virtual long Decrement(string key, int interval = 1, int seed = 0)
+		{
+			return this.Increment(key, -interval, seed);
+		}
 
-        public virtual long Increment(string key, int interval = 1, int seed = 0)
-        {
-            if(string.IsNullOrEmpty(key))
-                throw new ArgumentNullException(nameof(key));
+		public virtual long Increment(string key, int interval = 1, int seed = 0)
+		{
+			if(string.IsNullOrEmpty(key))
+				throw new ArgumentNullException(nameof(key));
 
-            if(_map.TryGetValue(key, out var index))
-                _locker.EnterReadLock();
-            else
-                _locker.EnterUpgradeableReadLock();
+			if(_map.TryGetValue(key, out var index))
+				_locker.EnterReadLock();
+			else
+				_locker.EnterUpgradeableReadLock();
 
-            try
-            {
-                if(_locker.IsUpgradeableReadLockHeld && !_map.TryGetValue(key, out index))
-                {
-                    try
-                    {
-                        _locker.EnterWriteLock();
+			try
+			{
+				if(_locker.IsUpgradeableReadLockHeld && !_map.TryGetValue(key, out index))
+				{
+					try
+					{
+						_locker.EnterWriteLock();
 
-                        index = _map.Count;
-                        _map.Add(key, index);
+						index = _map.Count;
+						_map.Add(key, index);
 
-                        if(index == _entries.Length)
-                            Expand();
-                    }
-                    finally
-                    {
-                        _locker.ExitWriteLock();
-                    }
-                }
+						if(index == _entries.Length)
+							Expand();
+					}
+					finally
+					{
+						_locker.ExitWriteLock();
+					}
+				}
 
-                unsafe
-                {
-                    fixed(Entry* entry = &_entries[index])
-                    {
-                        var value = Interlocked.Increment(ref entry->Value);
+				unsafe
+				{
+					fixed(Entry* entry = &_entries[index])
+					{
+						var value = Interlocked.Increment(ref entry->Value);
 
-                        if(value >= entry->Threshold)
-                        {
-                            var hold = Interlocked.CompareExchange(ref entry->Flags, LOCKED_FLAG, UNLOCK_FLAG);
+						if(value >= entry->Threshold)
+						{
+							var hold = Interlocked.CompareExchange(ref entry->Flags, LOCKED_FLAG, UNLOCK_FLAG);
 
-                            if(hold == UNLOCK_FLAG)
-                            {
-                                try
-                                {
-                                    entry->Threshold = this.Reserve(key, ref *entry, value, seed);
-                                    entry->Timestamp = GetTimestamp();
-                                }
-                                finally
-                                {
-                                    Interlocked.Exchange(ref entry->Flags, UNLOCK_FLAG);
-                                }
-                            }
-                        }
+							if(hold == UNLOCK_FLAG)
+							{
+								try
+								{
+									entry->Threshold = this.Reserve(key, ref *entry, value, seed);
+									entry->Timestamp = GetTimestamp();
+								}
+								finally
+								{
+									Interlocked.Exchange(ref entry->Flags, UNLOCK_FLAG);
+								}
+							}
+						}
 
-                        return value;
-                    }
-                }
-            }
-            finally
-            {
-                if(_locker.IsReadLockHeld)
-                    _locker.ExitReadLock();
-                else
-                    _locker.ExitUpgradeableReadLock();
-            }
-        }
+						return value;
+					}
+				}
+			}
+			finally
+			{
+				if(_locker.IsReadLockHeld)
+					_locker.ExitReadLock();
+				else
+					_locker.ExitUpgradeableReadLock();
+			}
+		}
 
-        public virtual void Reset(string key, int value = 0)
-        {
-            if(string.IsNullOrEmpty(key))
-                throw new ArgumentNullException(nameof(key));
+		public virtual void Reset(string key, int value = 0)
+		{
+			if(string.IsNullOrEmpty(key))
+				throw new ArgumentNullException(nameof(key));
 
-            _locker.EnterWriteLock();
+			_locker.EnterWriteLock();
 
-            try
-            {
-                if(_map.TryGetValue(key, out var index))
-                {
-                    unsafe
-                    {
-                        fixed(Entry* entry = &_entries[index])
-                        {
-                            var hold = Interlocked.CompareExchange(ref entry->Flags, LOCKED_FLAG, UNLOCK_FLAG);
+			try
+			{
+				if(_map.TryGetValue(key, out var index))
+				{
+					unsafe
+					{
+						fixed(Entry* entry = &_entries[index])
+						{
+							var hold = Interlocked.CompareExchange(ref entry->Flags, LOCKED_FLAG, UNLOCK_FLAG);
 
-                            if(hold == 0)
-                            {
-                                try
-                                {
-                                    entry->Value = value;
-                                    entry->Count = MINIMUM_FACTOR;
-                                    entry->Threshold = value + MINIMUM_FACTOR;
-                                    entry->Timestamp = GetTimestamp();
+							if(hold == 0)
+							{
+								try
+								{
+									entry->Value = value;
+									entry->Count = MINIMUM_FACTOR;
+									entry->Threshold = value + MINIMUM_FACTOR;
+									entry->Timestamp = GetTimestamp();
 
-                                    this.OnReset(key, value);
-                                }
-                                finally
-                                {
-                                    Interlocked.Exchange(ref entry->Flags, UNLOCK_FLAG);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            finally
-            {
-                _locker.ExitWriteLock();
-            }
-        }
-        #endregion
+									this.OnReset(key, value);
+								}
+								finally
+								{
+									Interlocked.Exchange(ref entry->Flags, UNLOCK_FLAG);
+								}
+							}
+						}
+					}
+				}
+			}
+			finally
+			{
+				_locker.ExitWriteLock();
+			}
+		}
+		#endregion
 
-        #region 抽象方法
-        protected abstract void OnReset(string key, int value);
-        protected abstract long OnReserve(string key, int count, int seed);
-        #endregion
+		#region 抽象方法
+		protected abstract void OnReset(string key, int value);
+		protected abstract long OnReserve(string key, int count, int seed);
+		#endregion
 
-        #region 私有方法
-        [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
-        private void Expand()
-        {
-            int capacity = _entries.Length == 0 ? 16 : _entries.Length * 2;
+		#region 私有方法
+		[System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
+		private void Expand()
+		{
+			int capacity = _entries.Length == 0 ? 16 : _entries.Length * 2;
 
-            if((uint)capacity > 0X7FEFFFFF)
-                capacity = 0X7FEFFFFF;
+			if((uint)capacity > 0X7FEFFFFF)
+				capacity = 0X7FEFFFFF;
 
-            if(capacity < 16)
-                capacity = 16;
+			if(capacity < 16)
+				capacity = 16;
 
-            var entries = new Entry[capacity];
+			var entries = new Entry[capacity];
 
-            if(_entries.Length > 0)
-                Array.Copy(_entries, entries, _entries.Length);
+			if(_entries.Length > 0)
+				Array.Copy(_entries, entries, _entries.Length);
 
-            _entries = entries;
-        }
+			_entries = entries;
+		}
 
-        [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
-        private long Reserve(string key, ref Entry entry, long value, int seed)
-        {
-            int Bound(int min, int max, double number)
-            {
-                return (int)Math.Min(max, Math.Max(min, number));
-            }
+		[System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
+		private long Reserve(string key, ref Entry entry, long value, int seed)
+		{
+			int Bound(int min, int max, double number)
+			{
+				return (int)Math.Min(max, Math.Max(min, number));
+			}
 
-            var duration = Math.Max(0, GetTimestamp() - entry.Timestamp);
-            var count = entry.Count = Bound(
-                MINIMUM_FACTOR,
-                MAXIMUM_FACTOR,
-                entry.Count * Math.Max(0.1, 2 - (duration / 300))
-            );
+			var duration = Math.Max(0, GetTimestamp() - entry.Timestamp);
+			var count = entry.Count = Bound(
+				MINIMUM_FACTOR,
+				MAXIMUM_FACTOR,
+				entry.Count * Math.Max(0.1, 2 - (duration / 300))
+			);
 
-            if(value < entry.Value && count < MAXIMUM_FACTOR)
-                count += (int)(entry.Value - value);
+			if(value < entry.Value && count < MAXIMUM_FACTOR)
+				count += (int)(entry.Value - value);
 
-            return this.OnReserve(key, count, seed);
-        }
+			return this.OnReserve(key, count, seed);
+		}
 
-        [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
-        private long GetTimestamp()
-        {
-            return System.Diagnostics.Stopwatch.GetTimestamp() / System.Diagnostics.Stopwatch.Frequency;
-        }
-        #endregion
+		[System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
+		private long GetTimestamp()
+		{
+			return System.Environment.TickCount64 / 1000;
+			//return System.Diagnostics.Stopwatch.GetTimestamp() / System.Diagnostics.Stopwatch.Frequency;
+		}
+		#endregion
 
-        #region 处置方法
-        protected virtual void Dispose(bool disposing)
-        {
-            if(disposing)
-            {
-                _map.Clear();
-                Array.Resize(ref _entries, 0);
-            }
+		#region 处置方法
+		protected virtual void Dispose(bool disposing)
+		{
+			if(disposing)
+			{
+				_map.Clear();
+				Array.Resize(ref _entries, 0);
+			}
 
-            _locker.Dispose();
-        }
+			_locker.Dispose();
+		}
 
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-        #endregion
-    }
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+		#endregion
+	}
 }
