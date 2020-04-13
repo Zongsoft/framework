7c45ccfbbd7f680d0720b1d235db417fbf7cad85
Popeye Zhong <9555843@qq.com>
2020-04-13 15:48:40 +0800

    完成数据实体(模型)的配置解析。 :mouse:

---
+++ Zongsoft.Core/src/Configuration/Models/ModelConfigurationProvider.cs
@@ -0,0 +1,145 @@
+﻿/*
+ *   _____                                ______
+ *  /_   /  ____  ____  ____  _________  / __/ /_
+ *    / /  / __ \/ __ \/ __ \/ ___/ __ \/ /_/ __/
+ *   / /__/ /_/ / / / / /_/ /\_ \/ /_/ / __/ /_
+ *  /____/\____/_/ /_/\__  /____/\____/_/  \__/
+ *                   /____/
+ *
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@gmail.com>
+ *
+ * Copyright (C) 2010-2020 Zongsoft Studio <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.Core library.
+ *
+ * The Zongsoft.Core is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3.0 of the License,
+ * or (at your option) any later version.
+ *
+ * The Zongsoft.Core is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Zongsoft.Core library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Linq;
+using System.Threading;
+using System.Collections.Generic;
+
+using Microsoft.Extensions.Primitives;
+using Microsoft.Extensions.Configuration;
+
+using Zongsoft.Data;
+
+namespace Zongsoft.Configuration.Models
+{
+	public class ModelConfigurationProvider<TModel> : IConfigurationProvider where TModel : IModel
+	{
+		#region 成员字段
+		private readonly ModelConfigurationSource<TModel> _source;
+		private ConfigurationReloadToken _reloadToken;
+		#endregion
+
+		#region 构造函数
+		public ModelConfigurationProvider(ModelConfigurationSource<TModel> source)
+		{
+			_source = source ?? throw new ArgumentNullException(nameof(source));
+			_reloadToken = new ConfigurationReloadToken();
+
+			this.Data = new Dictionary<string, TModel>(StringComparer.OrdinalIgnoreCase);
+
+			if(source.Models != null)
+			{
+				foreach(var model in source.Models)
+				{
+					if(model.TryGetValue(source.Mapping.Key, out var key) && key != null)
+						this.Data.Add(key.ToString(), model);
+				}
+			}
+		}
+		#endregion
+
+		#region 保护属性
+		protected IDictionary<string, TModel> Data
+		{
+			get; set;
+		}
+		#endregion
+
+		#region 公共方法
+		public virtual bool TryGet(string key, out string value)
+		{
+			if(this.Data.TryGetValue(key, out var model) && model.TryGetValue(_source.Mapping.Value, out var obj) && obj != null)
+			{
+				value = obj.ToString();
+				return true;
+			}
+
+			value = null;
+			return false;
+		}
+
+		public virtual void Set(string key, string value)
+		{
+			if(this.Data.TryGetValue(key, out var model))
+				model.TrySetValue(_source.Mapping.Value, value);
+			else
+				this.Data[key] = model = Model.Build<TModel>(m =>
+				{
+					m.TrySetValue(_source.Mapping.Key, key);
+					m.TrySetValue(_source.Mapping.Value, value);
+				});
+
+			var persistents = _source.Persistents;
+
+			if(persistents != null && persistents.Count > 0)
+			{
+				foreach(var persistent in persistents)
+					persistent(model);
+			}
+		}
+
+		public virtual void Load()
+		{
+		}
+
+		public virtual IEnumerable<string> GetChildKeys(IEnumerable<string> earlierKeys, string parentPath)
+		{
+			var prefix = parentPath == null ? string.Empty : parentPath + ConfigurationPath.KeyDelimiter;
+
+			return this.Data
+				.Where(kv => kv.Key.StartsWith(prefix, StringComparison.OrdinalIgnoreCase))
+				.Select(kv => Segment(kv.Key, prefix.Length))
+				.Concat(earlierKeys)
+				.OrderBy(k => k, ConfigurationKeyComparer.Instance);
+		}
+
+		public IChangeToken GetReloadToken()
+		{
+			return _reloadToken;
+		}
+		#endregion
+
+		#region 保护方法
+		protected void OnReload()
+		{
+			var previousToken = Interlocked.Exchange(ref _reloadToken, new ConfigurationReloadToken());
+			previousToken.OnReload();
+		}
+		#endregion
+
+		#region 私有方法
+		private static string Segment(string key, int prefixLength)
+		{
+			var indexOf = key.IndexOf(ConfigurationPath.KeyDelimiter, prefixLength);
+			return indexOf < 0 ? key.Substring(prefixLength) : key.Substring(prefixLength, indexOf - prefixLength);
+		}
+		#endregion
+	}
+}
---
+++ Zongsoft.Core/src/Configuration/Models/ModelConfigurationSource.cs
@@ -0,0 +1,118 @@
+﻿/*
+ *   _____                                ______
+ *  /_   /  ____  ____  ____  _________  / __/ /_
+ *    / /  / __ \/ __ \/ __ \/ ___/ __ \/ /_/ __/
+ *   / /__/ /_/ / / / / /_/ /\_ \/ /_/ / __/ /_
+ *  /____/\____/_/ /_/\__  /____/\____/_/  \__/
+ *                   /____/
+ *
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@gmail.com>
+ *
+ * Copyright (C) 2010-2020 Zongsoft Studio <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.Core library.
+ *
+ * The Zongsoft.Core is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3.0 of the License,
+ * or (at your option) any later version.
+ *
+ * The Zongsoft.Core is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Zongsoft.Core library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Collections.Generic;
+
+using Microsoft.Extensions.Configuration;
+
+namespace Zongsoft.Configuration.Models
+{
+	public class ModelConfigurationSource<TModel> : IConfigurationSource where TModel : Data.IModel
+	{
+		#region 成员字段
+		private readonly HashSet<Action<TModel>> _persistents;
+		#endregion
+
+		#region 构造函数
+		public ModelConfigurationSource()
+		{
+			this.Mapping = new ModelConfigurationMapping();
+			_persistents = new HashSet<Action<TModel>>();
+		}
+		#endregion
+
+		#region 公共属性
+		public IEnumerable<TModel> Models { get; set; }
+
+		public ModelConfigurationMapping Mapping { get; }
+		#endregion
+
+		#region 内部属性
+		internal ICollection<Action<TModel>> Persistents => _persistents;
+		#endregion
+
+		#region 公共方法
+		public IConfigurationProvider Build(IConfigurationBuilder builder)
+		{
+			return new ModelConfigurationProvider<TModel>(this);
+		}
+
+		public ModelConfigurationSource<TModel> Map(string key, string value = null)
+		{
+			if(!string.IsNullOrWhiteSpace(key))
+				this.Mapping.Key = key.Trim();
+
+			if(!string.IsNullOrWhiteSpace(value))
+				this.Mapping.Value = value.Trim();
+
+			return this;
+		}
+
+		public ModelConfigurationSource<TModel> OnSet(Action<TModel> persistent)
+		{
+			if(persistent == null)
+				throw new ArgumentNullException(nameof(persistent));
+
+			_persistents.Add(persistent);
+			return this;
+		}
+		#endregion
+	}
+
+	public class ModelConfigurationMapping
+	{
+		public ModelConfigurationMapping()
+		{
+			this.Key = nameof(Key);
+			this.Value = nameof(Value);
+		}
+
+		public ModelConfigurationMapping(string key, string value)
+		{
+			if(string.IsNullOrEmpty(key))
+				throw new ArgumentNullException(nameof(key));
+			if(string.IsNullOrEmpty(value))
+				throw new ArgumentNullException(nameof(value));
+
+			this.Key = key;
+			this.Value = value;
+		}
+
+		public string Key
+		{
+			get; internal set;
+		}
+
+		public string Value
+		{
+			get; internal set;
+		}
+	}
+}
