16d9faf95d768dc115da93c4d9836b16b43dd14f
Popeye Zhong <9555843@qq.com>
2020-04-30 18:32:41 +0800

    增强服务容器的单元测试。 :tv:

--- Zongsoft.Core/test/Services/ServiceProviderTest.cs
+++ Zongsoft.Core/test/Services/ServiceProviderTest.cs
@@ -63,17 +63,42 @@ namespace Zongsoft.Services.Tests
 
 			var provider = new ServiceProvider(services);
 
-			Assert.NotNull(provider.GetService<IFoo>());
-			Assert.NotNull(provider.GetService<Foo>());
+			var foo1 = provider.GetService<IFoo>();
+			var foo2 = provider.GetService<Foo>();
+
+			Assert.NotNull(foo1);
+			Assert.NotNull(foo2);
+			Assert.NotSame(foo1, foo2);
+			Assert.Same(foo1, provider.GetService<IFoo>());
+			Assert.NotSame(foo1, provider.GetService<Foo>());
+			Assert.NotSame(foo2, provider.GetService<IFoo>());
+			Assert.Same(foo2, provider.GetService<Foo>());
 			Assert.NotSame(provider.GetService<IFoo>(), provider.GetService<Foo>());
 
 			provider.Descriptors
 				.AddSingleton<Bar>()
 				.AddSingleton<IBar>(srv => srv.GetRequiredService<Bar>());
 
-			Assert.NotNull(provider.GetService<IBar>());
-			Assert.NotNull(provider.GetService<Bar>());
+			var bar1 = provider.GetService<IBar>();
+			var bar2 = provider.GetService<Bar>();
+
+			Assert.NotNull(bar1);
+			Assert.NotNull(bar2);
+			Assert.Same(bar1, bar2);
+			Assert.Same(bar1, provider.GetService<IBar>());
+			Assert.Same(bar1, provider.GetService<Bar>());
+			Assert.Same(bar2, provider.GetService<IBar>());
+			Assert.Same(bar2, provider.GetService<Bar>());
 			Assert.Same(provider.GetService<IBar>(), provider.GetService<Bar>());
+
+			var foo1s = provider.GetService<IFoo>();
+			var foo2s = provider.GetService<Foo>();
+
+			Assert.NotNull(foo1s);
+			Assert.NotNull(foo2s);
+			Assert.NotSame(foo1s, foo2s);
+			Assert.Same(foo1, foo1s);
+			Assert.Same(foo2, foo2s);
 		}
 
 		[Fact]
