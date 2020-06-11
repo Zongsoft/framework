55679d5416a76c2655f42bce164d81989165c0ae
Popeye Zhong <9555843@qq.com>
2020-06-11 16:53:11 +0800

    增加ReadAsStringAsync扩展方法。 :construction:

---
+++ Zongsoft.Web/src/Http/HttpRequestExtension.cs
@@ -0,0 +1,64 @@
+﻿/*
+ *   _____                                ______
+ *  /_   /  ____  ____  ____  _________  / __/ /_
+ *    / /  / __ \/ __ \/ __ \/ ___/ __ \/ /_/ __/
+ *   / /__/ /_/ / / / / /_/ /\_ \/ /_/ / __/ /_
+ *  /____/\____/_/ /_/\__  /____/\____/_/  \__/
+ *                   /____/
+ *
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@gmail.com>
+ *
+ * Copyright (C) 2010-2020 Zongsoft Studio <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.Web library.
+ *
+ * The Zongsoft.Web is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3.0 of the License,
+ * or (at your option) any later version.
+ *
+ * The Zongsoft.Web is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Zongsoft.Web library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.Net.Http.Headers;
+
+namespace Zongsoft.Web.Http
+{
+	public static class HttpRequestExtension
+	{
+		public static async Task<string> ReadAsStringAsync(this HttpRequest request)
+		{
+			if(request == null)
+				throw new ArgumentNullException(nameof(request));
+
+			MediaTypeHeaderValue.TryParse(request.ContentType, out MediaTypeHeaderValue mediaType);
+
+			var encoding = mediaType?.Encoding;
+			if(encoding == null || encoding == Encoding.UTF7)
+				encoding = Encoding.UTF8;
+
+			using(var reader = new StreamReader(
+				request.Body,
+				encoding,
+				detectEncodingFromByteOrderMarks: true,
+				bufferSize: 1024,
+				leaveOpen: true))
+			{
+				return await reader.ReadToEndAsync();
+			}
+		}
+	}
+}
