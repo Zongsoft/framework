afa1365924134e317372ad10797a39f09b62bcf3
Popeye Zhong <9555843@qq.com>
2020-08-02 00:37:26 +0800

    完成日期时间范围的函数解析器的实现。 :hammer:

--- Zongsoft.Core/src/Data/RangeParser.cs
+++ Zongsoft.Core/src/Data/RangeParser.cs
@@ -46,14 +46,14 @@ namespace Zongsoft.Data
 
 				switch(context.State)
 				{
+					case State.Error:
+						return new RangeParserResult(context.ErrorMessage);
 					case State.Start:
 						DoStart(ref context);
 						break;
 					case State.Final:
 						DoFinal(ref context);
 						break;
-					case State.Error:
-						return new RangeParserResult(context.ErrorMessage);
 					case State.Minimum:
 						DoMinimum(ref context, out minimum);
 						break;
@@ -78,7 +78,7 @@ namespace Zongsoft.Data
 				if(context.HasFlags(Flags.Parenthesis))
 					return new RangeParserResult("Missing the closing parenthesis.");
 
-				//移动指针到最后
+				//必须移动指针到最后，以便Reset正确计算内容值
 				context.Move(-1);
 
 				switch(context.State)
@@ -366,7 +366,10 @@ namespace Zongsoft.Data
 		[Flags]
 		private enum Flags
 		{
+			/// <summary>无标记</summary>
 			None = 0,
+
+			/// <summary>圆括号包裹</summary>
 			Parenthesis = 1,
 		}
 
@@ -382,8 +385,404 @@ namespace Zongsoft.Data
 			MaximumFinal,
 			Separator,
 		}
+		#endregion
+	}
 
-		private enum DurationState
+	internal static class DateTimeRangeParser
+	{
+		#region 公共方法
+		public static DateTimeRangeParserResult Parse<T>(ReadOnlySpan<char> span, int start) where T : struct
+		{
+			var name = string.Empty;
+			var value = 0;
+			var unit = '\0';
+
+			var context = new DataTimeRangeParserContext(span);
+
+			for(int i = start; i < span.Length; i++)
+			{
+				context.Move(i);
+
+				switch(context.State)
+				{
+					case State.Error:
+						return new DateTimeRangeParserResult(context.ErrorMessage);
+					case State.Start:
+						DoStart(ref context);
+						break;
+					case State.Final:
+						DoFinal(ref context);
+						break;
+					case State.Identifier:
+						DoIdentifier(ref context, out name);
+						break;
+					case State.Gutter:
+						DoGutter(ref context);
+						break;
+					case State.Argument:
+						DoArgument(ref context, out value, out unit);
+						break;
+					case State.Unit:
+						DoUnit(ref context);
+						break;
+				}
+			}
+
+			if(context.State == State.Final)
+				return (name.ToLowerInvariant()) switch
+				{
+					"ago" => GetAgo(value, unit),
+					"last" => GetLast(value, unit),
+					_ => new DateTimeRangeParserResult($"Invalid datetime range function name: {name}."),
+				};
+
+			if(context.State == State.Identifier || context.State == State.Gutter)
+				return (name.ToLowerInvariant()) switch
+				{
+					"today" => GetToday(),
+					"yesterday" => GetYesterday(),
+					"thisweek" => GetThisWeek(),
+					"thismonth" => GetThisMonth(),
+					"thisyear" => GetThisYear(),
+					"lastyear" => GetLastYear(),
+					_ => new DateTimeRangeParserResult($"Invalid datetime range identifier: {name}."),
+				};
+
+			return new DateTimeRangeParserResult($"Invalid datetime range expression format.");
+		}
+		#endregion
+
+		#region 区段计算
+		private static DateTimeRangeParserResult GetToday()
+		{
+			var today = DateTime.Today;
+			return new DateTimeRangeParserResult(today, today.AddSeconds((60 * 60 * 24) - 1));
+		}
+
+		private static DateTimeRangeParserResult GetYesterday()
+		{
+			var yesterday = DateTime.Today.AddDays(-1);
+			return new DateTimeRangeParserResult(yesterday, yesterday.AddSeconds((60 * 60 * 24) - 1));
+		}
+
+		private static DateTimeRangeParserResult GetThisWeek()
+		{
+			var firstday = DateTime.Today.AddDays(-(int)DateTime.Today.DayOfWeek);
+			return new DateTimeRangeParserResult(firstday, firstday.AddSeconds((60 * 60 * 24 * 6) - 1));
+		}
+
+		private static DateTimeRangeParserResult GetThisMonth()
+		{
+			var firstday = new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1);
+			return new DateTimeRangeParserResult(firstday, new DateTime(firstday.Year, firstday.Month, DateTime.DaysInMonth(firstday.Year, firstday.Month), 23, 59, 59, 999));
+		}
+
+		private static DateTimeRangeParserResult GetThisYear()
+		{
+			var firstday = new DateTime(DateTime.Today.Year, 1, 1);
+			return new DateTimeRangeParserResult(firstday, new DateTime(firstday.Year, 12, 31, 23, 59, 59, 999));
+		}
+
+		private static DateTimeRangeParserResult GetLastYear()
+		{
+			var firstday = new DateTime(DateTime.Today.Year - 1, 1, 1);
+			return new DateTimeRangeParserResult(firstday, new DateTime(firstday.Year, 12, 31, 23, 59, 59, 999));
+		}
+
+		private static DateTimeRangeParserResult GetAgo(int number, char unit)
+		{
+			if(number == 0)
+				return GetToday();
+
+			var now = DateTime.Now;
+
+			switch(unit)
+			{
+				case 'Y':
+				case 'y':
+					return new DateTimeRangeParserResult(null, now.AddYears(-number));
+				case 'M':
+					return new DateTimeRangeParserResult(null, now.AddMonths(-number));
+				case 'D':
+				case 'd':
+					return new DateTimeRangeParserResult(null, now.AddDays(-number));
+				case 'H':
+				case 'h':
+					return new DateTimeRangeParserResult(null, now.AddHours(-number));
+				case 'm':
+					return new DateTimeRangeParserResult(null, now.AddMinutes(-number));
+				case 'S':
+				case 's':
+					return new DateTimeRangeParserResult(null, now.AddSeconds(-number));
+				default:
+					throw new ArgumentException("Invalid datetime range unit.");
+			}
+		}
+
+		private static DateTimeRangeParserResult GetLast(int number, char unit)
+		{
+			if(number == 0)
+				return GetToday();
+
+			var now = DateTime.Now;
+
+			switch(unit)
+			{
+				case 'Y':
+				case 'y':
+					return new DateTimeRangeParserResult(now.AddYears(-number), now);
+				case 'M':
+					return new DateTimeRangeParserResult(now.AddMonths(-number), now);
+				case 'D':
+				case 'd':
+					return new DateTimeRangeParserResult(now.AddDays(-number), now);
+				case 'H':
+				case 'h':
+					return new DateTimeRangeParserResult(now.AddHours(-number), now);
+				case 'm':
+					return new DateTimeRangeParserResult(now.AddMinutes(-number), now);
+				case 'S':
+				case 's':
+					return new DateTimeRangeParserResult(now.AddSeconds(-number), now);
+				default:
+					throw new ArgumentException("Invalid datetime range unit.");
+			}
+		}
+		#endregion
+
+		#region 私有方法
+		private static void DoStart(ref DataTimeRangeParserContext context)
+		{
+			if(context.IsLetter)
+				context.Accept(State.Identifier);
+			else
+				context.Error($"The invalid character '{context.Character}' is at {context.Index + 1} character.");
+		}
+
+		private static void DoFinal(ref DataTimeRangeParserContext context)
+		{
+			if(!context.IsWhitespace)
+				context.Error("The datatime range expression contains redundant content.");
+		}
+
+		private static void DoIdentifier(ref DataTimeRangeParserContext context, out string name)
+		{
+			if(context.Character == '(')
+			{
+				context.Reset(State.Argument, out var value);
+				name = value.ToString();
+			}
+			else if(context.IsWhitespace)
+			{
+				context.Reset(State.Gutter, out var value);
+				name = value.ToString();
+			}
+			else if(context.IsLetterOrDigit)
+			{
+				name = null;
+				context.Accept(State.Identifier);
+			}
+			else
+			{
+				name = null;
+				context.Error($"The invalid character '{context.Character}' is at {context.Index + 1} character.");
+			}
+		}
+
+		private static void DoGutter(ref DataTimeRangeParserContext context)
+		{
+			if(context.IsWhitespace)
+				return;
+
+			if(context.Character == '(')
+				context.Reset(State.Argument);
+			else
+				context.Error($"Invalid datetime range expression format.");
+		}
+
+		private static void DoArgument(ref DataTimeRangeParserContext context, out int value, out char unit)
+		{
+			switch(context.Character)
+			{
+				case 'Y':
+				case 'y':
+				case 'M':
+				case 'D':
+				case 'd':
+				case 'H':
+				case 'h':
+				case 'm':
+				case 'S':
+				case 's':
+					unit = context.Character;
+					context.Reset(State.Unit, out var text);
+
+					if(text.Length > 0)
+					{
+						value = int.Parse(text.ToString());
+					}
+					else
+					{
+						value = 0;
+						context.Error($"Missing the argument value.");
+					}
+
+					break;
+				default:
+					if(context.IsDigit)
+						context.Accept(State.Argument);
+					else
+						context.Error($"The argument in the datetime range expression must be a number.");
+
+					value = 0;
+					unit = '\0';
+					break;
+			}
+		}
+
+		private static void DoUnit(ref DataTimeRangeParserContext context)
+		{
+			if(context.IsWhitespace)
+				return;
+
+			if(context.Character == ')')
+				context.Reset(State.Final);
+			else
+				context.Error($"The invalid character '{context.Character}' is at {context.Index + 1} character.");
+		}
+		#endregion
+
+		#region 嵌套结构
+		public ref struct DateTimeRangeParserResult
+		{
+			#region 私有字段
+			private readonly string _message;
+			#endregion
+
+			#region 公共字段
+			public readonly DateTime? Minimum;
+			public readonly DateTime? Maximum;
+			#endregion
+
+			#region 构造函数
+			public DateTimeRangeParserResult(string message)
+			{
+				_message = message;
+				this.Minimum = DateTime.MinValue;
+				this.Maximum = DateTime.MaxValue;
+			}
+
+			public DateTimeRangeParserResult(DateTime? minimum, DateTime? maximum)
+			{
+				_message = null;
+				this.Minimum = minimum;
+				this.Maximum = maximum;
+			}
+			#endregion
+
+			#region 公共方法
+			public bool IsFailed(out string message)
+			{
+				message = _message;
+				return message != null && message.Length > 0;
+			}
+			#endregion
+		}
+
+		private ref struct DataTimeRangeParserContext
+		{
+			#region 私有字段
+			private readonly ReadOnlySpan<char> _text;
+			private State _state;
+			private char _character;
+			private int _index;
+			private int _count;
+			private int _whitespaces;
+			private string _errorMessage;
+			#endregion
+
+			#region 构造函数
+			public DataTimeRangeParserContext(ReadOnlySpan<char> text)
+			{
+				_text = text;
+				_state = State.Start;
+				_index = 0;
+				_character = '\0';
+				_count = 0;
+				_whitespaces = 0;
+				_errorMessage = null;
+			}
+			#endregion
+
+			#region 公共属性
+			public State State { get => _state; }
+			public int Index { get => _index; }
+			public char Character { get => _character; }
+			public string ErrorMessage { get => _errorMessage; }
+			public bool IsWhitespace { get => char.IsWhiteSpace(_character); }
+			public bool IsLetter { get => char.IsLetter(_character); }
+			public bool IsDigit { get => char.IsDigit(_character); }
+			public bool IsLetterOrDigit { get => char.IsLetterOrDigit(_character); }
+			#endregion
+
+			#region 公共方法
+			public bool Move(int index)
+			{
+				if(index >= 0 && index < _text.Length)
+				{
+					_index = index;
+					_character = _text[index];
+					return true;
+				}
+
+				_index = _text.Length;
+				_character = '\0';
+				return false;
+			}
+
+			public void Error(string message)
+			{
+				_state = State.Error;
+				_errorMessage = message;
+			}
+
+			public void Reset(State state)
+			{
+				_count = 0;
+				_whitespaces = 0;
+				_state = state;
+			}
+
+			public void Reset(State state, out ReadOnlySpan<char> value)
+			{
+				value = _count > 0 ? _text.Slice(_index - _count - _whitespaces, _count) : ReadOnlySpan<char>.Empty;
+
+				_count = 0;
+				_whitespaces = 0;
+				_state = state;
+			}
+
+			public void Accept(State state)
+			{
+				if(char.IsWhiteSpace(_character))
+				{
+					if(_count > 0)
+						_whitespaces++;
+				}
+				else
+				{
+					_count += _whitespaces + 1;
+					_whitespaces = 0;
+				}
+
+				_state = state;
+			}
+			#endregion
+		}
+		#endregion
+
+		#region 枚举定义
+		private enum State
 		{
 			Start,
 			Final,
