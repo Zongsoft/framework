c8d22f7ef64c37dd49a0a640baa41ae7e40c4c66
Popeye Zhong <9555843@qq.com>
2020-08-14 16:26:47 +0800

    新增操作原类型，以便支持更加完备的条件定义。 :construction:

---
+++ Zongsoft.Core/src/Data/Operand.cs
@@ -0,0 +1,150 @@
+﻿/*
+ *   _____                                ______
+ *  /_   /  ____  ____  ____  _________  / __/ /_
+ *    / /  / __ \/ __ \/ __ \/ ___/ __ \/ /_/ __/
+ *   / /__/ /_/ / / / / /_/ /\_ \/ /_/ / __/ /_
+ *  /____/\____/_/ /_/\__  /____/\____/_/  \__/
+ *                   /____/
+ *
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@gmail.com>
+ *
+ * Copyright (C) 2010-2020 Zongsoft Studio <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.Core library.
+ *
+ * The Zongsoft.Core is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3.0 of the License,
+ * or (at your option) any later version.
+ *
+ * The Zongsoft.Core is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Zongsoft.Core library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+
+namespace Zongsoft.Data
+{
+	public abstract class Operand
+	{
+		#region 构造函数
+		protected Operand(OperandType type)
+		{
+			this.Type = type;
+		}
+		#endregion
+
+		#region 公共属性
+		public OperandType Type { get; }
+		#endregion
+
+		#region 静态方法
+		public static FieldOperand Field(string name) => new FieldOperand(name);
+		public static ConstantOperand<T> Constant<T>(T value) => new ConstantOperand<T>(value);
+		#endregion
+
+		#region 符号重写
+		public static Operand operator !(Operand operand) => new UnaryOperand(OperandType.Not, operand);
+		public static Operand operator ~(Operand operand) => new UnaryOperand(OperandType.Not, operand);
+		public static Operand operator -(Operand operand) => new UnaryOperand(OperandType.Negate, operand);
+
+		public static Operand operator +(Operand a, Operand b) => new BinaryOperand(OperandType.Add, a, b);
+		public static Operand operator -(Operand a, Operand b) => new BinaryOperand(OperandType.Subtract, a, b);
+		public static Operand operator *(Operand a, Operand b) => new BinaryOperand(OperandType.Multiply, a, b);
+		public static Operand operator /(Operand a, Operand b) => new BinaryOperand(OperandType.Divide, a, b);
+		public static Operand operator %(Operand a, Operand b) => new BinaryOperand(OperandType.Modulo, a, b);
+		public static Operand operator &(Operand a, Operand b) => new BinaryOperand(OperandType.And, a, b);
+		public static Operand operator |(Operand a, Operand b) => new BinaryOperand(OperandType.Or, a, b);
+		public static Operand operator ^(Operand a, Operand b) => new BinaryOperand(OperandType.Xor, a, b);
+		#endregion
+
+		#region 类型转化
+		public static implicit operator Operand(byte value) => Constant(value);
+		public static implicit operator Operand(sbyte value) => Constant(value);
+		public static implicit operator Operand(short value) => Constant(value);
+		public static implicit operator Operand(ushort value) => Constant(value);
+		public static implicit operator Operand(int value) => Constant(value);
+		public static implicit operator Operand(uint value) => Constant(value);
+		public static implicit operator Operand(long value) => Constant(value);
+		public static implicit operator Operand(ulong value) => Constant(value);
+		public static implicit operator Operand(float value) => Constant(value);
+		public static implicit operator Operand(double value) => Constant(value);
+		public static implicit operator Operand(decimal value) => Constant(value);
+		public static implicit operator Operand(bool value) => Constant(value);
+		public static implicit operator Operand(char value) => Constant(value);
+		public static implicit operator Operand(string value) => Constant(value);
+		public static implicit operator Operand(DateTime value) => Constant(value);
+		public static implicit operator Operand(DateTimeOffset value) => Constant(value);
+		public static implicit operator Operand(Guid value) => Constant(value);
+		#endregion
+
+		#region 嵌套子类
+		public class BinaryOperand : Operand
+		{
+			#region 构造函数
+			public BinaryOperand(OperandType type, Operand left, Operand right) : base(type)
+			{
+				this.Left = left;
+				this.Right = right;
+			}
+			#endregion
+
+			#region 公共属性
+			public Operand Left { get; }
+			public Operand Right { get; }
+			#endregion
+		}
+
+		public class UnaryOperand : Operand
+		{
+			#region 构造函数
+			public UnaryOperand(OperandType type, Operand operand) : base(type)
+			{
+				this.Operand = operand;
+			}
+			#endregion
+
+			#region 公共属性
+			public Operand Operand { get; }
+			#endregion
+		}
+
+		public class ConstantOperand<T> : Operand
+		{
+			#region 构造函数
+			public ConstantOperand(T value) : base(OperandType.Constant)
+			{
+				this.Value = value;
+			}
+			#endregion
+
+			#region 公共属性
+			public T Value { get; }
+			#endregion
+		}
+
+		public class FieldOperand : Operand
+		{
+			#region 构造函数
+			public FieldOperand(string name) : base(OperandType.Field)
+			{
+				if(string.IsNullOrWhiteSpace(name))
+					throw new ArgumentNullException(nameof(name));
+
+				this.Name = name.Trim();
+			}
+			#endregion
+
+			#region 公共属性
+			public string Name { get; }
+			#endregion
+		}
+		#endregion
+	}
+}
---
+++ Zongsoft.Core/src/Data/OperandType.cs
@@ -0,0 +1,67 @@
+﻿/*
+ *   _____                                ______
+ *  /_   /  ____  ____  ____  _________  / __/ /_
+ *    / /  / __ \/ __ \/ __ \/ ___/ __ \/ /_/ __/
+ *   / /__/ /_/ / / / / /_/ /\_ \/ /_/ / __/ /_
+ *  /____/\____/_/ /_/\__  /____/\____/_/  \__/
+ *                   /____/
+ *
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@gmail.com>
+ *
+ * Copyright (C) 2010-2020 Zongsoft Studio <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.Core library.
+ *
+ * The Zongsoft.Core is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3.0 of the License,
+ * or (at your option) any later version.
+ *
+ * The Zongsoft.Core is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Zongsoft.Core library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+
+namespace Zongsoft.Data
+{
+	/// <summary>
+	/// 表示操作数类型的枚举。
+	/// </summary>
+	public enum OperandType
+	{
+		/// <summary>加号</summary>
+		Add,
+		/// <summary>减号</summary>
+		Subtract,
+		/// <summary>乘号(*)</summary>
+		Multiply,
+		/// <summary>除号(/)</summary>
+		Divide,
+		/// <summary>取模(%)</summary>
+		Modulo,
+
+		/// <summary>负号</summary>
+		Negate,
+		/// <summary>逻辑非/位取反</summary>
+		Not,
+
+		/// <summary>逻辑与/位与</summary>
+		And,
+		/// <summary>逻辑或/位或</summary>
+		Or,
+		/// <summary>位异或</summary>
+		Xor,
+
+		/// <summary>字段</summary>
+		Field,
+		/// <summary>常量</summary>
+		Constant,
+	}
+}
