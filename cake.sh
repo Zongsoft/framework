ae2be572d3836c9eb0465d94745d118a0e28beb4
Popeye Zhong <9555843@qq.com>
2020-10-07 01:32:48 +0800

    将变量名改为大写。 :cupid:

--- cake.sh
+++ cake.sh
@@ -1,27 +1,24 @@
 #!/bin/sh
 
-set -ex
-
-project_core="Zongsoft.Core/build.cake"
-project_data="Zongsoft.Data/build.cake"
-project_web="Zongsoft.Web/build.cake"
-project_plugins="Zongsoft.Plugins/build.cake"
-project_plugins_web="Zongsoft.Plugins.Web/build.cake"
-project_security="Zongsoft.Security/build.cake"
-project_commands="Zongsoft.Commands/build.cake"
-project_aliyun="externals/aliyun/build.cake"
-project_redis="externals/redis/build.cake"
-
 CAKE_ARGS="--verbosity=verbose"
+PROJECT_CORE="Zongsoft.Core/build.cake"
+PROJECT_DATA="Zongsoft.Data/build.cake"
+PROJECT_WEB="Zongsoft.Web/build.cake"
+PROJECT_PLUGINS="Zongsoft.Plugins/build.cake"
+PROJECT_PLUGINS_WEB="Zongsoft.Plugins.Web/build.cake"
+PROJECT_SECURITY="Zongsoft.Security/build.cake"
+PROJECT_COMMANDS="Zongsoft.Commands/build.cake"
+PROJECT_ALIYUN="externals/aliyun/build.cake"
+PROJECT_REDIS="externals/redis/build.cake"
 
 dotnet tool restore
 
-dotnet cake $project_core $CAKE_ARGS "$@"
-dotnet cake $project_data $CAKE_ARGS "$@"
-dotnet cake $project_web $CAKE_ARGS "$@"
-dotnet cake $project_plugins $CAKE_ARGS "$@"
-dotnet cake $project_plugins_web $CAKE_ARGS "$@"
-dotnet cake $project_security $CAKE_ARGS "$@"
-dotnet cake $project_commands $CAKE_ARGS "$@"
-dotnet cake $project_aliyun $CAKE_ARGS "$@"
-dotnet cake $project_redis $CAKE_ARGS "$@"
+dotnet cake $PROJECT_CORE $CAKE_ARGS "$@"
+dotnet cake $PROJECT_DATA $CAKE_ARGS "$@"
+dotnet cake $PROJECT_WEB $CAKE_ARGS "$@"
+dotnet cake $PROJECT_PLUGINS $CAKE_ARGS "$@"
+dotnet cake $PROJECT_PLUGINS_WEB $CAKE_ARGS "$@"
+dotnet cake $PROJECT_SECURITY $CAKE_ARGS "$@"
+dotnet cake $PROJECT_COMMANDS $CAKE_ARGS "$@"
+dotnet cake $PROJECT_ALIYUN $CAKE_ARGS "$@"
+dotnet cake $PROJECT_REDIS $CAKE_ARGS "$@"
