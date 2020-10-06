82c997292feea9b864f23313e19a7af6dd6cc8ed
Popeye Zhong <9555843@qq.com>
2020-10-07 01:54:49 +0800

    重新还原 cake.sh 脚本文件。 :soccer:

--- cake.sh
+++ cake.sh
@@ -1,8 +1,27 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-CAKE_ARGS="--verbosity=verbose";
-PROJECT_CORE="Zongsoft.Core/build.cake";
+set -ex
+
+CAKE_ARGS="--verbosity=verbose"
+
+PROJECT_CORE="Zongsoft.Core/build.cake"
+PROJECT_DATA="Zongsoft.Data/build.cake"
+PROJECT_WEB="Zongsoft.Web/build.cake"
+PROJECT_PLUGINS="Zongsoft.Plugins/build.cake"
+PROJECT_PLUGINS_WEB="Zongsoft.Plugins.Web/build.cake"
+PROJECT_SECURITY="Zongsoft.Security/build.cake"
+PROJECT_COMMANDS="Zongsoft.Commands/build.cake"
+PROJECT_ALIYUN="externals/aliyun/build.cake"
+PROJECT_REDIS="externals/redis/build.cake"
 
 dotnet tool restore
 
 dotnet cake $PROJECT_CORE $CAKE_ARGS "$@"
+dotnet cake $PROJECT_DATA $CAKE_ARGS "$@"
+dotnet cake $PROJECT_WEB $CAKE_ARGS "$@"
+dotnet cake $PROJECT_PLUGINS $CAKE_ARGS "$@"
+dotnet cake $PROJECT_PLUGINS_WEB $CAKE_ARGS "$@"
+dotnet cake $PROJECT_SECURITY $CAKE_ARGS "$@"
+dotnet cake $PROJECT_COMMANDS $CAKE_ARGS "$@"
+dotnet cake $PROJECT_ALIYUN $CAKE_ARGS "$@"
+dotnet cake $PROJECT_REDIS $CAKE_ARGS "$@"
