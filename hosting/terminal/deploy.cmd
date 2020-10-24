6cc9fb201bacffe034a801dd989dd5260d6456dc
Popeye Zhong <9555843@qq.com>
2020-10-24 16:59:36 +0800

    调整终端程序的部署脚本，以增加引入scheduling插件。 :pineapple:

--- hosting/terminal/deploy.cmd
+++ hosting/terminal/deploy.cmd
@@ -16,30 +16,38 @@ SET Project_2_Destination=zongsoft\security\
 SET Project_3_Name=Zongsoft.Commands
 SET Project_3_Destination=zongsoft\commands\
 
-SET Project_4_Name=Zongsoft.Externals.Aliyun
-SET Project_4_Path=externals\aliyun
-SET Project_4_Destination=zongsoft\%Project_4_Path%\
+SET Project_4_Name=Zongsoft.Scheduling
+SET Project_4_Destination=zongsoft\scheduling\
 
-SET Project_5_Name=Zongsoft.Externals.Redis
-SET Project_5_Path=externals\redis
-SET Project_5_Destination=zongsoft\%Project_5_Path%\
+SET Project_5_Name=Zongsoft.Scheduling.Cron
+SET Project_5_Path=Zongsoft.Scheduling
+SET Project_5_Directory=cron
+SET Project_5_Destination=zongsoft\scheduling\
 
-SET Project_6_Name=Zongsoft.Data.MsSql
-SET Project_6_Path=Zongsoft.Data
-SET Project_6_Directory=drivers\mssql
-SET Project_6_Destination=zongsoft\data\%Project_6_Directory%\
+SET Project_6_Name=Zongsoft.Externals.Aliyun
+SET Project_6_Path=externals\aliyun
+SET Project_6_Destination=zongsoft\%Project_6_Path%\
 
-SET Project_7_Name=Zongsoft.Data.MySql
-SET Project_7_Path=Zongsoft.Data
-SET Project_7_Directory=drivers\mysql
-SET Project_7_Destination=zongsoft\data\%Project_7_Directory%\
+SET Project_7_Name=Zongsoft.Externals.Redis
+SET Project_7_Path=externals\redis
+SET Project_7_Destination=zongsoft\%Project_7_Path%\
+
+SET Project_8_Name=Zongsoft.Data.MsSql
+SET Project_8_Path=Zongsoft.Data
+SET Project_8_Directory=drivers\mssql
+SET Project_8_Destination=zongsoft\data\%Project_8_Directory%\
+
+SET Project_9_Name=Zongsoft.Data.MySql
+SET Project_9_Path=Zongsoft.Data
+SET Project_9_Directory=drivers\mysql
+SET Project_9_Destination=zongsoft\data\%Project_9_Directory%\
 
 REM 拷贝主插件(main.plugin)文件
 xcopy %SourcePathBase%Zongsoft.Plugins\Main.plugin %DestinationPath% /d /y
 REM 拷贝终端插件(terminal.plugin)文件
 xcopy .\plugins\Terminal.plugin %DestinationPath% /d /y
 
-for /L %%i in (1,1,7) do (
+for /L %%i in (1,1,9) do (
 	if not defined Project_%%i_Directory SET Project_%%i_Directory=src
 
 	if not defined Project_%%i_Path (
