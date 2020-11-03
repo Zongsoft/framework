1bae87ecb9caebcaa7b4287feefe9dd5322aae4f
Popeye Zhong <zongsoft@qq.com>
2020-11-03 20:51:01 +0800

    核心库添加消息队列处理程序基类(MessageQueueHandlerBase)。 :bamboo:

--- Zongsoft.Core/src/Messaging/MessageQueueHandlerBase.cs
+++ Zongsoft.Core/src/Messaging/MessageQueueHandlerBase.cs
@@ -11,29 +11,28 @@
  *
  * Copyright (C) 2010-2020 Zongsoft Studio <http://www.zongsoft.com>
  *
- * This file is part of Zongsoft.Scheduling library.
+ * This file is part of Zongsoft.Core library.
  *
- * The Zongsoft.Scheduling is free software: you can redistribute it and/or modify
+ * The Zongsoft.Core is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3.0 of the License,
  * or (at your option) any later version.
  *
- * The Zongsoft.Scheduling is distributed in the hope that it will be useful,
+ * The Zongsoft.Core is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with the Zongsoft.Scheduling library. If not, see <http://www.gnu.org/licenses/>.
+ * along with the Zongsoft.Core library. If not, see <http://www.gnu.org/licenses/>.
  */
 
 using System;
 using System.Threading.Tasks;
 
 using Zongsoft.Services;
-using Zongsoft.Messaging;
 
-namespace Zongsoft.Scheduling.Messaging
+namespace Zongsoft.Messaging
 {
 	public abstract class MessageQueueHandlerBase : IExecutionHandler<IExecutionContext>
 	{
