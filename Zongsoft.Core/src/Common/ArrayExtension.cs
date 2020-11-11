4b6519bb26749061661573418d62521d199d7a22
Popeye Zhong <9555843@qq.com>
2020-11-11 11:15:33 +0800

    添加数组扩展类。 :aerial_tramway:

---
+++ Zongsoft.Core/src/Common/ArrayExtension.cs
@@ -0,0 +1,43 @@
+﻿/*
+ *   _____                                ______
+ *  /_   /  ____  ____  ____  _________  / __/ /_
+ *    / /  / __ \/ __ \/ __ \/ ___/ __ \/ /_/ __/
+ *   / /__/ /_/ / / / / /_/ /\_ \/ /_/ / __/ /_
+ *  /____/\____/_/ /_/\__  /____/\____/_/  \__/
+ *                   /____/
+ *
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@gmail.com>
+ *
+ * Copyright (C) 2010-2020 Zongsoft Studio <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.Core library.
+ *
+ * The Zongsoft.Core is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3.0 of the License,
+ * or (at your option) any later version.
+ *
+ * The Zongsoft.Core is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Zongsoft.Core library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+
+namespace Zongsoft.Common
+{
+	public static class ArrayExtension
+	{
+		/// <summary>
+		/// 获取一个值，指示数组是否为空(null)或长度为零。
+		/// </summary>
+		/// <param name="array">指定要判断的数组。</param>
+		/// <returns>如果数组为空或空则返回真(True)，否则返回假(False)。</returns>
+		public static bool IsEmpty(Array array) => array == null || array.Length == 0;
+	}
+}
