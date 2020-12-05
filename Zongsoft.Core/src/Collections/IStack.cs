c76d8c993c01abe08e259af3ac1e69596bcbd4fc
Popeye Zhong <9555843@qq.com>
2020-12-05 16:15:08 +0800

    添加 栈(IStack) 接口。 :crystal_ball:

---
+++ Zongsoft.Core/src/Collections/IStack.cs
@@ -0,0 +1,49 @@
+﻿/*
+ *   _____                                ______
+ *  /_   /  ____  ____  ____  _________  / __/ /_
+ *    / /  / __ \/ __ \/ __ \/ ___/ __ \/ /_/ __/
+ *   / /__/ /_/ / / / / /_/ /\_ \/ /_/ / __/ /_
+ *  /____/\____/_/ /_/\__  /____/\____/_/  \__/
+ *                   /____/
+ *
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@gmail.com>
+ *
+ * Copyright (C) 2010-2020 Zongsoft Studio <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.Core library.
+ *
+ * The Zongsoft.Core is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3.0 of the License,
+ * or (at your option) any later version.
+ *
+ * The Zongsoft.Core is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Zongsoft.Core library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Collections.Generic;
+
+namespace Zongsoft.Collections
+{
+	public interface IStack<T> : IReadOnlyCollection<T>, IEnumerable<T>
+	{
+		T Pop();
+		T Peek();
+		T Take(int index);
+		T[] Take(int index, int count);
+		void Push(T value);
+		void Clear();
+
+		bool TryPop(out T value);
+		bool TryPeek(out T value);
+		bool TryTake(int index, out T value);
+		bool TryTake(int index, int count, out T[] values);
+	}
+}
