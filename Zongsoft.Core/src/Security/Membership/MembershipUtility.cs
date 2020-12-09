f154fec28df922a390666df4633927c0bf785f7d
Popeye Zhong <9555843@qq.com>
2020-12-09 23:09:56 +0800

    为 MembershipUtility 静态类添加获取授权集的方法。 :taurus:

--- Zongsoft.Core/src/Security/Membership/MembershipUtility.cs
+++ Zongsoft.Core/src/Security/Membership/MembershipUtility.cs
@@ -29,9 +29,11 @@
 
 using System;
 using System.Linq;
+using System.Security.Claims;
 using System.Collections.Generic;
 
 using Zongsoft.Data;
+using Zongsoft.Common;
 
 namespace Zongsoft.Security.Membership
 {
@@ -137,6 +139,33 @@ namespace Zongsoft.Security.Membership
 		}
 
 		/// <summary>
+		/// 获取指定用户的所有上级角色集。
+		/// </summary>
+		/// <param name="dataAccess">数据访问服务。</param>
+		/// <param name="identity">指定的身份标识。</param>
+		/// <param name="flats">输出参数，表示所隶属的所有上级角色集，该集已经去除重复。</param>
+		/// <param name="hierarchies">输出参数，表示所隶属的所有上级角色的层级列表，该列表包含的所有角色已经去除重复。</param>
+		/// <returns>返回指定成员隶属的所有上级角色去重后的数量。</returns>
+		public static int GetAncestors<TRole>(IDataAccess dataAccess, ClaimsIdentity identity, out ISet<TRole> flats, out IList<IEnumerable<TRole>> hierarchies) where TRole : IRole
+		{
+			if(dataAccess == null)
+				throw new ArgumentNullException(nameof(dataAccess));
+
+			if(identity == null)
+				throw new ArgumentNullException(nameof(identity));
+
+			//获取角色表的命名空间字段名
+			var field = Mapping.Instance.Namespace.GetField(Mapping.Instance.Role);
+
+			return GetAncestors(
+				dataAccess,
+				identity.GetIdentifier<uint>(),
+				MemberType.User,
+				Condition.Equal("Role." + field, Mapping.Instance.Namespace.GetNamespace(identity)),
+				out flats, out hierarchies);
+		}
+
+		/// <summary>
 		/// 获取指定用户或角色的上级角色集。
 		/// </summary>
 		/// <param name="dataAccess">数据访问服务。</param>
@@ -220,6 +249,24 @@ namespace Zongsoft.Security.Membership
 				Condition.Equal("Role." + field, Mapping.Instance.Namespace.GetNamespace(user)));
 		}
 
+		public static IEnumerable<TRole> GetAncestors<TRole>(IDataAccess dataAccess, ClaimsIdentity identity) where TRole : IRole
+		{
+			if(dataAccess == null)
+				throw new ArgumentNullException(nameof(dataAccess));
+
+			if(identity == null)
+				throw new ArgumentNullException(nameof(identity));
+
+			//获取角色表的命名空间字段名
+			var field = Mapping.Instance.Namespace.GetField(Mapping.Instance.Role);
+
+			return GetAncestors<TRole>(
+				dataAccess,
+				identity.GetIdentifier<uint>(),
+				MemberType.User,
+				Condition.Equal("Role." + field, Mapping.Instance.Namespace.GetNamespace(identity)));
+		}
+
 		private static IEnumerable<TRole> GetAncestors<TRole>(IDataAccess dataAccess, uint memberId, MemberType memberType, Condition filter) where TRole : IRole
 		{
 			if(dataAccess == null)
@@ -256,6 +303,93 @@ namespace Zongsoft.Security.Membership
 
 			return roles;
 		}
+
+		public static IEnumerable<AuthorizationToken> GetAuthorizes(IDataAccess dataAccess, IRole role)
+		{
+			if(GetAncestors<IRole>(dataAccess, role, out var flats, out var hierarchies) > 0)
+				return GetAuthorizedTokens(dataAccess, flats, hierarchies, role.RoleId, MemberType.Role);
+			else
+				return Array.Empty<AuthorizationToken>();
+		}
+
+		public static IEnumerable<AuthorizationToken> GetAuthorizes(IDataAccess dataAccess, IUser user)
+		{
+			if(GetAncestors<IRole>(dataAccess, user, out var flats, out var hierarchies) > 0)
+				return GetAuthorizedTokens(dataAccess, flats, hierarchies, user.UserId, MemberType.User);
+			else
+				return Array.Empty<AuthorizationToken>();
+		}
+
+		public static IEnumerable<AuthorizationToken> GetAuthorizes(IDataAccess dataAccess, ClaimsIdentity identity)
+		{
+			if(GetAncestors<IRole>(dataAccess, identity, out var flats, out var hierarchies) > 0)
+				return GetAuthorizedTokens(dataAccess, flats, hierarchies, identity.GetIdentifier<uint>(), MemberType.User);
+			else
+				return Array.Empty<AuthorizationToken>();
+		}
+
+		private static IEnumerable<AuthorizationToken> GetAuthorizedTokens(IDataAccess dataAccess, ISet<IRole> flats, IList<IEnumerable<IRole>> hierarchies, uint memberId, MemberType memberType)
+		{
+			var criteria = Condition.Equal(nameof(Permission.MemberId), memberId) &
+			               Condition.Equal(nameof(Permission.MemberType), memberType);
+
+			//获取指定成员的所有上级角色集和上级角色的层级列表
+			if(flats != null && flats.Count > 0)
+			{
+				//如果指定成员有上级角色，则进行权限定义的查询条件还需要加上所有上级角色
+				criteria = criteria.Or(
+					Condition.In(nameof(Permission.MemberId), flats.Select(p => p.RoleId)) &
+					Condition.Equal(nameof(Permission.MemberType), MemberType.Role)
+				);
+			}
+
+			//获取指定条件的所有权限定义（注：禁止分页查询，并即时加载到数组中）
+			var permissions = dataAccess.Select<Permission>(Mapping.Instance.Permission, criteria, Paging.Disabled).ToArray();
+
+			//获取指定条件的所有权限过滤定义（注：禁止分页查询，并即时加载到数组中）
+			var permissionFilters = dataAccess.Select<PermissionFilter>(Mapping.Instance.PermissionFilter, criteria, Paging.Disabled).ToArray();
+
+			var states = new HashSet<AuthorizationState>();
+			IEnumerable<Permission> prepares;
+			IEnumerable<AuthorizationState> grants, denies;
+
+			//如果上级角色层级列表不为空则进行分层过滤
+			if(hierarchies != null && hierarchies.Count > 0)
+			{
+				//从最顶层（即距离指定成员最远的层）开始到最底层（集距离指定成员最近的层）
+				for(int i = hierarchies.Count - 1; i >= 0; i--)
+				{
+					//定义权限集过滤条件：当前层级的角色集的所有权限定义
+					prepares = permissions.Where(p => hierarchies[i].Any(role => role.RoleId == p.MemberId) && p.MemberType == MemberType.Role);
+
+					grants = prepares.Where(p => p.Granted).Select(p => new AuthorizationState(p.SchemaId, p.ActionId)).ToArray();
+					denies = prepares.Where(p => !p.Granted).Select(p => new AuthorizationState(p.SchemaId, p.ActionId)).ToArray();
+
+					states.UnionWith(grants);  //合并授予的权限定义
+					states.ExceptWith(denies); //排除拒绝的权限定义
+
+					//更新授权集中的相关目标的过滤文本
+					SetPermissionFilters(states, permissionFilters.Where(p => hierarchies[i].Any(role => role.RoleId == p.MemberId) && p.MemberType == MemberType.Role));
+				}
+			}
+
+			//查找权限定义中当前成员的设置项
+			prepares = permissions.Where(p => p.MemberId == memberId && p.MemberType == memberType);
+
+			grants = prepares.Where(p => p.Granted).Select(p => new AuthorizationState(p.SchemaId, p.ActionId)).ToArray();
+			denies = prepares.Where(p => !p.Granted).Select(p => new AuthorizationState(p.SchemaId, p.ActionId)).ToArray();
+
+			states.UnionWith(grants);  //合并授予的权限定义
+			states.ExceptWith(denies); //排除拒绝的权限定义
+
+			//更新授权集中的相关目标的过滤文本
+			SetPermissionFilters(states, permissionFilters.Where(p => p.MemberId == memberId && p.MemberType == memberType));
+
+			foreach(var group in states.GroupBy(p => p.SchemaId))
+			{
+				yield return new AuthorizationToken(group.Key, group.Select(p => new AuthorizationToken.ActionToken(p.ActionId, p.Filter)));
+			}
+		}
 		#endregion
 
 		#region 内部方法
@@ -275,10 +409,10 @@ namespace Zongsoft.Security.Membership
 				return Condition.Equal(nameof(IUser.Email), identity);
 			}
 
-			if(IsNumericString(identity))
+			if(identity.IsDigits(out var digits))
 			{
 				identityType = UserIdentityType.Phone;
-				return Condition.Equal(nameof(IUser.Phone), identity);
+				return Condition.Equal(nameof(IUser.Phone), digits);
 			}
 
 			identityType = UserIdentityType.Name;
@@ -293,7 +427,7 @@ namespace Zongsoft.Security.Membership
 			if(identity.Contains("@"))
 				return UserIdentityType.Email;
 
-			if(IsNumericString(identity))
+			if(identity.IsDigits())
 				return UserIdentityType.Phone;
 
 			return UserIdentityType.Name;
@@ -301,25 +435,22 @@ namespace Zongsoft.Security.Membership
 		#endregion
 
 		#region 私有方法
-		[System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
-		private static bool IsNumericString(string text)
+		private static void SetPermissionFilters(IEnumerable<AuthorizationState> states, IEnumerable<PermissionFilter> filters)
 		{
-			if(string.IsNullOrEmpty(text))
-				return false;
+			var groups = filters.GroupBy(p => new AuthorizationState(p.SchemaId, p.ActionId));
 
-			for(var i = 0; i < text.Length; i++)
+			foreach(var group in groups)
 			{
-				if(text[i] < '0' || text[i] > '9')
-					return false;
+				var state = states.FirstOrDefault(p => p.Equals(group.Key));
+
+				if(state != null)
+				{
+					if(string.IsNullOrWhiteSpace(state.Filter))
+						state.Filter = string.Join("; ", group.Select(p => p.Filter));
+					else
+						state.Filter += " | " + string.Join("; ", group.Select(p => p.Filter));
+				}
 			}
-
-			return true;
-		}
-
-		private static bool IsBuiltin(string name)
-		{
-			return string.Equals(name, IUser.Administrator, StringComparison.OrdinalIgnoreCase) ||
-			       string.Equals(name, IRole.Administrators, StringComparison.OrdinalIgnoreCase);
 		}
 		#endregion
 
@@ -340,6 +471,60 @@ namespace Zongsoft.Security.Membership
 			{
 				return (int)role.RoleId;
 			}
+		}
+
+		private class AuthorizationState : IEquatable<AuthorizationState>
+		{
+			#region 公共字段
+			private readonly string KEY;
+
+			public readonly string SchemaId;
+			public readonly string ActionId;
+			public string Filter;
+			#endregion
+
+			#region 构造函数
+			public AuthorizationState(string schemaId, string actionId, string filter = null)
+			{
+				if(string.IsNullOrEmpty(schemaId))
+					throw new ArgumentNullException(nameof(schemaId));
+				if(string.IsNullOrEmpty(actionId))
+					throw new ArgumentNullException(nameof(actionId));
+
+				this.KEY = schemaId.ToUpperInvariant() + ":" + actionId.ToUpperInvariant();
+				this.SchemaId = schemaId;
+				this.ActionId = actionId;
+				this.Filter = filter;
+			}
+			#endregion
+
+			#region 重写方法
+			public bool Equals(AuthorizationState other)
+			{
+				return string.Equals(this.KEY, other.KEY, StringComparison.Ordinal);
+			}
+
+			public override bool Equals(object obj)
+			{
+				if(obj == null || obj.GetType() != typeof(AuthorizationState))
+					return false;
+
+				return this.Equals((AuthorizationState)obj);
+			}
+
+			public override int GetHashCode()
+			{
+				return this.KEY.GetHashCode();
+			}
+
+			public override string ToString()
+			{
+				if(string.IsNullOrEmpty(this.Filter))
+					return this.SchemaId + ":" + this.ActionId;
+				else
+					return this.SchemaId + ":" + this.ActionId + "(" + this.Filter + ")";
+			}
+			#endregion
 		}
 		#endregion
 	}
