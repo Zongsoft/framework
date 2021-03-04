2c0ba7c89a69d4917c0515bbd4a5183f37686df0
Popeye Zhong <9555843@qq.com>
2021-03-04 16:02:45 +0800

    调整代码格式。 :muscle:

--- Zongsoft.Core/src/Common/StringExtension.cs
+++ Zongsoft.Core/src/Common/StringExtension.cs
@@ -28,9 +28,8 @@
  */
 
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
+using System.Collections.Generic;
 
 namespace Zongsoft.Common
 {
