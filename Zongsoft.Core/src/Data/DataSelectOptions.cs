14d859363d1697ed5b7c3df93e116a75c9cf07fe
Popeye Zhong <9555843@qq.com>
2021-03-13 17:48:16 +0800

    增强查询选项的定义。 :blossom:

--- Zongsoft.Core/src/Data/DataSelectOptions.cs
+++ Zongsoft.Core/src/Data/DataSelectOptions.cs
@@ -37,13 +37,14 @@ namespace Zongsoft.Data
 	/// </summary>
 	public interface IDataSelectOptions : IDataOptions
 	{
-		/// <summary>
-		/// 获取或设置一个值，指示是否进行去重查询。
-		/// </summary>
+		/// <summary>获取或设置一个值，指示是否进行去重查询。</summary>
 		bool IsDistinct { get; set; }
 
 		/// <summary>获取或设置过滤表达式文本。</summary>
 		string Filter { get; set; }
+
+		/// <summary>获取或设置一个值，指示是否禁用对子集的延迟加载。</summary>
+		bool LazySuppressed { get; set; }
 
 		/// <summary>
 		/// 获取或设置一个值，指示是否禁用当前数据访问操作的验证器，默认不禁用。
@@ -69,6 +70,9 @@ namespace Zongsoft.Data
 		public string Filter { get; set; }
 
 		/// <inheritdoc />
+		public bool LazySuppressed { get; set; }
+
+		/// <inheritdoc />
 		public bool ValidatorSuppressed { get; set; }
 		#endregion
 
@@ -79,6 +83,35 @@ namespace Zongsoft.Data
 		/// <param name="filter">查询过滤表达式。</param>
 		/// <returns>返回创建的<see cref="DataSelectOptions"/>查询选项对象。</returns>
 		public static DataSelectOptions Distinct(string filter = null) => new DataSelectOptions(filter) { IsDistinct = true };
+
+		/// <summary>
+		/// 创建一个禁用延迟加载的查询选项。
+		/// </summary>
+		/// <param name="distinct">指示是否进行去重查询。</param>
+		/// <returns>返回创建的<see cref="DataSelectOptions"/>查询选项对象。</returns>
+		public static DataSelectOptions SuppressLazy(bool distinct = false)
+		{
+			return new DataSelectOptions()
+			{
+				IsDistinct = distinct,
+				LazySuppressed = true,
+			};
+		}
+
+		/// <summary>
+		/// 创建一个禁用延迟加载的查询选项。
+		/// </summary>
+		/// <param name="filter">查询过滤表达式。</param>
+		/// <param name="distinct">指示是否进行去重查询。</param>
+		/// <returns>返回创建的<see cref="DataSelectOptions"/>查询选项对象。</returns>
+		public static DataSelectOptions SuppressLazy(string filter, bool distinct = false)
+		{
+			return new DataSelectOptions(filter)
+			{
+				IsDistinct = distinct,
+				LazySuppressed = true,
+			};
+		}
 
 		/// <summary>
 		/// 创建一个禁用数据验证器的查询选项。
