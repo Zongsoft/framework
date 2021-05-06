49a3458c4e8862f5064569b34b3ae38e26fbf234
Popeye Zhong <9555843@qq.com>
2021-05-07 00:56:30 +0800

    添加全局过滤器约定类。 :eyeglasses:

---
+++ Zongsoft.Web/src/Filters/GlobalFilterConvention.cs
@@ -0,0 +1,53 @@
+﻿/*
+ *   _____                                ______
+ *  /_   /  ____  ____  ____  _________  / __/ /_
+ *    / /  / __ \/ __ \/ __ \/ ___/ __ \/ /_/ __/
+ *   / /__/ /_/ / / / / /_/ /\_ \/ /_/ / __/ /_
+ *  /____/\____/_/ /_/\__  /____/\____/_/  \__/
+ *                   /____/
+ *
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@gmail.com>
+ *
+ * Copyright (C) 2010-2020 Zongsoft Studio <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.Web library.
+ *
+ * The Zongsoft.Web is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3.0 of the License,
+ * or (at your option) any later version.
+ *
+ * The Zongsoft.Web is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Zongsoft.Web library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
+namespace Zongsoft.Web.Filters
+{
+	public class GlobalFilterConvention : IApplicationModelConvention
+	{
+		public void Apply(ApplicationModel application)
+		{
+			var provider = Zongsoft.Services.ApplicationContext.Current?.Services;
+			var filters = (IEnumerable<IFilterMetadata>)provider.GetService(typeof(IEnumerable<IFilterMetadata>));
+
+			if(filters != null)
+			{
+				foreach(var filter in filters)
+					application.Filters.Add(filter);
+			}
+		}
+	}
+}
