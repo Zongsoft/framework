37763b6e5cadb51fe40e980180a669ba8147a18a
Popeye Zhong <9555843@qq.com>
2021-06-01 20:32:27 +0800

    为核心库添加模板接口。 :tea:

---
+++ Zongsoft.Core/src/Text/ITemplate.cs
@@ -0,0 +1,51 @@
+﻿/*
+ *   _____                                ______
+ *  /_   /  ____  ____  ____  _________  / __/ /_
+ *    / /  / __ \/ __ \/ __ \/ ___/ __ \/ /_/ __/
+ *   / /__/ /_/ / / / / /_/ /\_ \/ /_/ / __/ /_
+ *  /____/\____/_/ /_/\__  /____/\____/_/  \__/
+ *                   /____/
+ *
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@gmail.com>
+ *
+ * Copyright (C) 2010-2020 Zongsoft Studio <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.Core library.
+ *
+ * The Zongsoft.Core is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3.0 of the License,
+ * or (at your option) any later version.
+ *
+ * The Zongsoft.Core is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Zongsoft.Core library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.IO;
+
+namespace Zongsoft.Text
+{
+	/// <summary>
+	/// 表示模板的接口。
+	/// </summary>
+	public interface ITemplate
+	{
+		/// <summary>获取模板名称。</summary>
+		string Name { get; }
+
+		/// <summary>
+		/// 应用模板。
+		/// </summary>
+		/// <param name="data">待应用的模板数据。</param>
+		/// <param name="arguments">附加参数集。</param>
+		/// <returns>返回应用后的文本。</returns>
+		string Evaluate(object data, params object[] arguments);
+	}
+}
---
+++ Zongsoft.Core/src/Text/ITemplateFormatter.cs
@@ -0,0 +1,51 @@
+﻿/*
+ *   _____                                ______
+ *  /_   /  ____  ____  ____  _________  / __/ /_
+ *    / /  / __ \/ __ \/ __ \/ ___/ __ \/ /_/ __/
+ *   / /__/ /_/ / / / / /_/ /\_ \/ /_/ / __/ /_
+ *  /____/\____/_/ /_/\__  /____/\____/_/  \__/
+ *                   /____/
+ *
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@gmail.com>
+ *
+ * Copyright (C) 2010-2020 Zongsoft Studio <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.Core library.
+ *
+ * The Zongsoft.Core is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3.0 of the License,
+ * or (at your option) any later version.
+ *
+ * The Zongsoft.Core is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Zongsoft.Core library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+
+namespace Zongsoft.Text
+{
+	/// <summary>
+	/// 提供模板格式化功能的接口。
+	/// </summary>
+	public interface ITemplateFormatter
+	{
+		/// <summary>获取格式化器名称。</summary>
+		string Name { get; }
+
+		/// <summary>
+		/// 格式化模板参数。
+		/// </summary>
+		/// <param name="name">指定的模板名称。</param>
+		/// <param name="data">待格式化的模板数据。</param>
+		/// <param name="arguments">附加参数集。</param>
+		/// <returns>返回格式化后的模板数据。</returns>
+		object Format(string name, object data, params object[] arguments);
+	}
+}
