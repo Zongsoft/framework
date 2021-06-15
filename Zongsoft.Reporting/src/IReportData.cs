61092140f8432c1dbb686e675110cb13b8b20b3e
Popeye Zhong <9555843@qq.com>
2021-06-15 09:16:50 +0800

    添加报表类库。 :bamboo:

---
+++ Zongsoft.Reporting/src/IReportData.cs
@@ -0,0 +1,65 @@
+﻿/*
+ *   _____                                ______
+ *  /_   /  ____  ____  ____  _________  / __/ /_
+ *    / /  / __ \/ __ \/ __ \/ ___/ __ \/ /_/ __/
+ *   / /__/ /_/ / / / / /_/ /\_ \/ /_/ / __/ /_
+ *  /____/\____/_/ /_/\__  /____/\____/_/  \__/
+ *                   /____/
+ *
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@gmail.com>
+ *
+ * Copyright (C) 2010-2020 Zongsoft Studio <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.Reporting library.
+ *
+ * The Zongsoft.Reporting is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3.0 of the License,
+ * or (at your option) any later version.
+ *
+ * The Zongsoft.Reporting is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Zongsoft.Reporting library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Collections.Generic;
+
+namespace Zongsoft.Reporting
+{
+	public interface IReportData
+	{
+		IList<ReportModelData> Models { get; }
+	}
+
+	public struct ReportModelData
+	{
+		#region 构造函数
+		public ReportModelData(string name, object value = null)
+		{
+			this.Name = name;
+			this.Value = value;
+		}
+		#endregion
+
+		#region 公共属性
+		public string Name { get; }
+		public object Value { get; set; }
+		#endregion
+
+		#region 重写方法
+		public override string ToString()
+		{
+			if(string.IsNullOrEmpty(this.Name))
+				return this.Value?.ToString();
+			else
+				return $"[{this.Name}]{this.Value}";
+		}
+		#endregion
+	}
+}
