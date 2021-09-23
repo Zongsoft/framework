d357c61a4c99d63168144480cd0f320b4560043c
Popeye Zhong <9555843@qq.com>
2021-09-23 17:33:26 +0800

    添加了消息队列的订阅命令。 :musical_note:

---
+++ Zongsoft.Commands/src/Messaging/QueueSubscribeCommand.cs
@@ -0,0 +1,76 @@
+﻿/*
+ *   _____                                ______
+ *  /_   /  ____  ____  ____  _________  / __/ /_
+ *    / /  / __ \/ __ \/ __ \/ ___/ __ \/ /_/ __/
+ *   / /__/ /_/ / / / / /_/ /\_ \/ /_/ / __/ /_
+ *  /____/\____/_/ /_/\__  /____/\____/_/  \__/
+ *                   /____/
+ *
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@gmail.com>
+ *
+ * Copyright (C) 2010-2020 Zongsoft Studio <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.Commands library.
+ *
+ * The Zongsoft.Commands is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3.0 of the License,
+ * or (at your option) any later version.
+ *
+ * The Zongsoft.Commands is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Zongsoft.Commands library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.ComponentModel;
+
+using Zongsoft.Services;
+
+namespace Zongsoft.Messaging.Commands
+{
+	public class QueueSubscribeCommand : CommandBase<CommandContext>
+	{
+		#region 构造函数
+		public QueueSubscribeCommand() : this("Subscribe") { }
+		public QueueSubscribeCommand(string name) : base(name) { }
+		#endregion
+
+		protected override object OnExecute(CommandContext context)
+		{
+			var queue = context.CommandNode.FindQueue();
+
+			if(queue != null)
+				return queue.Subscribe() ? 1 : 0;
+
+			var topic = context.CommandNode.FindTopic();
+
+			if(topic != null)
+			{
+				if(context.Expression.Arguments.Length == 0)
+					throw new CommandException(Properties.Resources.Text_Command_MissingArguments);
+
+				int count = 0;
+
+				foreach(var argument in context.Expression.Arguments)
+				{
+					var index = argument.IndexOf(':');
+
+					if(index > 0 && index < argument.Length)
+						count += topic.Subscribe(argument.Substring(0, index), argument.Substring(index + 1)) ? 1 : 0;
+					else
+						count += topic.Subscribe(argument) ? 1 : 0;
+				}
+
+				return count;
+			}
+
+			return 0;
+		}
+	}
+}
