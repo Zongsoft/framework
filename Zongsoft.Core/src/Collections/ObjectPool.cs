1d880ee4e50f174c003d960748020a7fcdaebff1
Popeye Zhong <9555843@qq.com>
2021-10-11 22:02:01 +0800

    重构 ObjectPool 的方法名称。 :recycle:

--- Zongsoft.Core/src/Collections/ObjectPool.cs
+++ Zongsoft.Core/src/Collections/ObjectPool.cs
@@ -44,15 +44,12 @@ namespace Zongsoft.Collections
 		private ConcurrentBag<T> _idles;
 		private Func<T> _creator;
 		private Action<T> _remover;
-		private int _maximumLimit;
+		private readonly int _maximumLimit;
 		private SemaphoreSlim _semaphore;
 		#endregion
 
 		#region 构造函数
-		protected ObjectPool() : this(0)
-		{
-		}
-
+		protected ObjectPool() : this(0) { }
 		protected ObjectPool(int maximumLimit)
 		{
 			_maximumLimit = Math.Max(maximumLimit, 0);
@@ -66,25 +63,20 @@ namespace Zongsoft.Collections
 		/// 创建一个新的对象管理池。
 		/// </summary>
 		/// <param name="creator">对象的创建方法。</param>
-		/// <param name="remover">对象移除时的回调，该参数值可以为空(null)。</param>
-		public ObjectPool(Func<T> creator, Action<T> remover) : this(creator, remover, 0)
-		{
-		}
+		/// <param name="limit">对象池的最大容量，如果为零则表示不控制池的大小。</param>
+		public ObjectPool(Func<T> creator, int limit = 0) : this(creator, null, limit) { }
 
 		/// <summary>
 		/// 创建一个新的对象管理池。
 		/// </summary>
 		/// <param name="creator">对象的创建方法。</param>
 		/// <param name="remover">对象移除时的回调，该参数值可以为空(null)。</param>
-		/// <param name="maximumLimit">对象池的最大容量，如果小于一则表示不控制池的大小。</param>
-		public ObjectPool(Func<T> creator, Action<T> remover, int maximumLimit)
+		/// <param name="limit">对象池的最大容量，如果为零则表示不控制池的大小。</param>
+		public ObjectPool(Func<T> creator, Action<T> remover, int limit = 0)
 		{
-			if(creator == null)
-				throw new ArgumentNullException("creator");
-
-			_creator = creator;
+			_creator = creator ?? throw new ArgumentNullException(nameof(creator));
 			_remover = remover;
-			_maximumLimit = Math.Max(maximumLimit, 0);
+			_maximumLimit = Math.Max(limit, 0);
 			_idles = new ConcurrentBag<T>();
 
 			if(_maximumLimit > 0)
@@ -93,9 +85,7 @@ namespace Zongsoft.Collections
 		#endregion
 
 		#region 公共属性
-		/// <summary>
-		/// 获取对象池的可用空闲元素数量，负数表示未限定。
-		/// </summary>
+		/// <summary>获取对象池的可用空闲元素数量，负数表示未限定。</summary>
 		public int Count
 		{
 			get
@@ -112,35 +102,25 @@ namespace Zongsoft.Collections
 			}
 		}
 
-		/// <summary>
-		/// 获取对象池的最大容量，零表示不限制。
-		/// </summary>
-		public int MaximumLimit
-		{
-			get
-			{
-				return _maximumLimit;
-			}
-		}
+		/// <summary>获取对象池的最大容量，零表示不限制。</summary>
+		public int MaximumLimit { get => _maximumLimit; }
 		#endregion
 
 		#region 公共方法
 		/// <summary>
 		/// 从对象池中获取一个可用对象。
 		/// </summary>
-		public T GetObject()
+		public T Get()
 		{
 			var idles = _idles;
 
 			if(idles == null)
 				throw new ObjectDisposedException(null);
 
-			T item;
-
 			if(_semaphore != null)
 				_semaphore.Wait();
 
-			if(!idles.TryTake(out item))
+			if(!idles.TryTake(out T item))
 				item = this.OnCreate();
 
 			//如果获取或者创建的新项为空，则释放一个信号量并返回空值
@@ -161,20 +141,20 @@ namespace Zongsoft.Collections
 		/// <summary>
 		/// 将一个对象释放到池中。
 		/// </summary>
-		public void Release(T item)
+		public void Return(T value)
 		{
 			var idles = _idles;
 
 			if(idles == null)
 				throw new ObjectDisposedException(null);
 
-			if(item == null)
-				throw new ArgumentNullException("item");
+			if(value == null)
+				throw new ArgumentNullException(nameof(value));
 
 			//回调放入方法
-			this.OnTakein(item);
+			this.OnTakein(value);
 
-			idles.Add(item);
+			idles.Add(value);
 
 			if(_semaphore != null)
 				_semaphore.Release();
@@ -190,9 +170,7 @@ namespace Zongsoft.Collections
 			if(idles == null)
 				throw new ObjectDisposedException(null);
 
-			T item;
-
-			while(idles.TryTake(out item))
+			while(idles.TryTake(out T item))
 			{
 				this.OnRemove(item);
 			}
@@ -230,12 +208,9 @@ namespace Zongsoft.Collections
 		/// <param name="value">被删除的那个缓存项对象。</param>
 		protected virtual void OnRemove(T value)
 		{
-			if(_remover != null)
-				_remover(value);
+			_remover?.Invoke(value);
 
-			IDisposable disposable = value as IDisposable;
-
-			if(disposable != null)
+			if(value is IDisposable disposable)
 				disposable.Dispose();
 		}
 		#endregion
@@ -252,9 +227,9 @@ namespace Zongsoft.Collections
 			if(!disposing)
 				return;
 
-			Clear();
+			this.Clear();
 
-			var idles = System.Threading.Interlocked.Exchange(ref _idles, null);
+			var idles = Interlocked.Exchange(ref _idles, null);
 
 			if(idles != null)
 			{
