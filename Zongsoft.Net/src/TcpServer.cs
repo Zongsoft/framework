963fdf3facf67486fa99847c1843a1595fe47d3c
Popeye Zhong <9555843@qq.com>
2021-10-15 12:08:58 +0800

    初始化网络库。 :beginner:

---
+++ Zongsoft.Net/src/TcpServer.cs
@@ -0,0 +1,66 @@
+﻿/*
+ *   _____                                ______
+ *  /_   /  ____  ____  ____  _________  / __/ /_
+ *    / /  / __ \/ __ \/ __ \/ ___/ __ \/ /_/ __/
+ *   / /__/ /_/ / / / / /_/ /\_ \/ /_/ / __/ /_
+ *  /____/\____/_/ /_/\__  /____/\____/_/  \__/
+ *                   /____/
+ *
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@gmail.com>
+ *
+ * Copyright (C) 2010-2020 Zongsoft Studio <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.Scheduling library.
+ *
+ * The Zongsoft.Scheduling is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3.0 of the License,
+ * or (at your option) any later version.
+ *
+ * The Zongsoft.Scheduling is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Zongsoft.Scheduling library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Threading.Tasks;
+
+using Zongsoft.Communication;
+
+using Pipelines.Sockets.Unofficial;
+
+namespace Zongsoft.Net
+{
+	public class TcpServer : SocketServer
+	{
+		protected override Task OnClientConnectedAsync(in ClientConnection client)
+		{
+			IMeasuredDuplexPipe clie = client.Transport as IMeasuredDuplexPipe;
+			return Task.CompletedTask;
+		}
+	}
+
+	public class TcpServerChannel : IChannel
+	{
+		public event EventHandler Closed;
+		public event EventHandler Closing;
+
+		public int ChannelId => throw new NotImplementedException();
+		public bool IsIdled => throw new NotImplementedException();
+
+		public void Close()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Dispose()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
