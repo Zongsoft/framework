ad9187bc5f30c37792bd01ff63ea44feabdd2714
Popeye Zhong <9555843@qq.com>
2021-10-18 22:54:20 +0800

    为终端部署脚本添加网络插件部署。 :sound:

--- hosting/terminal/deploy.cmd
+++ hosting/terminal/deploy.cmd
@@ -16,38 +16,41 @@ SET Project_2_Destination=zongsoft\security\
 SET Project_3_Name=Zongsoft.Commands
 SET Project_3_Destination=zongsoft\commands\
 
-SET Project_4_Name=Zongsoft.Scheduling
-SET Project_4_Destination=zongsoft\scheduling\
+SET Project_4_Name=Zongsoft.Net
+SET Project_4_Destination=zongsoft\net\
 
-SET Project_5_Name=Zongsoft.Scheduling.Cron
-SET Project_5_Path=Zongsoft.Scheduling
-SET Project_5_Directory=cron
-SET Project_5_Destination=zongsoft\scheduling\cron\
+SET Project_5_Name=Zongsoft.Scheduling
+SET Project_5_Destination=zongsoft\scheduling\
 
-SET Project_6_Name=Zongsoft.Externals.Aliyun
-SET Project_6_Path=externals\aliyun
-SET Project_6_Destination=zongsoft\%Project_6_Path%\
+SET Project_6_Name=Zongsoft.Scheduling.Cron
+SET Project_6_Path=Zongsoft.Scheduling
+SET Project_6_Directory=cron
+SET Project_6_Destination=zongsoft\scheduling\cron\
 
-SET Project_7_Name=Zongsoft.Externals.Redis
-SET Project_7_Path=externals\redis
+SET Project_7_Name=Zongsoft.Externals.Aliyun
+SET Project_7_Path=externals\aliyun
 SET Project_7_Destination=zongsoft\%Project_7_Path%\
 
-SET Project_8_Name=Zongsoft.Data.MsSql
-SET Project_8_Path=Zongsoft.Data
-SET Project_8_Directory=drivers\mssql
-SET Project_8_Destination=zongsoft\data\%Project_8_Directory%\
+SET Project_8_Name=Zongsoft.Externals.Redis
+SET Project_8_Path=externals\redis
+SET Project_8_Destination=zongsoft\%Project_8_Path%\
 
-SET Project_9_Name=Zongsoft.Data.MySql
+SET Project_9_Name=Zongsoft.Data.MsSql
 SET Project_9_Path=Zongsoft.Data
-SET Project_9_Directory=drivers\mysql
+SET Project_9_Directory=drivers\mssql
 SET Project_9_Destination=zongsoft\data\%Project_9_Directory%\
+
+SET Project_10_Name=Zongsoft.Data.MySql
+SET Project_10_Path=Zongsoft.Data
+SET Project_10_Directory=drivers\mysql
+SET Project_10_Destination=zongsoft\data\%Project_10_Directory%\
 
 REM 拷贝主插件(main.plugin)文件
 xcopy %SourcePathBase%Zongsoft.Plugins\Main.plugin %DestinationPath% /d /y
 REM 拷贝终端插件(terminal.plugin)文件
 xcopy .\plugins\Terminal.plugin %DestinationPath% /d /y
 
-for /L %%i in (1,1,9) do (
+for /L %%i in (1,1,10) do (
 	if not defined Project_%%i_Directory SET Project_%%i_Directory=src
 
 	if not defined Project_%%i_Path (
