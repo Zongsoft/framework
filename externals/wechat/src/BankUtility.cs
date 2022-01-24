3949cc056b45d39d6b833d1e0737a96d45737ee9
Popeye Zhong <9555843@qq.com>
2022-01-24 23:10:52 +0800

    添加微信SDK中的银行查询接口。 :briefcase:

---
+++ externals/wechat/src/BankUtility.cs
@@ -0,0 +1,149 @@
+﻿/*
+ *   _____                                ______
+ *  /_   /  ____  ____  ____  _________  / __/ /_
+ *    / /  / __ \/ __ \/ __ \/ ___/ __ \/ /_/ __/
+ *   / /__/ /_/ / / / / /_/ /\_ \/ /_/ / __/ /_
+ *  /____/\____/_/ /_/\__  /____/\____/_/  \__/
+ *                   /____/
+ *
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@gmail.com>
+ *
+ * Copyright (C) 2010-2020 Zongsoft Studio <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.Externals.WeChat library.
+ *
+ * The Zongsoft.Externals.WeChat is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3.0 of the License,
+ * or (at your option) any later version.
+ *
+ * The Zongsoft.Externals.WeChat is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Zongsoft.Externals.WeChat library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+using Zongsoft.Data;
+using Zongsoft.Security;
+
+namespace Zongsoft.Externals.Wechat
+{
+	public static class BankUtility
+	{
+		public static async ValueTask<IEnumerable<Bank>> GetBanksAsync(this IAuthority authority, BankKind kind, Paging page = null, CancellationToken cancellation = default)
+		{
+			if(authority == null)
+				throw new ArgumentNullException(nameof(authority));
+
+			var offset = page == null || page.PageIndex < 1 ? 0 : (page.PageIndex - 1) * page.PageSize;
+			var limit = page == null || page.PageSize < 1 ? 100 : page.PageSize;
+
+			var response = kind == BankKind.Personal ?
+				await Paying.HttpClientFactory.GetHttpClient(authority.Certificate).GetAsync($"capital/capitallhh/banks/personal-banking?offset={offset}&limit={limit}", cancellation) :
+				await Paying.HttpClientFactory.GetHttpClient(authority.Certificate).GetAsync($"capital/capitallhh/banks/corporate-banking?offset={offset}&limit={limit}", cancellation);
+
+			var result = await response.GetResultAsync<Result<Bank>>(cancellation);
+
+			if(result.Succeed)
+			{
+				if(page != null)
+					page.TotalCount = result.Value.Total;
+
+				return result.Value.Data;
+			}
+
+			return null;
+		}
+
+		public static async ValueTask<IEnumerable<BankBranch>> GetBranchesAsync(this IAuthority authority, string id, string city, Paging page = null, CancellationToken cancellation = default)
+		{
+			if(authority == null)
+				throw new ArgumentNullException(nameof(authority));
+
+			var offset = page == null || page.PageIndex < 1 ? 0 : (page.PageIndex - 1) * page.PageSize;
+			var limit = page == null || page.PageSize < 1 ? 100 : page.PageSize;
+
+			var response = await Paying.HttpClientFactory.GetHttpClient(authority.Certificate).GetAsync($"capital/capitallhh/banks/{id}/branches?city_code={city}&offset={offset}&limit={limit}", cancellation);
+			var result = await response.GetResultAsync<Result<BankBranch>>(cancellation);
+
+			if(result.Succeed)
+			{
+				if(page != null)
+					page.TotalCount = result.Value.Total;
+
+				return result.Value.Data;
+			}
+
+			return null;
+		}
+
+		public static async ValueTask<Bank?> FindAsync(this IAuthority authority, string code, CancellationToken cancellation = default)
+		{
+			if(authority == null)
+				throw new ArgumentNullException(nameof(authority));
+
+			if(string.IsNullOrEmpty(code))
+				throw new ArgumentNullException(nameof(code));
+
+			var account = Convert.ToBase64String(authority.Certificate.Encrypt(code));
+			var response = await Paying.HttpClientFactory.GetHttpClient(authority.Certificate).GetAsync($"capital/capitallhh/banks/search-banks-by-bank-account?account_number={account}", cancellation);
+			var result = await Paying.HttpUtility.GetResultAsync<Result<Bank>>(response, cancellation);
+
+			return result.Succeed && result.Value.Data != null && result.Value.Data.Length > 0 ? result.Value.Data[0] : null;
+		}
+
+		private struct Result<T>
+		{
+			[JsonPropertyName("total_count")]
+			public int Total { get; set; }
+			[JsonPropertyName("count")]
+			public int Count { get; set; }
+			[JsonPropertyName("offset")]
+			public int Offset { get; set; }
+			[JsonPropertyName("data")]
+			public T[] Data { get; set; }
+		}
+
+		public enum BankKind
+		{
+			Personal,
+			Corporational,
+		}
+
+		public struct Bank
+		{
+			[JsonPropertyName("account_bank_code")]
+			public uint Id { get; set; }
+
+			[JsonPropertyName("account_bank")]
+			public string Name { get; set; }
+
+			[JsonPropertyName("bank_alias_code")]
+			public string Code { get; set; }
+
+			[JsonPropertyName("bank_alias")]
+			public string Alias { get; set; }
+
+			[JsonPropertyName("need_bank_branch")]
+			public bool Branched { get; set; }
+		}
+
+		public struct BankBranch
+		{
+			[JsonPropertyName("bank_branch_id")]
+			public string Code { get; set; }
+			[JsonPropertyName("bank_branch_name")]
+			public string Name { get; set; }
+		}
+	}
+}
---
+++ externals/wechat/src/api/Controllers/BankController.cs
@@ -0,0 +1,99 @@
+﻿/*
+ *   _____                                ______
+ *  /_   /  ____  ____  ____  _________  / __/ /_
+ *    / /  / __ \/ __ \/ __ \/ ___/ __ \/ /_/ __/
+ *   / /__/ /_/ / / / / /_/ /\_ \/ /_/ / __/ /_
+ *  /____/\____/_/ /_/\__  /____/\____/_/  \__/
+ *                   /____/
+ *
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@gmail.com>
+ *
+ * Copyright (C) 2010-2020 Zongsoft Studio <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.Externals.WeChat library.
+ *
+ * The Zongsoft.Externals.WeChat is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3.0 of the License,
+ * or (at your option) any later version.
+ *
+ * The Zongsoft.Externals.WeChat is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Zongsoft.Externals.WeChat library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+using Zongsoft.Data;
+
+namespace Zongsoft.Externals.Wechat.Web.Controllers
+{
+	[ApiController]
+	[Route("Externals/Wechat/Banks")]
+	public class BankController : ControllerBase
+	{
+		[HttpGet("{kind}")]
+		public async ValueTask<IActionResult> Get(BankUtility.BankKind kind, [FromQuery]Paging page = null, CancellationToken cancellation = default)
+		{
+			if(page == null)
+				page = Paging.Page(1, 50);
+
+			var result = await AuthorityFactory.GetAuthority().GetBanksAsync(kind, page, cancellation);
+
+			if(page.TotalCount > 0)
+			{
+				this.Response.Headers.Add("X-Paging", page.ToString());
+				this.Response.Headers.Add("X-Pagination", page.ToString());
+
+				return this.Ok(result);
+			}
+
+			return this.NotFound();
+		}
+
+		[HttpGet("{code}/Branches")]
+		public async ValueTask<IActionResult> GetBranches(string code, [FromQuery]string city, [FromQuery]Paging page = null, CancellationToken cancellation = default)
+		{
+			if(string.IsNullOrEmpty(code))
+				return this.BadRequest();
+			if(string.IsNullOrEmpty(city))
+				return this.BadRequest();
+
+			if(page == null)
+				page = Paging.Page(1, 50);
+
+			var result = await AuthorityFactory.GetAuthority().GetBranchesAsync(code, city, page, cancellation);
+
+			if(page.TotalCount > 0)
+			{
+				this.Response.Headers.Add("X-Paging", page.ToString());
+				this.Response.Headers.Add("X-Pagination", page.ToString());
+
+				return this.Ok(result);
+			}
+
+			return this.NotFound();
+		}
+
+		[HttpGet("Find/card:{code}")]
+		public async ValueTask<IActionResult> Find(string code, CancellationToken cancellation = default)
+		{
+			if(string.IsNullOrEmpty(code))
+				return this.BadRequest();
+
+			var result = await AuthorityFactory.GetAuthority().FindAsync(code, cancellation);
+			return result != null ? this.Ok(result.Value) : this.NotFound();
+		}
+	}
+}
