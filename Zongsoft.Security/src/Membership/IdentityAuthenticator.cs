fc928f97c18e2bf71d0ae091fe97a691b3b2fa88
Popeye Zhong <9555843@qq.com>
2022-02-11 18:35:50 +0800

    修改代码注释。 :dog:

--- Zongsoft.Security/src/Membership/IdentityAuthenticator.cs
+++ Zongsoft.Security/src/Membership/IdentityAuthenticator.cs
@@ -11,20 +11,20 @@
  *
  * Copyright (C) 2010-2020 Zongsoft Studio <http://www.zongsoft.com>
  *
- * This file is part of Zongsoft.Core library.
+ * This file is part of Zongsoft.Security library.
  *
- * The Zongsoft.Core is free software: you can redistribute it and/or modify
+ * The Zongsoft.Security is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3.0 of the License,
  * or (at your option) any later version.
  *
- * The Zongsoft.Core is distributed in the hope that it will be useful,
+ * The Zongsoft.Security is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with the Zongsoft.Core library. If not, see <http://www.gnu.org/licenses/>.
+ * along with the Zongsoft.Security library. If not, see <http://www.gnu.org/licenses/>.
  */
 
 using System;
