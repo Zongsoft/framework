4eb46fe2f974af1c682b6ebf516f2cc030c2d139
PopeyeZhong <9555843@qq.com>
2022-08-24 21:11:30 +0800

    重构任务调度器接口。 :school_satchel:

--- Zongsoft.Core/src/Scheduling/IScheduler.cs
+++ Zongsoft.Core/src/Scheduling/IScheduler.cs
@@ -33,17 +33,33 @@ using Zongsoft.Components;
 
 namespace Zongsoft.Scheduling
 {
+	/// <summary>
+	/// 表示作业任务调度器的接口。
+	/// </summary>
 	public interface IScheduler
 	{
-		void Schedule(IHandler handler, ITriggerOptions options = null);
-		void Schedule(Type handlerType, ITriggerOptions options = null);
-		void Schedule<THandler>(ITriggerOptions options = null) where THandler : IHandler;
+		/// <summary>调度作业任务。</summary>
+		/// <param name="name">任务名称或处理程序标识。</param>
+		/// <param name="options">任务触发选项。</param>
+		/// <returns>返回调度成功的作业标识。</returns>
+		string Schedule(string name, ITriggerOptions options = null);
 
-		void Schedule<TParameter>(IHandler<TParameter> handler, TParameter parameter, ITriggerOptions options = null);
-		void Schedule<TParameter>(Type handlerType, TParameter parameter, ITriggerOptions options = null);
-		void Schedule<THandler, TParameter>(TParameter parameter, ITriggerOptions options = null) where THandler : IHandler;
+		/// <summary>调度作业任务。</summary>
+		/// <typeparam name="TParameter">任务参数类型。</typeparam>
+		/// <param name="name">任务名称或处理程序标识。</param>
+		/// <param name="parameter">任务处理参数。</param>
+		/// <param name="options">任务触发选项。</param>
+		/// <returns>返回调度成功的作业标识。</returns>
+		string Schedule<TParameter>(string name, TParameter parameter, ITriggerOptions options = null);
 
+		/// <summary>触发作业调度。</summary>
+		/// <param name="identifier">要触发的作业标识。</param>
+		/// <returns>如果触发成功则返回真(True)，否则返回假(False)。</returns>
 		bool Reschedule(string identifier);
+
+		/// <summary>取消作业调度。</summary>
+		/// <param name="identifier">要取消的作业标识。</param>
+		/// <returns>如果取消作业成功则返回真(True)，否则返回假(False)。</returns>
 		bool Unschedule(string identifier);
 	}
 }
