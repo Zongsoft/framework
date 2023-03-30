93d2311df12332bb362ed1977b40c64c65f53821
Popeye Zhong <9555843@qq.com>
2023-03-30 10:53:00 +0800

    添加对Kafka的部署编译。 :accept:

--- cake.ps1
+++ cake.ps1
@@ -1,19 +1,20 @@
-[string]$project_core           = 'Zongsoft.Core/build.cake'
-[string]$project_data           = 'Zongsoft.Data/build.cake'
-[string]$project_net            = 'Zongsoft.Net/build.cake'
-[string]$project_web            = 'Zongsoft.Web/build.cake'
-[string]$project_plugins        = 'Zongsoft.Plugins/build.cake'
-[string]$project_plugins_web    = 'Zongsoft.Plugins.Web/build.cake'
-[string]$project_scheduling     = 'Zongsoft.Scheduling/build.cake'
-[string]$project_security       = 'Zongsoft.Security/build.cake'
-[string]$project_commands       = 'Zongsoft.Commands/build.cake'
-[string]$project_reporting      = 'Zongsoft.Reporting/build.cake'
-[string]$project_messaging_mqtt = 'Zongsoft.Messaging.Mqtt/build.cake'
-[string]$project_aliyun         = 'externals/aliyun/build.cake'
-[string]$project_redis          = 'externals/redis/build.cake'
-[string]$project_wechat         = 'externals/wechat/build.cake'
-[string]$project_grapecity      = 'externals/grapecity/build.cake'
-[string]$project_hangfire       = 'externals/hangfire/build.cake'
+[string]$project_core            = 'Zongsoft.Core/build.cake'
+[string]$project_data            = 'Zongsoft.Data/build.cake'
+[string]$project_net             = 'Zongsoft.Net/build.cake'
+[string]$project_web             = 'Zongsoft.Web/build.cake'
+[string]$project_plugins         = 'Zongsoft.Plugins/build.cake'
+[string]$project_plugins_web     = 'Zongsoft.Plugins.Web/build.cake'
+[string]$project_scheduling      = 'Zongsoft.Scheduling/build.cake'
+[string]$project_security        = 'Zongsoft.Security/build.cake'
+[string]$project_commands        = 'Zongsoft.Commands/build.cake'
+[string]$project_reporting       = 'Zongsoft.Reporting/build.cake'
+[string]$project_messaging_mqtt  = 'Zongsoft.Messaging.Mqtt/build.cake'
+[string]$project_messaging_kafka = 'Zongsoft.Messaging.Kafka/build.cake'
+[string]$project_aliyun          = 'externals/aliyun/build.cake'
+[string]$project_redis           = 'externals/redis/build.cake'
+[string]$project_wechat          = 'externals/wechat/build.cake'
+[string]$project_grapecity       = 'externals/grapecity/build.cake'
+[string]$project_hangfire        = 'externals/hangfire/build.cake'
 
 [string]$project_administratives = '../Administratives/build.cake'
 
@@ -49,6 +50,9 @@ dotnet cake $project_reporting $CAKE_ARGS $ARGS
 
 Write-Host "dotnet cake $project_messaging_mqtt $CAKE_ARGS $ARGS" -ForegroundColor Magenta
 dotnet cake $project_messaging_mqtt $CAKE_ARGS $ARGS
+
+Write-Host "dotnet cake $project_messaging_kafka $CAKE_ARGS $ARGS" -ForegroundColor Magenta
+dotnet cake $project_messaging_kafka $CAKE_ARGS $ARGS
 
 Write-Host "dotnet cake $project_aliyun $CAKE_ARGS $ARGS" -ForegroundColor Magenta
 dotnet cake $project_aliyun $CAKE_ARGS $ARGS
