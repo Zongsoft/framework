707093817371f024307c0355cd142319110d318c
PopeyeZhong <9555843@qq.com>
2023-07-21 18:19:36 +0800

    添加数据模板相关功能。 :christmas_tree:

---
+++ Zongsoft.Core/src/Data/Templates/DataFileTemplate.cs
@@ -0,0 +1,56 @@
+﻿/*
+ *   _____                                ______
+ *  /_   /  ____  ____  ____  _________  / __/ /_
+ *    / /  / __ \/ __ \/ __ \/ ___/ __ \/ /_/ __/
+ *   / /__/ /_/ / / / / /_/ /\_ \/ /_/ / __/ /_
+ *  /____/\____/_/ /_/\__  /____/\____/_/  \__/
+ *                   /____/
+ *
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@gmail.com>
+ *
+ * Copyright (C) 2010-2023 Zongsoft Studio <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.Core library.
+ *
+ * The Zongsoft.Core is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3.0 of the License,
+ * or (at your option) any later version.
+ *
+ * The Zongsoft.Core is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Zongsoft.Core library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+
+namespace Zongsoft.Data.Templates
+{
+	public class DataFileTemplate : IDataFileTemplate
+	{
+		#region 构造函数
+		public DataFileTemplate(string name, string title, string description = null)
+        {
+			if(string.IsNullOrEmpty(name))
+				throw new ArgumentNullException(nameof(name));
+
+            this.Name = name;
+			this.Title = string.IsNullOrEmpty(title) ? name : title;
+			this.Description = description;
+			this.Fields = new DataFileFieldCollection();
+        }
+		#endregion
+
+		#region 公共属性
+		public string Name { get; }
+		public string Title { get; }
+		public string Description { get; set; }
+		public DataFileFieldCollection Fields { get; }
+		#endregion
+	}
+}
