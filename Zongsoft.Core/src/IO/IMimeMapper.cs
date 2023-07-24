125ad71c636e78f5c6d553f4058597b6d3bd0827
PopeyeZhong <9555843@qq.com>
2023-07-24 17:42:03 +0800

    添加Mime类型映射的接口。 :minibus:

---
+++ Zongsoft.Core/src/IO/IMimeMapper.cs
@@ -0,0 +1,48 @@
+﻿/*
+ *   _____                                ______
+ *  /_   /  ____  ____  ____  _________  / __/ /_
+ *    / /  / __ \/ __ \/ __ \/ ___/ __ \/ /_/ __/
+ *   / /__/ /_/ / / / / /_/ /\_ \/ /_/ / __/ /_
+ *  /____/\____/_/ /_/\__  /____/\____/_/  \__/
+ *                   /____/
+ *
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@gmail.com>
+ *
+ * Copyright (C) 2010-2023 Zongsoft Studio <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.Core library.
+ *
+ * The Zongsoft.Core is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3.0 of the License,
+ * or (at your option) any later version.
+ *
+ * The Zongsoft.Core is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Zongsoft.Core library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Collections.Generic;
+
+namespace Zongsoft.IO
+{
+	/// <summary>
+	/// 提供MIME类型映射功能的接口。
+	/// </summary>
+	public interface IMimeMapper
+	{
+		/// <summary>获取MIME类型的映射表。</summary>
+		IDictionary<string, string> Mappings { get; }
+
+		/// <summary>获取指定名称映射的MIME类型。</summary>
+		/// <param name="path">指定的文件路径或扩展名。</param>
+		/// <returns>如果映射成功则返回指定名称对应的MIME类型，否则返回空(null)。</returns>
+		string GetMimeType(string path);
+	}
+}
---
+++ Zongsoft.Core/src/IO/MimeMapper.cs
@@ -0,0 +1,72 @@
+﻿/*
+ *   _____                                ______
+ *  /_   /  ____  ____  ____  _________  / __/ /_
+ *    / /  / __ \/ __ \/ __ \/ ___/ __ \/ /_/ __/
+ *   / /__/ /_/ / / / / /_/ /\_ \/ /_/ / __/ /_
+ *  /____/\____/_/ /_/\__  /____/\____/_/  \__/
+ *                   /____/
+ *
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@gmail.com>
+ *
+ * Copyright (C) 2010-2023 Zongsoft Studio <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.Core library.
+ *
+ * The Zongsoft.Core is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3.0 of the License,
+ * or (at your option) any later version.
+ *
+ * The Zongsoft.Core is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Zongsoft.Core library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Collections.Generic;
+
+using Zongsoft.Services;
+
+namespace Zongsoft.IO
+{
+	public static class Mime
+	{
+		public static readonly ICollection<IMimeMapper> Mappers = new List<IMimeMapper>();
+
+		public static string GetMimeType(string path)
+		{
+			if(string.IsNullOrEmpty(path))
+				return null;
+
+			if(Mappers.Count == 0)
+			{
+				var mappers = (List<IMimeMapper>)Mappers;
+
+				lock(mappers)
+				{
+					if(mappers.Count == 0)
+					{
+						mappers.AddRange(ApplicationContext.Current.Services.ResolveAll<IMimeMapper>());
+						foreach(var module in ApplicationContext.Current.Modules)
+							mappers.AddRange(module.Services.ResolveAll<IMimeMapper>());
+					}
+				}
+			}
+
+			foreach(var mapper in Mappers)
+			{
+				var result = mapper.GetMimeType(path);
+
+				if(!string.IsNullOrEmpty(result))
+					return result;
+			}
+
+			return null;
+		}
+	}
+}
