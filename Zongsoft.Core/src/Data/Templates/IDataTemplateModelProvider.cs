200aa88d75ada603597e310a3360c8cc026c4c4d
Popeye Zhong <zongsoft@qq.com>
2023-08-03 03:02:18 +0800

    添加数据模板模型相关的设计。 :smoking:

---
+++ Zongsoft.Core/src/Data/Templates/IDataTemplateModelProvider.cs
@@ -0,0 +1,45 @@
+﻿/*
+ *   _____                                ______
+ *  /_   /  ____  ____  ____  _________  / __/ /_
+ *    / /  / __ \/ __ \/ __ \/ ___/ __ \/ /_/ __/
+ *   / /__/ /_/ / / / / /_/ /\_ \/ /_/ / __/ /_
+ *  /____/\____/_/ /_/\__  /____/\____/_/  \__/
+ *                   /____/
+ *
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@gmail.com>
+ *
+ * Copyright (C) 2010-2023 Zongsoft Studio <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.Core library.
+ *
+ * The Zongsoft.Core is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3.0 of the License,
+ * or (at your option) any later version.
+ *
+ * The Zongsoft.Core is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Zongsoft.Core library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+
+namespace Zongsoft.Data.Templates
+{
+	/// <summary>
+	/// 表示数据模板模型提供程序的接口。
+	/// </summary>
+	public interface IDataTemplateModelProvider
+	{
+		/// <summary>获取数据模板模型。</summary>
+		/// <param name="template">指定数据模板。</param>
+		/// <param name="argument">指定的获取参数。</param>
+		/// <returns>如果获取成功则返回对应的数据模板模型对象，否则返回空(<c>null</c>)。</returns>
+		IDataTemplateModel GetModel(IDataTemplate template, object argument);
+	}
+}
