8190d285eabde53b4f7c7024895d40764cbe1cdc
Popeye Zhong <zongsoft@qq.com>
2023-08-15 22:36:21 +0800

    采用最新的终端应用构建和启动方式。 :tv:

--- hosting/terminal/Program.cs
+++ hosting/terminal/Program.cs
@@ -2,27 +2,15 @@
 
 using Microsoft.Extensions.Hosting;
 
-using Zongsoft.Plugins;
-using Zongsoft.Terminals;
-
 namespace Zongsoft.Hosting.Terminal
 {
-	class Program
+	internal class Program
 	{
 		static void Main(string[] args)
 		{
-			Zongsoft.Diagnostics.Logger.Loggers.Add(new Zongsoft.Diagnostics.TextFileLogger());
-			AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
-
-			Host.CreateDefaultBuilder(args)
-				.ConfigurePlugins<TerminalApplicationContext>()
-				.Build()
+			Zongsoft.Plugins.Hosting.Application
+				.Terminal("Zongsoft.Terminal", args)
 				.Run();
-		}
-
-		private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
-		{
-			Zongsoft.Diagnostics.Logger.Error(e.ExceptionObject.ToString());
 		}
 	}
 }
