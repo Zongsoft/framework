1a98eecb6353a0ab469b1af8c9186d93ac471a95
Popeye Zhong <zongsoft@qq.com>
2023-08-15 18:40:41 +0800

    调整条件编译的顺序。 :soccer:

--- Zongsoft.Data/drivers/mysql/MySqlImporter.cs
+++ Zongsoft.Data/drivers/mysql/MySqlImporter.cs
@@ -183,18 +183,16 @@ namespace Zongsoft.Data.MySql
 			writer.Close();
 			file.Dispose();
 
-#if NET5_0
-			context.Count = await bulker.LoadAsync(cancellation);
-#elif NET6_0
-			context.Count = await bulker.LoadAsync(cancellation);
-#elif NET7_0_OR_GREATER
+#if NET7_0_OR_GREATER
 			context.Count = await bulker.LoadAsync(null, cancellation);
+#else
+			context.Count = await bulker.LoadAsync(cancellation);
 #endif
 
 			//删除数据导入的临时文件
 			DeleteFile(bulker.FileName);
 		}
-#endregion
+		#endregion
 
 		#region 私有方法
 		private static MySqlBulkLoader GetBulker(string name, string filePath, MySqlConnection connection, IDataImportOptions options) => new MySqlBulkLoader(connection)
