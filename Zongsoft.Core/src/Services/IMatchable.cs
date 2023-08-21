6db1384e5f50af1134e667af5cf9beb6f583f585
Popeye Zhong <zongsoft@qq.com>
2023-08-22 00:33:32 +0800

    添加 IMatchable<T> 泛型接口。 :guardsman:

--- Zongsoft.Core/src/Services/IMatchable.cs
+++ Zongsoft.Core/src/Services/IMatchable.cs
@@ -43,4 +43,17 @@ namespace Zongsoft.Services
 		/// <returns>如果当前对象符合<paramref name="parameter"/>参数的匹配规则，则为真(true)；否则为假(false)。</returns>
 		bool Match(object parameter);
 	}
+
+	/// <summary>
+	/// 提供一种特定于类型的通用匹配方法，某些同类型的类通过实现此接口对其进行更进一步的匹配。
+	/// </summary>
+	public interface IMatchable<in T>
+	{
+		/// <summary>
+		/// 指示当前对象是否匹配指定参数的条件约束。
+		/// </summary>
+		/// <param name="parameter">指定是否匹配逻辑的参数。</param>
+		/// <returns>如果当前对象符合<paramref name="parameter"/>参数的匹配规则，则为真(true)；否则为假(false)。</returns>
+		bool Match(T parameter);
+	}
 }
