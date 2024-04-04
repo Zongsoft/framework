f25a15ab772269e472a6e944a6a06a022bd9131a
PopeyeZhong <9555843@qq.com>
2024-04-05 01:06:56 +0800

    消除多余的 XML 注释。 :saxophone:

--- Zongsoft.Core/src/Data/IDataServiceValidator.cs
+++ Zongsoft.Core/src/Data/IDataServiceValidator.cs
@@ -40,7 +40,6 @@ namespace Zongsoft.Data
 		/// <param name="service">指定的数据服务对象。</param>
 		/// <param name="method">待验证数据服务方法。</param>
 		/// <param name="criteria">待验证的数据过滤条件。</param>
-		/// <param name="filter">待验证的数据过滤表达式。</param>
 		/// <param name="options">待验证方法的选项参数。</param>
 		/// <returns>返回验证后的过滤条件。</returns>
 		ICondition Validate(IDataService service, DataServiceMethod method, ICondition criteria, IDataOptions options);
