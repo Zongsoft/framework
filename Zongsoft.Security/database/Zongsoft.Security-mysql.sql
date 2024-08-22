e7351220cfb0645550b4a8a78b447b5f9b85b987
PopeyeZhong <9555843@qq.com>
2024-08-22 17:01:13 +0800

    调整MySQL字符集。 :chicken:

--- Zongsoft.Security/database/Zongsoft.Security-mysql.sql
+++ Zongsoft.Security/database/Zongsoft.Security-mysql.sql
@@ -4,9 +4,9 @@ SET TIME_ZONE='+08:00';
 CREATE TABLE IF NOT EXISTS `Security_Role` (
   `RoleId`      int unsigned NOT NULL COMMENT '主键，角色编号',
   `Namespace`   varchar(50)  NULL     COMMENT '命名空间，表示应用或组织机构的标识' COLLATE 'ascii_general_ci',
-  `Name`        varchar(50)  NOT NULL COMMENT '角色名称，所属命名空间内具有唯一性' COLLATE 'utf8mb4_general_ci',
-  `Nickname`    varchar(50)  NULL     COMMENT '角色昵称' COLLATE 'utf8mb4_general_ci',
-  `Description` varchar(500) NULL     COMMENT '描述信息' COLLATE 'utf8mb4_general_ci',
+  `Name`        varchar(50)  NOT NULL COMMENT '角色名称，所属命名空间内具有唯一性' COLLATE 'utf8mb4_0900_ai_ci',
+  `Nickname`    varchar(50)  NULL     COMMENT '角色昵称' COLLATE 'utf8mb4_0900_ai_ci',
+  `Description` varchar(500) NULL     COMMENT '描述信息' COLLATE 'utf8mb4_0900_ai_ci',
   PRIMARY KEY (`RoleId`),
   UNIQUE INDEX `UX_Security_Role_Name` (`Namespace`, `Name`)
 ) ENGINE = InnoDB COMMENT='角色表';
@@ -14,23 +14,23 @@ CREATE TABLE IF NOT EXISTS `Security_Role` (
 CREATE TABLE IF NOT EXISTS `Security_User` (
   `UserId`            int unsigned     NOT NULL COMMENT '主键，用户编号',
   `Namespace`         varchar(50)      NULL     COMMENT '命名空间，表示应用或组织机构的标识' COLLATE 'ascii_general_ci',
-  `Name`              varchar(50)      NOT NULL COMMENT '用户名称，所属命名空间内具有唯一性' COLLATE 'utf8mb4_general_ci',
-  `Nickname`          varchar(50)      NULL     COMMENT '用户昵称' COLLATE 'utf8mb4_general_ci',
+  `Name`              varchar(50)      NOT NULL COMMENT '用户名称，所属命名空间内具有唯一性' COLLATE 'utf8mb4_0900_ai_ci',
+  `Nickname`          varchar(50)      NULL     COMMENT '用户昵称' COLLATE 'utf8mb4_0900_ai_ci',
   `Password`          varbinary(64)    NULL     COMMENT '用户的登录口令',
   `PasswordSalt`      bigint unsigned  NULL     COMMENT '口令加密向量(随机数)',
   `Email`             varchar(50)      NULL     COMMENT '用户的电子邮箱，该邮箱地址在所属命名空间内具有唯一性' COLLATE 'ascii_general_ci',
   `Phone`             varchar(50)      NULL     COMMENT '用户的手机号码，该手机号码在所属命名空间内具有唯一性' COLLATE 'ascii_general_ci',
   `Status`            tinyint unsigned NOT NULL COMMENT '用户状态（0:正常; 1:待批准; 2:已停用）',
   `StatusTimestamp`   datetime         NULL     COMMENT '状态更新时间',
-  `PasswordQuestion1` varchar(50)      NULL     COMMENT '用户的密码问答的题面(1)' COLLATE 'utf8mb4_general_ci',
+  `PasswordQuestion1` varchar(50)      NULL     COMMENT '用户的密码问答的题面(1)' COLLATE 'utf8mb4_0900_ai_ci',
   `PasswordAnswer1`   varbinary(64)    NULL     COMMENT '用户的密码问答的答案(1)',
-  `PasswordQuestion2` varchar(50)      NULL     COMMENT '用户的密码问答的题面(2)' COLLATE 'utf8mb4_general_ci',
+  `PasswordQuestion2` varchar(50)      NULL     COMMENT '用户的密码问答的题面(2)' COLLATE 'utf8mb4_0900_ai_ci',
   `PasswordAnswer2`   varbinary(64)    NULL     COMMENT '用户的密码问答的答案(2)',
-  `PasswordQuestion3` varchar(50)      NULL     COMMENT '用户的密码问答的题面(3)' COLLATE 'utf8mb4_general_ci',
+  `PasswordQuestion3` varchar(50)      NULL     COMMENT '用户的密码问答的题面(3)' COLLATE 'utf8mb4_0900_ai_ci',
   `PasswordAnswer3`   varbinary(64)    NULL     COMMENT '用户的密码问答的答案(3)',
   `Creation`          datetime         NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `Modification`      datetime         NULL     COMMENT '最后修改时间',
-  `Description`       varchar(500)     NULL     COMMENT '描述信息' COLLATE 'utf8mb4_general_ci',
+  `Description`       varchar(500)     NULL     COMMENT '描述信息' COLLATE 'utf8mb4_0900_ai_ci',
   PRIMARY KEY (`UserId`),
   UNIQUE INDEX `UX_Security_User_Name` (`Namespace`, `Name`),
   UNIQUE INDEX `UX_Security_User_Email` (`Namespace`, `Email`),
@@ -63,8 +63,8 @@ CREATE TABLE IF NOT EXISTS `Security_PermissionFilter` (
 ) ENGINE = InnoDB COMMENT='权限过滤表';
 
 CREATE TABLE IF NOT EXISTS `Security_Censorship` (
-  `Name` varchar(50) NOT NULL COMMENT '主键，审查类名' COLLATE 'utf8mb4_general_ci',
-  `Word` varchar(50) NOT NULL COMMENT '主键，阻止词汇' COLLATE 'utf8mb4_general_ci',
+  `Name` varchar(50) NOT NULL COMMENT '主键，审查类名' COLLATE 'utf8mb4_0900_ai_ci',
+  `Word` varchar(50) NOT NULL COMMENT '主键，阻止词汇' COLLATE 'utf8mb4_0900_ai_ci',
   PRIMARY KEY (`Name`, `Word`)
 ) ENGINE = InnoDB COMMENT='词汇审查表';
 
