b926ffffd7a30d973bb259295cced3ea7de726ab
PopeyeZhong <9555843@qq.com>
2024-08-28 03:17:59 +0800

    修复对象构建器的Append(...)方法的隐藏问题。 :dancer:

--- Zongsoft.Plugins/src/Builders/ObjectBuilder.cs
+++ Zongsoft.Plugins/src/Builders/ObjectBuilder.cs
@@ -32,19 +32,15 @@ using System.Linq;
 using System.Reflection;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace Zongsoft.Plugins.Builders
 {
 	public class ObjectBuilder : BuilderBase, IAppender
 	{
 		#region 构造函数
-		public ObjectBuilder()
-		{
-		}
-
-		public ObjectBuilder(IEnumerable<string> ignoredProperties) : base(ignoredProperties)
-		{
-		}
+		public ObjectBuilder() { }
+		public ObjectBuilder(IEnumerable<string> ignoredProperties) : base(ignoredProperties) { }
 		#endregion
 
 		#region 重写方法
@@ -110,6 +106,192 @@ namespace Zongsoft.Plugins.Builders
 
 		#region 私有方法
 		private bool Append(object container, object child, string key)
+		{
+			if(container == null || child == null)
+				return false;
+
+			//第一步：尝试容器是否实现泛型字典接口
+			var invoker = GetGenericDictionaryAdder(container, child, out var convertedChild);
+			if(invoker != null && convertedChild != null)
+			{
+				invoker.DynamicInvoke(key, convertedChild);
+				return true;
+			}
+
+			//第二步：尝试容器是否实现泛型集合接口
+			invoker = GetGenericCollectionAdder(container, child, out convertedChild, out var elementType);
+			if(invoker != null && convertedChild != null)
+			{
+				if(elementType == null)
+					invoker.DynamicInvoke(convertedChild);
+				else
+				{
+					foreach(var element in (IEnumerable)convertedChild)
+						invoker.DynamicInvoke(element);
+				}
+
+				return true;
+			}
+
+			//第三步：尝试容器是否实现传统的非泛型字典接口
+			if(container is IDictionary dictionary && !dictionary.IsReadOnly)
+			{
+				dictionary.Add(key, child);
+				return true;
+			}
+
+			//第四步：尝试容器是否实现传统的非泛型列表接口
+			if(container is IList list && !list.IsReadOnly)
+			{
+				list.Add(child);
+				return true;
+			}
+
+			//第五步：尝试获取容器对象的默认属性，如果获取成功则以其默认属性作为容器
+			var defaultMember = GetDefaultMemberName(container.GetType());
+			if(defaultMember != null && defaultMember.Length > 0 && this.Append(Reflection.Reflector.GetValue(ref container, defaultMember), child, key))
+				return true;
+
+			//第六步：尝试使用容器中的特定(Add/Register)方法进行追加操作
+			var methods = container.GetType().GetMethods(BindingFlags.Public | BindingFlags.Instance)
+						  .Where(method => method.Name == "Add" || method.Name == "Register")
+						  .OrderByDescending(method => method.GetParameters().Length);
+
+			foreach(var method in methods)
+			{
+				var parameters = method.GetParameters();
+
+				if(parameters.Length == 2)
+				{
+					if(parameters[0].ParameterType == typeof(string) && parameters[1].ParameterType.IsAssignableFrom(child.GetType()))
+					{
+						method.Invoke(container, new object[] { key, child });
+						return true;
+					}
+				}
+				else if(parameters.Length == 1)
+				{
+					if(parameters[0].ParameterType.IsAssignableFrom(child.GetType()))
+					{
+						method.Invoke(container, new object[] { child });
+						return true;
+					}
+				}
+			}
+
+			//上述所有步骤均未完成则返回失败
+			return false;
+		}
+
+		private static string GetDefaultMemberName(Type type)
+		{
+			var attribute = Attribute.GetCustomAttribute(type, typeof(DefaultMemberAttribute), true);
+			if(attribute != null)
+				return ((DefaultMemberAttribute)attribute).MemberName;
+
+			attribute = Attribute.GetCustomAttribute(type, typeof(System.ComponentModel.DefaultPropertyAttribute), true);
+			if(attribute != null)
+				return ((System.ComponentModel.DefaultPropertyAttribute)attribute).Name;
+
+			return null;
+		}
+
+		private static Delegate GetGenericDictionaryAdder(object container, object child, out object convertedChild)
+		{
+			convertedChild = null;
+
+			foreach(var contract in container.GetType().GetInterfaces().Where(p => p.IsGenericType && p.GetGenericTypeDefinition() == typeof(IDictionary<,>)))
+			{
+				MethodInfo method = null;
+				var mapping = container.GetType().GetInterfaceMap(contract);
+
+				for(int i = 0; i < mapping.InterfaceMethods.Length; i++)
+				{
+					if(mapping.InterfaceMethods[i].Name == "Add")
+					{
+						var parameters = mapping.InterfaceMethods[i].GetParameters();
+
+						if(parameters.Length == 2 && (parameters[0].ParameterType == typeof(string) || parameters[0].ParameterType == typeof(object)))
+						{
+							if(Zongsoft.Common.Convert.TryConvertValue(child, parameters[1].ParameterType, out convertedChild))
+							{
+								method = mapping.TargetMethods[i];
+								break;
+							}
+						}
+					}
+				}
+
+				if(method != null)
+					return method.CreateDelegate(typeof(Action<,>).MakeGenericType(method.GetParameters()[0].ParameterType, method.GetParameters()[1].ParameterType), container);
+			}
+
+			return null;
+		}
+
+		private static Delegate GetGenericCollectionAdder(object container, object child, out object convertedChild, out Type elementType)
+		{
+			convertedChild = null;
+			elementType = null;
+
+			foreach(var contract in container.GetType().GetInterfaces().Where(p => p.IsGenericType && p.GetGenericTypeDefinition() == typeof(ICollection<>)))
+			{
+				MethodInfo method = null;
+				var mapping = container.GetType().GetInterfaceMap(contract);
+
+				for(int i = 0; i < mapping.InterfaceMethods.Length; i++)
+				{
+					if(mapping.InterfaceMethods[i].Name == "Add")
+					{
+						var parameters = mapping.InterfaceMethods[i].GetParameters();
+
+						if(parameters.Length == 1)
+						{
+							if(Zongsoft.Common.Convert.TryConvertValue(child, parameters[0].ParameterType, out convertedChild))
+							{
+								method = mapping.TargetMethods[i];
+								break;
+							}
+
+							elementType = child is string ? null : Zongsoft.Common.TypeExtension.GetElementType(child.GetType());
+							if(elementType != null && CanConvertTo(elementType, parameters[0].ParameterType))
+							{
+								convertedChild = child;
+								method = mapping.TargetMethods[i];
+								break;
+							}
+						}
+					}
+				}
+
+				if(method != null)
+					return method.CreateDelegate(typeof(Action<>).MakeGenericType(method.GetParameters()[0].ParameterType), container);
+			}
+
+			return null;
+		}
+
+		private static bool CanConvertTo(Type instanceType, Type contractType)
+		{
+			if(contractType == null || instanceType == null)
+				return false;
+
+			if(contractType.IsAssignableFrom(instanceType))
+				return true;
+
+			var converter = TypeDescriptor.GetConverter(instanceType);
+			if(converter != null && converter.CanConvertTo(contractType))
+				return true;
+
+			converter = TypeDescriptor.GetConverter(contractType);
+			if(converter != null && converter.CanConvertFrom(instanceType))
+				return true;
+
+			return false;
+		}
+
+		[Obsolete]
+		private bool AppendObsoleted(object container, object child, string key)
 		{
 			if(container == null || child == null)
 				return false;
@@ -193,22 +375,8 @@ namespace Zongsoft.Plugins.Builders
 			return false;
 		}
 
-		private string GetDefaultMemberName(Type type)
-		{
-			var attribute = Attribute.GetCustomAttribute(type, typeof(DefaultMemberAttribute), true);
-
-			if(attribute != null)
-				return ((DefaultMemberAttribute)attribute).MemberName;
-
-			attribute = Attribute.GetCustomAttribute(type, typeof(System.ComponentModel.DefaultPropertyAttribute), true);
-
-			if(attribute != null)
-				return ((System.ComponentModel.DefaultPropertyAttribute)attribute).Name;
-
-			return null;
-		}
-
-		private Delegate GetDictionaryAddMethod(object container, Type childType, out Type valueType)
+		[Obsolete]
+		private static Delegate GetDictionaryAddMethod(object container, Type childType, out Type valueType)
 		{
 			//设置输出参数默认值
 			valueType = null;
@@ -249,7 +417,8 @@ namespace Zongsoft.Plugins.Builders
 			return null;
 		}
 
-		private Delegate GetCollectionAddMethod(object container, Type childType, out Type valueType)
+		[Obsolete]
+		private static Delegate GetCollectionAddMethod(object container, Type childType, out Type valueType)
 		{
 			//设置输出参数默认值
 			valueType = null;
