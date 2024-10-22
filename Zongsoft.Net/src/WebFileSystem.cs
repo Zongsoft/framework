9c1bcbc861e2d58e9cdaf41c5086c077d25c3fae
PopeyeZhong <9555843@qq.com>
2024-10-22 16:36:23 +0800

    完善Web文件系统。 :wedding:

--- Zongsoft.Net/src/WebFileSystem.cs
+++ Zongsoft.Net/src/WebFileSystem.cs
@@ -29,7 +29,9 @@
 
 using System;
 using System.IO;
+using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Collections.Generic;
@@ -43,33 +45,350 @@ public class WebFileSystem : Zongsoft.IO.IFileSystem
 {
 	public string Scheme => "zfs.web";
 	public IFile File => new FileProvider(ApplicationContext.Current.Services.ResolveRequired<IHttpClientFactory>());
-	public IDirectory Directory => null;
+	public IDirectory Directory => new DirectoryProvider(ApplicationContext.Current.Services.ResolveRequired<IHttpClientFactory>());
 
 	public string GetUrl(string virtualPath) => Zongsoft.IO.Path.TryParse(virtualPath, out var path) ? this.GetUrl(path) : virtualPath;
 	public string GetUrl(Zongsoft.IO.Path path) => $"http://{path.FullPath.TrimStart('/')}";
+
+	private static HttpClient GetClient(IHttpClientFactory factory, ReadOnlySpan<char> fullPath, out string path)
+	{
+		if(factory == null)
+			throw new ArgumentNullException(nameof(factory));
+		if(fullPath.IsEmpty)
+			throw new ArgumentNullException(nameof(fullPath));
+
+		var client = factory.CreateClient();
+		var index = fullPath.IndexOf('/');
+
+		if(index == 0)
+		{
+			fullPath = fullPath[1..];
+			index = fullPath.IndexOf('/');
+		}
+
+		if(index < 0)
+		{
+			path = string.Empty;
+			client.BaseAddress = new Uri($"http://{fullPath.Trim('/')}/Files/");
+		}
+		else
+		{
+			path = fullPath[(index + 1)..].ToString();
+			client.BaseAddress = new Uri($"http://{fullPath[..index].Trim('/')}/Files/");
+		}
+
+		return client;
+	}
+
+	private sealed class DirectoryProvider(IHttpClientFactory factory) : Zongsoft.IO.IDirectory
+	{
+		private readonly IHttpClientFactory _factory = factory;
+
+		public bool Create(string fullPath, IDictionary<string, object> properties = null)
+		{
+			var client = GetClient(_factory, fullPath, out var path);
+			var request = new HttpRequestMessage(HttpMethod.Post, path);
+			var response = client.Send(request);
+			return response.IsSuccessStatusCode;
+		}
+
+		public async ValueTask<bool> CreateAsync(string fullPath, IDictionary<string, object> properties = null)
+		{
+			var client = GetClient(_factory, fullPath, out var path);
+			var response = await client.PostAsync(path, null);
+			return response.IsSuccessStatusCode;
+		}
+
+		public bool Delete(string fullPath, bool recursive = false)
+		{
+			var client = GetClient(_factory, fullPath, out var path);
+			var request = new HttpRequestMessage(HttpMethod.Delete, $"{path}?recursive={recursive}");
+			var response = client.Send(request);
+			return response.IsSuccessStatusCode;
+		}
+
+		public async ValueTask<bool> DeleteAsync(string fullPath, bool recursive = false)
+		{
+			var client = GetClient(_factory, fullPath, out var path);
+			var response = await client.DeleteAsync($"{path}?recursive={recursive}");
+			return response.IsSuccessStatusCode;
+		}
+
+		public bool Exists(string fullPath)
+		{
+			var client = GetClient(_factory, fullPath, out var path);
+			var request = new HttpRequestMessage(HttpMethod.Head, path);
+			var response = client.Send(request);
+			return response.IsSuccessStatusCode;
+		}
+
+		public async ValueTask<bool> ExistsAsync(string fullPath)
+		{
+			var client = GetClient(_factory, fullPath, out var path);
+			var request = new HttpRequestMessage(HttpMethod.Head, path);
+			var response = await client.SendAsync(request);
+			return response.IsSuccessStatusCode;
+		}
+
+		public IEnumerable<PathInfo> GetChildren(string fullPath) => this.GetChildren(fullPath, null, false);
+		public IEnumerable<PathInfo> GetChildren(string fullPath, string pattern, bool recursive = false)
+		{
+			var client = GetClient(_factory, fullPath, out var path);
+			var request = new HttpRequestMessage(HttpMethod.Get, $"{path}?pattern={pattern}&recursive={recursive}");
+			var response = client.Send(request);
+
+			if(response.Content.Headers.ContentLength == null || response.Content.Headers.ContentLength.Value == 0)
+				yield break;
+
+			var stream = response.Content.ReadAsStream();
+			var result = System.Text.Json.JsonSerializer.Deserialize<Result>(stream);
+
+			if(result.Directories != null)
+			{
+				foreach(var directory in result.Directories)
+					yield return new IO.DirectoryInfo(directory.Name, directory.Creation, directory.Modification);
+			}
+
+			if(result.Files != null)
+			{
+				foreach(var file in result.Files)
+					yield return new IO.FileInfo(file.Name, file.Size, file.Creation, file.Modification);
+			}
+		}
+
+		public IAsyncEnumerable<PathInfo> GetChildrenAsync(string fullPath) => this.GetChildrenAsync(fullPath, null, false);
+		public async IAsyncEnumerable<PathInfo> GetChildrenAsync(string fullPath, string pattern, bool recursive = false)
+		{
+			var client = GetClient(_factory, fullPath, out var path);
+			var response = await client.GetAsync($"{path}?pattern={pattern}&recursive={recursive}");
+
+			if(response.Content.Headers.ContentLength == null || response.Content.Headers.ContentLength.Value == 0)
+				yield break;
+
+			var stream = response.Content.ReadAsStream();
+			var result = System.Text.Json.JsonSerializer.Deserialize<Result>(stream);
+
+			if(result.Directories != null)
+			{
+				foreach(var directory in result.Directories)
+					yield return new IO.DirectoryInfo(directory.Name, directory.Creation, directory.Modification);
+			}
+
+			if(result.Files != null)
+			{
+				foreach(var file in result.Files)
+					yield return new IO.FileInfo(file.Name, file.Size, file.Creation, file.Modification);
+			}
+		}
+
+		public IEnumerable<IO.DirectoryInfo> GetDirectories(string fullPath) => this.GetDirectories(fullPath, null, false);
+		public IEnumerable<IO.DirectoryInfo> GetDirectories(string fullPath, string pattern, bool recursive = false)
+		{
+			var client = GetClient(_factory, fullPath, out var path);
+			var request = new HttpRequestMessage(HttpMethod.Get, $"{path}?mode=directory&pattern={pattern}&recursive={recursive}");
+			var response = client.Send(request);
+			return response.IsSuccessStatusCode ? GetDirectoryInfos(response.Content) : [];
+		}
+
+		public IAsyncEnumerable<IO.DirectoryInfo> GetDirectoriesAsync(string fullPath) => this.GetDirectoriesAsync(fullPath, null, false);
+		public async IAsyncEnumerable<IO.DirectoryInfo> GetDirectoriesAsync(string fullPath, string pattern, bool recursive = false)
+		{
+			var client = GetClient(_factory, fullPath, out var path);
+			var response = await client.GetAsync($"{path}?mode=directory&pattern={pattern}&recursive={recursive}");
+
+			if(response.IsSuccessStatusCode)
+			{
+				var infos = GetDirectoryInfosAsync(response.Content);
+				await foreach(var info in infos)
+					yield return info;
+			}
+		}
+
+		public IEnumerable<IO.FileInfo> GetFiles(string fullPath) => this.GetFiles(fullPath, null, false);
+		public IEnumerable<IO.FileInfo> GetFiles(string fullPath, string pattern, bool recursive = false)
+		{
+			var client = GetClient(_factory, fullPath, out var path);
+			var request = new HttpRequestMessage(HttpMethod.Get, $"{path}?mode=file&pattern={pattern}&recursive={recursive}");
+			var response = client.Send(request);
+			return response.IsSuccessStatusCode ? GetFileInfos(response.Content) : [];
+		}
+
+		public IAsyncEnumerable<IO.FileInfo> GetFilesAsync(string fullPath) => this.GetFilesAsync(fullPath, null, false);
+		public async IAsyncEnumerable<IO.FileInfo> GetFilesAsync(string fullPath, string pattern, bool recursive = false)
+		{
+			var client = GetClient(_factory, fullPath, out var path);
+			var response = await client.GetAsync($"{path}?mode=file&pattern={pattern}&recursive={recursive}");
+
+			if(response.IsSuccessStatusCode)
+			{
+				var infos = GetFileInfosAsync(response.Content);
+				await foreach(var info in infos)
+					yield return info;
+			}
+		}
+
+		public IO.DirectoryInfo GetInfo(string fullPath)
+		{
+			var client = GetClient(_factory, fullPath, out var path);
+			var request = new HttpRequestMessage(HttpMethod.Head, path);
+			var response = client.Send(request);
+			return GetDirectoryInfo(response);
+		}
+
+		public async ValueTask<IO.DirectoryInfo> GetInfoAsync(string fullPath)
+		{
+			var client = GetClient(_factory, fullPath, out var path);
+			var request = new HttpRequestMessage(HttpMethod.Head, path);
+			var response = await client.SendAsync(request);
+			return GetDirectoryInfo(response);
+		}
+
+		public void Move(string source, string destination) => throw new NotSupportedException();
+		public ValueTask MoveAsync(string source, string destination) => throw new NotSupportedException();
+		public bool SetInfo(string path, IDictionary<string, object> properties) => throw new NotSupportedException();
+		public ValueTask<bool> SetInfoAsync(string path, IDictionary<string, object> properties) => throw new NotSupportedException();
+
+		private static Zongsoft.IO.DirectoryInfo GetDirectoryInfo(HttpResponseMessage response)
+		{
+			if(response == null || !response.IsSuccessStatusCode)
+				return null;
+
+			var name = response.Headers.TryGetValues("X-Directory-Name", out var value);
+			var creation = response.Headers.TryGetValues("X-Directory-Creation", out value) && DateTime.TryParse(value.ToString(), out var datetime) ? datetime : DateTime.MinValue;
+			var modification = response.Headers.TryGetValues("X-Directory-Modification", out value) && DateTime.TryParse(value.ToString(), out datetime) ? datetime : DateTime.MinValue;
+
+			return new IO.DirectoryInfo(name.ToString(), creation, modification);
+		}
+
+		private static IEnumerable<Zongsoft.IO.DirectoryInfo> GetDirectoryInfos(HttpContent content)
+		{
+			if(content == null || content.Headers.ContentLength == null || content.Headers.ContentLength.Value == 0)
+				return null;
+
+			var stream = content.ReadAsStream();
+			var infos = System.Text.Json.JsonSerializer.Deserialize<DirectoryInfo[]>(stream);
+			return infos.Select(info => new IO.DirectoryInfo(info.Name, info.Creation, info.Modification));
+		}
+
+		private static async IAsyncEnumerable<Zongsoft.IO.DirectoryInfo> GetDirectoryInfosAsync(HttpContent content)
+		{
+			if(content == null || content.Headers.ContentLength == null || content.Headers.ContentLength.Value == 0)
+				yield break;
+
+			var infos = await content.ReadFromJsonAsync<DirectoryInfo[]>();
+			foreach(var info in infos)
+				yield return new IO.DirectoryInfo(info.Name, info.Creation, info.Modification);
+		}
+
+		private static IEnumerable<Zongsoft.IO.FileInfo> GetFileInfos(HttpContent content)
+		{
+			if(content == null || content.Headers.ContentLength == null || content.Headers.ContentLength.Value == 0)
+				return null;
+
+			var stream = content.ReadAsStream();
+			var infos = System.Text.Json.JsonSerializer.Deserialize<FileInfo[]>(stream);
+			return infos.Select(info => new IO.FileInfo(info.Name, info.Size, info.Creation, info.Modification));
+		}
+
+		private static async IAsyncEnumerable<Zongsoft.IO.FileInfo> GetFileInfosAsync(HttpContent content)
+		{
+			if(content == null || content.Headers.ContentLength == null || content.Headers.ContentLength.Value == 0)
+				yield break;
+
+			var infos = await content.ReadFromJsonAsync<FileInfo[]>();
+			foreach(var info in infos)
+				yield return new IO.FileInfo(info.Name, info.Size, info.Creation, info.Modification);
+		}
+
+		private sealed class Result
+		{
+			public DirectoryInfo[] Directories { get; set; }
+			public FileInfo[] Files { get; set; }
+		}
+
+		private sealed class FileInfo
+		{
+			public string Name { get; set; }
+			public string Type { get; set; }
+			public long Size { get; set; }
+			public DateTime Creation { get; set; }
+			public DateTime Modification { get; set; }
+		}
+
+		private sealed class DirectoryInfo
+		{
+			public string Name { get; set; }
+			public DateTime Creation { get; set; }
+			public DateTime Modification { get; set; }
+		}
+	}
 
 	private sealed class FileProvider(IHttpClientFactory factory) : Zongsoft.IO.IFile
 	{
 		private readonly IHttpClientFactory _factory = factory;
 
 		public void Copy(string source, string destination) => this.Copy(source, destination, true);
-		public void Copy(string source, string destination, bool overwrite) => throw new NotImplementedException();
-		public Task CopyAsync(string source, string destination) => this.CopyAsync(source, destination, true);
-		public Task CopyAsync(string source, string destination, bool overwrite) => throw new NotImplementedException();
-		public bool Delete(string path) => throw new NotImplementedException();
-		public Task<bool> DeleteAsync(string path) => throw new NotImplementedException();
-		public bool Exists(string path) => throw new NotImplementedException();
-		public Task<bool> ExistsAsync(string path) => throw new NotImplementedException();
-		public Zongsoft.IO.FileInfo GetInfo(string path) => throw new NotImplementedException();
-		public Task<Zongsoft.IO.FileInfo> GetInfoAsync(string path) => throw new NotImplementedException();
-		public void Move(string source, string destination) => throw new NotImplementedException();
-		public Task MoveAsync(string source, string destination) => throw new NotImplementedException();
+		public void Copy(string source, string destination, bool overwrite) => throw new NotSupportedException();
+		public ValueTask CopyAsync(string source, string destination) => this.CopyAsync(source, destination, true);
+		public ValueTask CopyAsync(string source, string destination, bool overwrite) => throw new NotSupportedException();
+		public void Move(string source, string destination) => throw new NotSupportedException();
+		public ValueTask MoveAsync(string source, string destination) => throw new NotSupportedException();
+		public bool SetInfo(string path, IDictionary<string, object> properties) => throw new NotSupportedException();
+		public ValueTask<bool> SetInfoAsync(string path, IDictionary<string, object> properties) => throw new NotSupportedException();
+
+		public bool Delete(string fullPath)
+		{
+			var client = GetClient(_factory, fullPath, out var path);
+			var request = new HttpRequestMessage(HttpMethod.Delete, path);
+			var response = client.Send(request);
+			return response.IsSuccessStatusCode;
+		}
+
+		public async ValueTask<bool> DeleteAsync(string fullPath)
+		{
+			var client = GetClient(_factory, fullPath, out var path);
+			var response = await client.DeleteAsync(path);
+			return response.IsSuccessStatusCode;
+		}
+
+		public bool Exists(string fullPath)
+		{
+			var client = GetClient(_factory, fullPath, out var path);
+			var request = new HttpRequestMessage(HttpMethod.Head, path);
+			var response = client.Send(request);
+			return response.IsSuccessStatusCode;
+		}
+
+		public async ValueTask<bool> ExistsAsync(string fullPath)
+		{
+			var client = GetClient(_factory, fullPath, out var path);
+			var request = new HttpRequestMessage(HttpMethod.Head, path);
+			var response = await client.SendAsync(request);
+			return response.IsSuccessStatusCode;
+		}
+
+		public Zongsoft.IO.FileInfo GetInfo(string fullPath)
+		{
+			var client = GetClient(_factory, fullPath, out var path);
+			var request = new HttpRequestMessage(HttpMethod.Head, path);
+			var response = client.Send(request);
+			return GetFileInfo(response);
+		}
+
+		public async ValueTask<Zongsoft.IO.FileInfo> GetInfoAsync(string fullPath)
+		{
+			var client = GetClient(_factory, fullPath, out var path);
+			var request = new HttpRequestMessage(HttpMethod.Head, path);
+			var response = await client.SendAsync(request);
+			return GetFileInfo(response);
+		}
+
 		public Stream Open(string path, IDictionary<string, object> properties = null) => this.Open(path, FileMode.Open, FileAccess.Read, FileShare.None, properties);
 		public Stream Open(string path, FileMode mode, IDictionary<string, object> properties = null) => this.Open(path, mode, FileAccess.Read, FileShare.None, properties);
 		public Stream Open(string path, FileMode mode, FileAccess access, IDictionary<string, object> properties = null) => this.Open(path, mode, access, FileShare.None, properties);
 		public Stream Open(string path, FileMode mode, FileAccess access, FileShare share, IDictionary<string, object> properties = null)
 		{
-			var client = this.GetClient(path, out var p);
+			var client = GetClient(_factory, path, out var p);
 			bool writable = (mode != FileMode.Open) || (access & FileAccess.Write) == FileAccess.Write;
 
 			if(writable)
@@ -80,35 +399,27 @@ public class WebFileSystem : Zongsoft.IO.IFileSystem
 			return response.Content.ReadAsStream();
 		}
 
-		public bool SetInfo(string path, IDictionary<string, object> properties) => throw new NotImplementedException();
-		public Task<bool> SetInfoAsync(string path, IDictionary<string, object> properties) => throw new NotImplementedException();
-
-		private HttpClient GetClient(ReadOnlySpan<char> fullPath, out string path)
+		private static Zongsoft.IO.FileInfo GetFileInfo(HttpResponseMessage response)
 		{
-			if(fullPath.IsEmpty)
-				throw new ArgumentNullException(nameof(fullPath));
+			if(response == null || !response.IsSuccessStatusCode)
+				return null;
 
-			var client = _factory.CreateClient();
-			var index = fullPath.IndexOf('/');
+			var name = response.Headers.TryGetValues("X-File-Name", out var value);
+			var size = response.Headers.TryGetValues("X-File-Size", out value) && long.TryParse(value.ToString(), out var number) ? number : 0;
+			var type = response.Headers.TryGetValues("X-File-Type", out value) ? value.ToString() : null;
+			var creation = response.Headers.TryGetValues("X-File-Creation", out value) && DateTime.TryParse(value.ToString(), out var datetime) ? datetime : DateTime.MinValue;
+			var modification = response.Headers.TryGetValues("X-File-Modification", out value) && DateTime.TryParse(value.ToString(), out datetime) ? datetime : DateTime.MinValue;
 
-			if(index == 0)
-			{
-				fullPath = fullPath[1..];
-				index = fullPath.IndexOf('/');
-			}
+			return new IO.FileInfo(name.ToString(), size, creation, modification);
+		}
 
-			if(index < 0)
-			{
-				path = string.Empty;
-				client.BaseAddress = new Uri($"http://{fullPath.Trim('/')}/Files/");
-			}
-			else
-			{
-				path = fullPath[(index + 1)..].ToString();
-				client.BaseAddress = new Uri($"http://{fullPath[..index].Trim('/')}/Files/");
-			}
-
-			return client;
+		private struct FileInfo
+		{
+			public string Name { get; set; }
+			public long Size { get; set; }
+			public string Type { get; set; }
+			public DateTimeOffset Creation { get; set; }
+			public DateTimeOffset Modification { get; set; }
 		}
 	}
 
