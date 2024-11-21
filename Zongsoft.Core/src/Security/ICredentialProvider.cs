2809105e34c975d4e702bd8162138a70479608fd
PopeyeZhong <9555843@qq.com>
2024-11-21 19:22:55 +0800

    调整代码格式。 ☕

--- Zongsoft.Core/src/Security/ICredentialProvider.cs
+++ Zongsoft.Core/src/Security/ICredentialProvider.cs
@@ -51,37 +51,27 @@ namespace Zongsoft.Security
 		#endregion
 
 		#region 方法定义
-		/// <summary>
-		/// 将指定的凭证主体注册到凭证容器中。
-		/// </summary>
+		/// <summary>将指定的凭证主体注册到凭证容器中。</summary>
 		/// <param name="principal">指定要注册的凭证主体对象。</param>
 		void Register(CredentialPrincipal principal);
 
-		/// <summary>
-		/// 从安全凭证容器中注销指定的凭证。
-		/// </summary>
+		/// <summary>从安全凭证容器中注销指定的凭证。</summary>
 		/// <param name="credentialId">指定的要注销的安全凭证编号。</param>
 		void Unregister(string credentialId);
 
-		/// <summary>
-		/// 续约指定的凭证主体。
-		/// </summary>
+		/// <summary>续约指定的凭证主体。</summary>
 		/// <param name="credentialId">指定要续约的安全凭证编号。</param>
 		/// <param name="token">续约的安全标记。</param>
 		/// <returns>返回续约成功的凭证主体对象，如果续约失败则返回空(null)。</returns>
 		/// <remarks>注：续约操作会依次激发“Unregistered”和“Registered”事件。</remarks>
 		CredentialPrincipal Renew(string credentialId, string token);
 
-		/// <summary>
-		/// 获取指定安全凭证编号对应的<see cref="CredentialPrincipal"/>凭证主体对象。
-		/// </summary>
+		/// <summary>获取指定安全凭证编号对应的<see cref="CredentialPrincipal"/>凭证主体对象。</summary>
 		/// <param name="credentialId">指定要获取的安全凭证编号。</param>
 		/// <returns>返回的对应的凭证主体对象，如果指定的安全凭证编号不存在则返回空(null)。</returns>
 		CredentialPrincipal GetPrincipal(string credentialId);
 
-		/// <summary>
-		/// 获取指定用户及应用场景对应的<see cref="CredentialPrincipal"/>凭证主体对象。
-		/// </summary>
+		/// <summary>获取指定用户及应用场景对应的<see cref="CredentialPrincipal"/>凭证主体对象。</summary>
 		/// <param name="identity">指定要获取的安全凭证对应的用户唯一标识。</param>
 		/// <param name="scenario">指定要获取的安全凭证对应的应用场景。</param>
 		/// <returns>返回成功的凭证主体对象，如果指定的用户身份及应用场景未被注册则返回空(null)。</returns>
