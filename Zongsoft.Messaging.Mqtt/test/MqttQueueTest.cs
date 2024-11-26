69a31e54642ae9c0bead921cd3371323bf2a5637
PopeyeZhong <9555843@qq.com>
2024-11-26 17:14:13 +0800

    简单调整下单元测试代码。 📤

--- Zongsoft.Messaging.Mqtt/test/MqttQueueTest.cs
+++ Zongsoft.Messaging.Mqtt/test/MqttQueueTest.cs
@@ -9,14 +9,17 @@ namespace Zongsoft.Messaging.Mqtt.Tests;
 
 public class MqttQueueTest
 {
+	private const string TOPIC = "TopicX";
+	private const string CONNECTION_STRING = $"Server=127.0.0.1;UserName=program;Password=MyMQTT2-Password;Topic={TOPIC}";
+
 	[Fact]
 	public void TestProduce()
 	{
-		var settings = new ConnectionSettings("MyMqtt", @"Server=127.0.0.1;UserName=program;Password=MyMQTT2-Password;Topic=Topic1");
+		var settings = new ConnectionSettings("MyMqtt", CONNECTION_STRING);
 		var queue = new MqttQueue(string.Empty, settings);
 		Assert.NotNull(queue);
 
-		var topic = settings.Values.TryGetValue("Topic", out var value) ? value : "Topic1";
+		var topic = settings.Values.TryGetValue("Topic", out var value) ? value : TOPIC;
 		var data = System.Text.Encoding.UTF8.GetBytes("Hello, World!");
 		var result = queue.Produce(topic, data);
 	}
