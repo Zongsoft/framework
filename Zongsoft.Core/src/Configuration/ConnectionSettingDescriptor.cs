52a0a1eef7dbf5d5f99fe895c1d363ca78d3ef15
PopeyeZhong <9555843@qq.com>
2024-12-27 14:12:56 +0800

    ConnectionSettingDescriptor 类添加 Format 和 Dependencies 属性。 👑

--- Zongsoft.Core/src/Configuration/ConnectionSettingDescriptor.cs
+++ Zongsoft.Core/src/Configuration/ConnectionSettingDescriptor.cs
@@ -29,143 +29,164 @@
 
 using System;
 
-namespace Zongsoft.Configuration
+namespace Zongsoft.Configuration;
+
+public class ConnectionSettingDescriptor : IEquatable<string>
 {
-	public class ConnectionSettingDescriptor : IEquatable<string>
+	#region 静态字段
+	/// <summary>获取分组标识描述器。</summary>
+	public static readonly ConnectionSettingDescriptor Group = new(nameof(Group), typeof(string), null, Properties.Resources.ConnectionSettingDescriptor_Group, Properties.Resources.ConnectionSettingDescriptor_Group_Description);
+	/// <summary>获取客户端标识描述器。</summary>
+	public static readonly ConnectionSettingDescriptor Client = new(nameof(Client), typeof(string), null, Properties.Resources.ConnectionSettingDescriptor_Client, Properties.Resources.ConnectionSettingDescriptor_Client_Description);
+	/// <summary>获取服务器地址描述器。</summary>
+	public static readonly ConnectionSettingDescriptor Server = new(nameof(Server), typeof(string), true, null, Properties.Resources.ConnectionSettingDescriptor_Server, Properties.Resources.ConnectionSettingDescriptor_Server_Description);
+	/// <summary>获取端口号描述器。</summary>
+	public static readonly ConnectionSettingDescriptor Port = new(nameof(Port), typeof(ushort), null, Properties.Resources.ConnectionSettingDescriptor_Port, Properties.Resources.ConnectionSettingDescriptor_Port_Description);
+	/// <summary>获取超时描述器。</summary>
+	public static readonly ConnectionSettingDescriptor Timeout = new(nameof(Timeout), typeof(TimeSpan), null, Properties.Resources.ConnectionSettingDescriptor_Timeout, Properties.Resources.ConnectionSettingDescriptor_Timeout_Description);
+	/// <summary>获取字符集描述器。</summary>
+	public static readonly ConnectionSettingDescriptor Charset = new(nameof(Charset), typeof(string), null, Properties.Resources.ConnectionSettingDescriptor_Charset, Properties.Resources.ConnectionSettingDescriptor_Charset_Description);
+	/// <summary>获取字符编码描述器。</summary>
+	public static readonly ConnectionSettingDescriptor Encoding = new(nameof(Encoding), typeof(string), null, Properties.Resources.ConnectionSettingDescriptor_Encoding, Properties.Resources.ConnectionSettingDescriptor_Encoding_Description);
+	/// <summary>获取提供程序描述器。</summary>
+	public static readonly ConnectionSettingDescriptor Provider = new(nameof(Provider), typeof(string), null, Properties.Resources.ConnectionSettingDescriptor_Provider, Properties.Resources.ConnectionSettingDescriptor_Provider_Description);
+	/// <summary>获取数据库名描述器。</summary>
+	public static readonly ConnectionSettingDescriptor Database = new(nameof(Database), typeof(string), null, Properties.Resources.ConnectionSettingDescriptor_Database, Properties.Resources.ConnectionSettingDescriptor_Database_Description);
+	/// <summary>获取连接账户描述器。</summary>
+	public static readonly ConnectionSettingDescriptor UserName = new(nameof(UserName), typeof(string), null, Properties.Resources.ConnectionSettingDescriptor_UserName, Properties.Resources.ConnectionSettingDescriptor_UserName_Description);
+	/// <summary>获取连接密码描述器。</summary>
+	public static readonly ConnectionSettingDescriptor Password = new(nameof(Password), typeof(string), null, Properties.Resources.ConnectionSettingDescriptor_Password, Properties.Resources.ConnectionSettingDescriptor_Password_Description);
+	/// <summary>获取实例标识描述器。</summary>
+	public static readonly ConnectionSettingDescriptor Instance = new(nameof(Instance), typeof(string), null, Properties.Resources.ConnectionSettingDescriptor_Instance, Properties.Resources.ConnectionSettingDescriptor_Instance_Description);
+	/// <summary>获取应用标识描述器。</summary>
+	public static readonly ConnectionSettingDescriptor Application = new(nameof(Application), typeof(string), null, Properties.Resources.ConnectionSettingDescriptor_Application, Properties.Resources.ConnectionSettingDescriptor_Application_Description);
+	#endregion
+
+	#region 构造函数
+	public ConnectionSettingDescriptor(string name, object defaultValue = null, string label = null, string description = null) : this(name, null, typeof(string), false, defaultValue, label, description) { }
+	public ConnectionSettingDescriptor(string name, bool required, object defaultValue = null, string label = null, string description = null) : this(name, null, typeof(string), required, defaultValue, label, description) { }
+	public ConnectionSettingDescriptor(string name, Type type, object defaultValue = null, string label = null, string description = null) : this(name, null, type, false, defaultValue, label, description) { }
+	public ConnectionSettingDescriptor(string name, Type type, bool required, object defaultValue = null, string label = null, string description = null) : this(name, null, type, required, defaultValue, label, description) { }
+
+	public ConnectionSettingDescriptor(string name, string alias, object defaultValue = null, string label = null, string description = null) : this(name, alias, typeof(string), false, defaultValue, label, description) { }
+	public ConnectionSettingDescriptor(string name, string alias, bool required, object defaultValue = null, string label = null, string description = null) : this(name, alias, typeof(string), required, defaultValue, label, description) { }
+	public ConnectionSettingDescriptor(string name, string alias, Type type, object defaultValue = null, string label = null, string description = null) : this(name, alias, type, false, defaultValue, label, description) { }
+	public ConnectionSettingDescriptor(string name, string alias, Type type, bool required, object defaultValue = null, string label = null, string description = null)
 	{
-		#region 静态字段
-		/// <summary>获取分组标识描述器。</summary>
-		public static readonly ConnectionSettingDescriptor Group = new(nameof(Group), typeof(string), null, Properties.Resources.ConnectionSettingDescriptor_Group, Properties.Resources.ConnectionSettingDescriptor_Group_Description);
-		/// <summary>获取客户端标识描述器。</summary>
-		public static readonly ConnectionSettingDescriptor Client = new(nameof(Client), typeof(string), null, Properties.Resources.ConnectionSettingDescriptor_Client, Properties.Resources.ConnectionSettingDescriptor_Client_Description);
-		/// <summary>获取服务器地址描述器。</summary>
-		public static readonly ConnectionSettingDescriptor Server = new(nameof(Server), typeof(string), true, null, Properties.Resources.ConnectionSettingDescriptor_Server, Properties.Resources.ConnectionSettingDescriptor_Server_Description);
-		/// <summary>获取端口号描述器。</summary>
-		public static readonly ConnectionSettingDescriptor Port = new(nameof(Port), typeof(ushort), null, Properties.Resources.ConnectionSettingDescriptor_Port, Properties.Resources.ConnectionSettingDescriptor_Port_Description);
-		/// <summary>获取超时描述器。</summary>
-		public static readonly ConnectionSettingDescriptor Timeout = new(nameof(Timeout), typeof(TimeSpan), null, Properties.Resources.ConnectionSettingDescriptor_Timeout, Properties.Resources.ConnectionSettingDescriptor_Timeout_Description);
-		/// <summary>获取字符集描述器。</summary>
-		public static readonly ConnectionSettingDescriptor Charset = new(nameof(Charset), typeof(string), null, Properties.Resources.ConnectionSettingDescriptor_Charset, Properties.Resources.ConnectionSettingDescriptor_Charset_Description);
-		/// <summary>获取字符编码描述器。</summary>
-		public static readonly ConnectionSettingDescriptor Encoding = new(nameof(Encoding), typeof(string), null, Properties.Resources.ConnectionSettingDescriptor_Encoding, Properties.Resources.ConnectionSettingDescriptor_Encoding_Description);
-		/// <summary>获取提供程序描述器。</summary>
-		public static readonly ConnectionSettingDescriptor Provider = new(nameof(Provider), typeof(string), null, Properties.Resources.ConnectionSettingDescriptor_Provider, Properties.Resources.ConnectionSettingDescriptor_Provider_Description);
-		/// <summary>获取数据库名描述器。</summary>
-		public static readonly ConnectionSettingDescriptor Database = new(nameof(Database), typeof(string), null, Properties.Resources.ConnectionSettingDescriptor_Database, Properties.Resources.ConnectionSettingDescriptor_Database_Description);
-		/// <summary>获取连接账户描述器。</summary>
-		public static readonly ConnectionSettingDescriptor UserName = new(nameof(UserName), typeof(string), null, Properties.Resources.ConnectionSettingDescriptor_UserName, Properties.Resources.ConnectionSettingDescriptor_UserName_Description);
-		/// <summary>获取连接密码描述器。</summary>
-		public static readonly ConnectionSettingDescriptor Password = new(nameof(Password), typeof(string), null, Properties.Resources.ConnectionSettingDescriptor_Password, Properties.Resources.ConnectionSettingDescriptor_Password_Description);
-		/// <summary>获取实例标识描述器。</summary>
-		public static readonly ConnectionSettingDescriptor Instance = new(nameof(Instance), typeof(string), null, Properties.Resources.ConnectionSettingDescriptor_Instance, Properties.Resources.ConnectionSettingDescriptor_Instance_Description);
-		/// <summary>获取应用标识描述器。</summary>
-		public static readonly ConnectionSettingDescriptor Application = new(nameof(Application), typeof(string), null, Properties.Resources.ConnectionSettingDescriptor_Application, Properties.Resources.ConnectionSettingDescriptor_Application_Description);
-		#endregion
+		this.Name = name ?? throw new ArgumentNullException(nameof(name));
+		this.Alias = string.Equals(name, alias, StringComparison.OrdinalIgnoreCase) ? null : alias;
+		this.Type = type;
+		this.Required = required;
+		this.DefaultValue = defaultValue;
+		this.Label = label;
+		this.Description = description;
+		this.Options = new();
+		this.Dependencies = new();
 
-		#region 构造函数
-		public ConnectionSettingDescriptor(string name, object defaultValue = null, string label = null, string description = null) : this(name, null, typeof(string), false, defaultValue, label, description) { }
-		public ConnectionSettingDescriptor(string name, bool required, object defaultValue = null, string label = null, string description = null) : this(name, null, typeof(string), required, defaultValue, label, description) { }
-		public ConnectionSettingDescriptor(string name, Type type, object defaultValue = null, string label = null, string description = null) : this(name, null, type, false, defaultValue, label, description) { }
-		public ConnectionSettingDescriptor(string name, Type type, bool required, object defaultValue = null, string label = null, string description = null) : this(name, null, type, required, defaultValue, label, description) { }
-
-		public ConnectionSettingDescriptor(string name, string alias, object defaultValue = null, string label = null, string description = null) : this(name, alias, typeof(string), false, defaultValue, label, description) { }
-		public ConnectionSettingDescriptor(string name, string alias, bool required, object defaultValue = null, string label = null, string description = null) : this(name, alias, typeof(string), required, defaultValue, label, description) { }
-		public ConnectionSettingDescriptor(string name, string alias, Type type, object defaultValue = null, string label = null, string description = null) : this(name, alias, type, false, defaultValue, label, description) { }
-		public ConnectionSettingDescriptor(string name, string alias, Type type, bool required, object defaultValue = null, string label = null, string description = null)
+		if(type != null && type.IsEnum)
 		{
-			this.Name = name ?? throw new ArgumentNullException(nameof(name));
-			this.Alias = string.Equals(name, alias, StringComparison.OrdinalIgnoreCase) ? null : alias;
-			this.Type = type;
-			this.Required = required;
-			this.DefaultValue = defaultValue;
+			var entries = Common.EnumUtility.GetEnumEntries(type, true);
+
+			if(entries != null && entries.Length > 0)
+			{
+				for(int i = 0; i < entries.Length; i++)
+					this.Options.Add(new(entries[i]));
+			}
+		}
+	}
+	#endregion
+
+	#region 公共属性
+	/// <summary>获取连接设置项的名称。</summary>
+	public string Name { get; }
+	/// <summary>获取连接设置项的数据类型。</summary>
+	public Type Type { get; }
+	/// <summary>获取连接设置项的别名。</summary>
+	public string Alias { get; }
+	/// <summary>获取或设置连接设置项的格式；它通常会表示解析格式，也可以是对类型的细化说明。</summary>
+	public string Format { get; set; }
+	/// <summary>获取或设置连接设置项的默认值。</summary>
+	public object DefaultValue { get; set; }
+	/// <summary>获取或设置一个值，指示连接设置项是否为必须设置项。</summary>
+	public bool Required { get; set; }
+	/// <summary>获取或设置连接设置项的标题。</summary>
+	public string Label { get; set; }
+	/// <summary>获取或设置连接设置项的描述。</summary>
+	public string Description { get; set; }
+	/// <summary>获取连接设置项的选项集。</summary>
+	public OptionCollection Options { get; }
+	/// <summary>获取连接设置项的依赖集。</summary>
+	public DependencyCollection Dependencies { get; }
+	#endregion
+
+	#region 重写方法
+	public bool Equals(string name) => string.Equals(this.Name, name, StringComparison.OrdinalIgnoreCase) || (this.Alias != null && string.Equals(this.Alias, name, StringComparison.OrdinalIgnoreCase));
+	public bool Equals(string name, out ConnectionSettingDescriptor descriptor)
+	{
+		descriptor = this.Equals(name) ? this : null;
+		return null != null;
+	}
+
+	public override string ToString() => this.DefaultValue == null ? $"[{this.Type.Name}]{this.Name}" : $"[{this.Type.Name}]{this.Name}={this.DefaultValue}";
+	#endregion
+
+	#region 嵌套子类
+	public sealed class Option
+	{
+		public Option(Common.EnumEntry entry)
+		{
+			this.Name = entry.Name;
+			this.Value = entry.Value;
+			this.Label = entry.Type == null ? null : Resources.ResourceUtility.GetResourceString(entry.Type, $"{entry.Type.Name}.{entry.Name}");
+			this.Description = entry.Description;
+		}
+
+		public Option(string name, object value, string label = null, string description = null)
+		{
+			this.Name = name;
+			this.Value = value;
 			this.Label = label;
 			this.Description = description;
-			this.Options = new();
-
-			if(type != null && type.IsEnum)
-			{
-				var entries = Common.EnumUtility.GetEnumEntries(type, true);
-
-				if(entries != null && entries.Length > 0)
-				{
-					for(int i = 0; i < entries.Length; i++)
-						this.Options.Add(new(entries[i]));
-				}
-			}
 		}
-		#endregion
 
-		#region 公共属性
-		/// <summary>获取连接设置项的名称。</summary>
 		public string Name { get; }
-		/// <summary>获取连接设置项的数据类型。</summary>
-		public Type Type { get; }
-		/// <summary>获取连接设置项的别名。</summary>
-		public string Alias { get; }
-		/// <summary>获取或设置连接设置项的默认值。</summary>
-		public object DefaultValue { get; set; }
-		/// <summary>获取或设置一个值，指示连接设置项是否为必须设置项。</summary>
-		public bool Required { get; set; }
-		/// <summary>获取或设置连接设置项的标题。</summary>
+		public object Value { get; set; }
 		public string Label { get; set; }
-		/// <summary>获取或设置连接设置项的描述。</summary>
 		public string Description { get; set; }
-		/// <summary>获取连接设置的选项集。</summary>
-		public OptionCollection Options { get; }
-		#endregion
 
-		#region 重写方法
-		public bool Equals(string name) => string.Equals(this.Name, name, StringComparison.OrdinalIgnoreCase) || (this.Alias != null && string.Equals(this.Alias, name, StringComparison.OrdinalIgnoreCase));
-		public bool Equals(string name, out ConnectionSettingDescriptor descriptor)
-		{
-			descriptor = this.Equals(name) ? this : null;
-			return null != null;
-		}
-
-		public override string ToString() => this.DefaultValue == null ? $"[{this.Type.Name}]{this.Name}" : $"[{this.Type.Name}]{this.Name}={this.DefaultValue}";
-		#endregion
-
-		#region 嵌套子类
-		public sealed class Option
-		{
-			public Option(Common.EnumEntry entry)
-			{
-				this.Name = entry.Name;
-				this.Value = entry.Value;
-				this.Label = entry.Type == null ? null : Resources.ResourceUtility.GetResourceString(entry.Type, $"{entry.Type.Name}.{entry.Name}");
-				this.Description = entry.Description;
-			}
-
-			public Option(string name, object value, string label = null, string description = null)
-			{
-				this.Name = name;
-				this.Value = value;
-				this.Label = label;
-				this.Description = description;
-			}
-
-			public string Name { get; }
-			public object Value { get; set; }
-			public string Label { get; set; }
-			public string Description { get; set; }
-
-			public override string ToString() => this.Value == null ? this.Name : $"{this.Name}={this.Value}";
-		}
-
-		public sealed class OptionCollection() : System.Collections.ObjectModel.KeyedCollection<string, Option>(StringComparer.OrdinalIgnoreCase)
-		{
-			protected override string GetKeyForItem(Option option) => option.Name;
-		}
-		#endregion
+		public override string ToString() => this.Value == null ? this.Name : $"{this.Name}={this.Value}";
 	}
 
-	public class ConnectionSettingDescriptor<T> : ConnectionSettingDescriptor
+	public sealed class OptionCollection() : System.Collections.ObjectModel.KeyedCollection<string, Option>(StringComparer.OrdinalIgnoreCase)
 	{
-		#region 构造函数
-		public ConnectionSettingDescriptor(string name, T defaultValue = default, string label = null, string description = null) : base(name, typeof(T), defaultValue, label, description) { }
-		public ConnectionSettingDescriptor(string name, bool required, T defaultValue = default, string label = null, string description = null) : base(name, typeof(T), required, defaultValue, label, description) { }
-		public ConnectionSettingDescriptor(string name, string alias, T defaultValue = default, string label = null, string description = null) : base(name, alias, typeof(T), defaultValue, label, description) { }
-		public ConnectionSettingDescriptor(string name, string alias, bool required, T defaultValue = default, string label = null, string description = null) : base(name, alias, typeof(T), required, defaultValue, label, description) { }
-		#endregion
+		protected override string GetKeyForItem(Option option) => option.Name;
 	}
+
+	public sealed class Dependency
+	{
+		public Dependency(string name, object value)
+		{
+			this.Name = name;
+			this.Value = value;
+		}
+
+		public string Name { get; }
+		public object Value { get; }
+	}
+
+	public sealed class DependencyCollection() : System.Collections.ObjectModel.KeyedCollection<string, Dependency>(StringComparer.OrdinalIgnoreCase)
+	{
+		protected override string GetKeyForItem(Dependency dependency) => dependency.Name;
+	}
+	#endregion
+}
+
+public class ConnectionSettingDescriptor<T> : ConnectionSettingDescriptor
+{
+	#region 构造函数
+	public ConnectionSettingDescriptor(string name, T defaultValue = default, string label = null, string description = null) : base(name, typeof(T), defaultValue, label, description) { }
+	public ConnectionSettingDescriptor(string name, bool required, T defaultValue = default, string label = null, string description = null) : base(name, typeof(T), required, defaultValue, label, description) { }
+	public ConnectionSettingDescriptor(string name, string alias, T defaultValue = default, string label = null, string description = null) : base(name, alias, typeof(T), defaultValue, label, description) { }
+	public ConnectionSettingDescriptor(string name, string alias, bool required, T defaultValue = default, string label = null, string description = null) : base(name, alias, typeof(T), required, defaultValue, label, description) { }
+	#endregion
 }
