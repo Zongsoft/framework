15cbb657a538ae6e7d4d7b4aa20b7595310442d3
PopeyeZhong <9555843@qq.com>
2025-01-06 19:21:23 +0800

    添加 Zongsoft.Diagnostics 项目。 🎉

---
+++ Zongsoft.Diagnostics/build.cake
@@ -0,0 +1,58 @@
+var target = Argument("target", "default");
+
+var solutionFile  = "Zongsoft.Diagnostics.sln";
+
+Task("clean")
+	.Description("清理解决方案")
+	.Does(() =>
+{
+	DeleteFiles("**/*.nupkg");
+	CleanDirectories("**/bin");
+	CleanDirectories("**/obj");
+});
+
+Task("restore")
+	.Description("还原项目依赖")
+	.Does(() =>
+{
+	DotNetRestore(solutionFile);
+});
+
+Task("build")
+	.Description("编译项目")
+	.IsDependentOn("clean")
+	.IsDependentOn("restore")
+	.Does(() =>
+{
+	var settings = new DotNetBuildSettings
+	{
+		NoRestore = true
+	};
+
+	DotNetBuild(solutionFile, settings);
+});
+
+Task("test")
+	.Description("单元测试")
+	.IsDependentOn("build")
+	.Does(() =>
+{
+	var settings = new DotNetTestSettings
+	{
+		NoRestore = true,
+		NoBuild = true
+	};
+
+	var projects = GetFiles("**/test/*.csproj");
+
+	foreach(var project in projects)
+	{
+		DotNetTest(project.FullPath, settings);
+	}
+});
+
+Task("default")
+	.Description("默认")
+	.IsDependentOn("test");
+
+RunTarget(target);
---
+++ Zongsoft.Diagnostics/src/Initializer.cs
@@ -0,0 +1,99 @@
+﻿/*
+ *   _____                                ______
+ *  /_   /  ____  ____  ____  _________  / __/ /_
+ *    / /  / __ \/ __ \/ __ \/ ___/ __ \/ /_/ __/
+ *   / /__/ /_/ / / / / /_/ /\_ \/ /_/ / __/ /_
+ *  /____/\____/_/ /_/\__  /____/\____/_/  \__/
+ *                   /____/
+ *
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@qq.com>
+ *
+ * Copyright (C) 2020-2025 Zongsoft Studio <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.Diagnostics library.
+ *
+ * The Zongsoft.Diagnostics is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3.0 of the License,
+ * or (at your option) any later version.
+ *
+ * The Zongsoft.Diagnostics is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Zongsoft.Diagnostics library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using OpenTelemetry;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Trace;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Extensions;
+
+using Zongsoft.Services;
+
+namespace Zongsoft.Diagnostics;
+
+[Service<IApplicationInitializer>(Members = nameof(Instance))]
+public sealed class Initializer : IApplicationInitializer, IServiceRegistration
+{
+	public static readonly Initializer Instance = new();
+
+	private Initializer()
+	{
+		this.Meters = new HashSet<string>();
+		this.Tracers = new HashSet<string>();
+		this.Exporters = new HashSet<string>();
+	}
+
+	public ICollection<string> Meters { get; }
+	public ICollection<string> Tracers { get; }
+	public ICollection<string> Exporters { get; }
+
+	public void Initialize(IApplicationContext context)
+	{
+		var meters = Sdk.CreateMeterProviderBuilder();
+		var tracers = Sdk.CreateTracerProviderBuilder();
+
+		this.Initialize(meters);
+		this.Initialize(tracers);
+	}
+
+	private void Initialize(MeterProviderBuilder meters)
+	{
+		if(this.Meters.Count > 0)
+			meters.AddMeter(this.Meters.ToArray());
+	}
+
+	private void Initialize(TracerProviderBuilder tracers)
+	{
+		if(this.Tracers.Count > 0)
+			tracers.AddSource(this.Tracers.ToArray());
+	}
+
+	private void Initialize(LoggerProviderBuilder loggers)
+	{
+	}
+
+	public void Register(IServiceCollection services, IConfiguration configuration)
+	{
+		services.AddOpenTelemetry()
+			.WithMetrics(this.Initialize)
+			.WithTracing(this.Initialize)
+			.WithLogging(this.Initialize);
+	}
+}
