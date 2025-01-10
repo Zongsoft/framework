5ff3cef65fe2f6c2ab0ee606ff4caad1868c6998
PopeyeZhong <9555843@qq.com>
2025-01-10 18:47:19 +0800

    重构XML(.option)配置文件解析器。 🌏

--- Zongsoft.Core/src/Configuration/Xml/XmlStreamConfigurationProvider.cs
+++ Zongsoft.Core/src/Configuration/Xml/XmlStreamConfigurationProvider.cs
@@ -65,161 +65,60 @@ namespace Zongsoft.Configuration.Xml
 		#region 公共方法
 		public static IDictionary<string, string> Read(Stream stream)
 		{
-			var data = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
-
-			var settings = new XmlReaderSettings()
+			using var reader = XmlReader.Create(stream, new XmlReaderSettings()
 			{
 				CloseInput = false,
 				IgnoreComments = true,
 				IgnoreWhitespace = true
-			};
+			});
 
-			using(var reader = XmlReader.Create(stream, settings))
+			//移动到根节点
+			SkipUntilRootElement(reader);
+
+			//确认根节点的名称是否合法
+			if(reader.LocalName != "configuration" && reader.LocalName != "options")
+				throw new FormatException(string.Format(Properties.Resources.Error_IllegalRootNodeName, reader.LocalName, GetLineInfo(reader)));
+
+			var context = new Context(reader);
+
+			while(reader.Read())
 			{
-				var pathStack = new Stack<string>();
-				var indexStack = new Stack<int>();
-
-				SkipUntilRootElement(reader);
-
-				//确认根节点的名称是否合法
-				if(reader.LocalName != "configuration" && reader.LocalName != "options")
-					throw new FormatException(string.Format(Properties.Resources.Error_IllegalRootNodeName, reader.LocalName, GetLineInfo(reader)));
-
-				var preNodeType = reader.NodeType;
-
-				while(reader.Read())
+				switch(reader.NodeType)
 				{
-					switch(reader.NodeType)
-					{
-						case XmlNodeType.Element:
-							if(indexStack.Count < reader.Depth)
-								indexStack.Push(0);
+					case XmlNodeType.Element:
+						if(context.Indexes.Count < context.Depth)
+							context.Indexes.Push(0);
 
-							if(reader.Depth == 1)
-							{
-								//确保配置元素必须位于<option>节点之内
-								if(reader.LocalName != XML_OPTION_ELEMENT)
-									throw new FormatException(string.Format(Properties.Resources.Error_InvalidOptionConfigurationFileFormat, GetLineInfo(reader)));
+						if(reader.Depth == 1)
+							context.DoOption();
+						else
+							context.DoElement();
 
-								pathStack.Clear();
-
-								var path = reader.GetAttribute(XML_OPTION_PATH_ATTRIBUTE);
-
-								if(!string.IsNullOrWhiteSpace(path))
-								{
-									path = path.Trim('/', ' ', '\t');
-
-									if(path.Length > 0)
-										pathStack.Push(path.Replace("/", ConfigurationPath.KeyDelimiter));
-								}
-							}
-							else
-							{
-								var elementName = reader.LocalName;
-								pathStack.Push(elementName);
-
-								for(int i = 0; i < reader.AttributeCount; i++)
-								{
-									var index = 0;
-									reader.MoveToAttribute(i);
-
-									if(!string.IsNullOrEmpty(reader.NamespaceURI))
-										throw new FormatException(string.Format(Properties.Resources.Error_NamespaceIsNotSupported, GetLineInfo(reader)));
-
-									if(i == 0 &&
-									   string.Equals(reader.LocalName, elementName + "." + XML_KEY_ATTRIBUTE, StringComparison.OrdinalIgnoreCase) ||
-									   string.Equals(reader.LocalName, elementName + "." + XML_NAME_ATTRIBUTE, StringComparison.OrdinalIgnoreCase))
-									{
-										if(reader.Value.IndexOfAny(ILLEGAL_CHARACTERS) >= 0)
-											throw new FormatException(string.Format(Properties.Resources.Error_IllegalConfigurationKeyValue, reader.Value, GetLineInfo(reader)));
-
-										pathStack.Pop();
-
-										if(string.IsNullOrWhiteSpace(reader.Value) || reader.Value == "#")
-										{
-											index = indexStack.Pop() + 1;
-											pathStack.Push($"#{index}");
-											indexStack.Push(index);
-										}
-										else
-											pathStack.Push(reader.Value);
-
-										pathStack.Push(reader.LocalName.Substring(elementName.Length + 1));
-									}
-									else
-									{
-										if(reader.LocalName.Contains('.'))
-											throw new FormatException(string.Format(Properties.Resources.Error_IllegalConfigurationAttributeName, reader.LocalName, GetLineInfo(reader)));
-
-										pathStack.Push(reader.LocalName);
-									}
-
-									var key = ConfigurationPath.Combine(pathStack.Reverse());
-									if(data.ContainsKey(key))
-										throw new FormatException(string.Format(Properties.Resources.Error_KeyIsDuplicated, key, GetLineInfo(reader)));
-
-									if(index > 0)
-										data[key] = index.ToString();
-									else
-										data[key] = reader.Value;
-
-									pathStack.Pop();
-								}
-
-								reader.MoveToElement();
-
-								if(reader.IsEmptyElement)
-									pathStack.Pop();
-							}
-
-							break;
-						case XmlNodeType.EndElement:
-							if(pathStack.Count > 0)
-							{
-								// 如果上一个节点类型是元素则说明当前元素中没有TEXT/CDATA节点
-								if(preNodeType == XmlNodeType.Element)
-								{
-									var key = ConfigurationPath.Combine(pathStack.Reverse());
-									data[key] = string.Empty;
-								}
-
-								pathStack.Pop();
-							}
-
-							if(indexStack.Count > 0)
-								indexStack.Pop();
-
-							break;
-						case XmlNodeType.CDATA:
-						case XmlNodeType.Text:
-							{
-								var key = ConfigurationPath.Combine(pathStack.Reverse());
-
-								if(data.ContainsKey(key))
-									throw new FormatException(string.Format(Properties.Resources.Error_KeyIsDuplicated, key, GetLineInfo(reader)));
-
-								data[key] = reader.Value;
-							}
-
-							break;
-						case XmlNodeType.XmlDeclaration:
-						case XmlNodeType.ProcessingInstruction:
-						case XmlNodeType.Comment:
-						case XmlNodeType.Whitespace:
-							break;
-						default:
-							throw new FormatException(string.Format(Properties.Resources.Error_UnSupportedNodeType, reader.NodeType, GetLineInfo(reader)));
-					}
-
-					preNodeType = reader.NodeType;
-
-					if(preNodeType == XmlNodeType.Element && reader.IsEmptyElement)
-						preNodeType = XmlNodeType.EndElement;
+						break;
+					case XmlNodeType.EndElement:
+						context.DoElementEnd();
+						break;
+					case XmlNodeType.CDATA:
+					case XmlNodeType.Text:
+						context.DoText();
+						break;
+					case XmlNodeType.XmlDeclaration:
+					case XmlNodeType.ProcessingInstruction:
+					case XmlNodeType.Comment:
+					case XmlNodeType.Whitespace:
+						break;
+					default:
+						throw new FormatException(string.Format(Properties.Resources.Error_UnSupportedNodeType, reader.NodeType, GetLineInfo(reader)));
 				}
+
+				context.Next();
 			}
 
-			return data;
+			return context.Data;
 		}
+		#endregion
+
+		#region 解析方法
 		#endregion
 
 		#region 私有方法
@@ -240,5 +139,149 @@ namespace Zongsoft.Configuration.Xml
 			return string.Empty;
 		}
 		#endregion
+
+		private partial struct Context
+		{
+			public Context(XmlReader reader)
+			{
+				this.Reader = reader;
+				this.Previous = reader.NodeType;
+				this.Data = new(StringComparer.OrdinalIgnoreCase);
+				this.Paths = new();
+				this.Indexes = new();
+			}
+
+			public readonly Dictionary<string, string> Data;
+			public readonly XmlReader Reader;
+			public readonly Stack<string> Paths;
+			public readonly Stack<int> Indexes;
+
+			public XmlNodeType Previous;
+			public readonly int Depth => this.Reader.Depth;
+
+			public void Next()
+			{
+				if(this.Reader.NodeType == XmlNodeType.Element && this.Reader.IsEmptyElement)
+					this.Previous = XmlNodeType.EndElement;
+				else
+					this.Previous = this.Reader.NodeType;
+			}
+
+			public readonly void Clear() { this.Paths.Clear(); this.Indexes.Clear(); }
+			public readonly void Indent(string path) => this.Paths.Push(path);
+			public readonly string Dedent() => this.Paths.Pop();
+
+			public readonly int Increase()
+			{
+				var index = this.Indexes.Pop() + 1;
+				this.Paths.Push($"#{index}");
+				this.Indexes.Push(index);
+				return index;
+			}
+		}
+
+		partial struct Context
+		{
+			public readonly void DoOption()
+			{
+				//确保配置元素必须位于<option>节点之内
+				if(this.Reader.LocalName != XML_OPTION_ELEMENT)
+					throw new FormatException(string.Format(Properties.Resources.Error_InvalidOptionConfigurationFileFormat, GetLineInfo(this.Reader)));
+
+				this.Clear();
+				var path = this.Reader.GetAttribute(XML_OPTION_PATH_ATTRIBUTE);
+
+				if(!string.IsNullOrWhiteSpace(path))
+				{
+					path = path.Trim('/', ' ', '\t');
+
+					if(path.Length > 0)
+						this.Indent(path.Replace("/", ConfigurationPath.KeyDelimiter));
+				}
+			}
+
+			public readonly void DoElement()
+			{
+				var elementName = this.Reader.LocalName;
+				this.Indent(elementName);
+
+				for(int i = 0; i < this.Reader.AttributeCount; i++)
+				{
+					var index = 0;
+					this.Reader.MoveToAttribute(i);
+
+					if(!string.IsNullOrEmpty(this.Reader.NamespaceURI))
+						throw new FormatException(string.Format(Properties.Resources.Error_NamespaceIsNotSupported, GetLineInfo(this.Reader)));
+
+					if(i == 0 &&
+					   string.Equals(this.Reader.LocalName, elementName + "." + XML_KEY_ATTRIBUTE, StringComparison.OrdinalIgnoreCase) ||
+					   string.Equals(this.Reader.LocalName, elementName + "." + XML_NAME_ATTRIBUTE, StringComparison.OrdinalIgnoreCase))
+					{
+						if(this.Reader.Value.IndexOfAny(ILLEGAL_CHARACTERS) >= 0)
+							throw new FormatException(string.Format(Properties.Resources.Error_IllegalConfigurationKeyValue, this.Reader.Value, GetLineInfo(this.Reader)));
+
+						this.Dedent();
+
+						if(string.IsNullOrWhiteSpace(this.Reader.Value) || this.Reader.Value == "#")
+							index = this.Increase();
+						else
+							this.Indent(this.Reader.Value);
+
+						this.Indent(this.Reader.LocalName.Substring(elementName.Length + 1));
+					}
+					else
+					{
+						if(this.Reader.LocalName.Contains('.'))
+							throw new FormatException(string.Format(Properties.Resources.Error_IllegalConfigurationAttributeName, this.Reader.LocalName, GetLineInfo(this.Reader)));
+
+						this.Indent(this.Reader.LocalName);
+					}
+
+					var key = ConfigurationPath.Combine(this.Paths.Reverse());
+					if(this.Data.ContainsKey(key))
+						throw new FormatException(string.Format(Properties.Resources.Error_KeyIsDuplicated, key, GetLineInfo(this.Reader)));
+
+					if(index > 0)
+						this.Data[key] = index.ToString();
+					else
+						this.Data[key] = this.Reader.Value;
+
+					this.Dedent();
+				}
+
+				this.Reader.MoveToElement();
+
+				if(this.Reader.IsEmptyElement)
+					this.Dedent();
+			}
+
+			public readonly void DoElementEnd()
+			{
+				if(this.Paths.Count > 0)
+				{
+					// 如果上一个节点类型是元素则说明当前元素中没有TEXT/CDATA节点
+					if(this.Previous == XmlNodeType.Element)
+					{
+						var key = ConfigurationPath.Combine(this.Paths.Reverse());
+						this.Data[key] = string.Empty;
+					}
+
+					this.Dedent();
+				}
+
+				if(this.Indexes.Count > 0)
+					this.Indexes.Pop();
+			}
+
+			public readonly void DoText()
+			{
+				var key = ConfigurationPath.Combine(this.Paths.Reverse());
+
+				if(this.Data.ContainsKey(key))
+					throw new FormatException(string.Format(Properties.Resources.Error_KeyIsDuplicated, key, GetLineInfo(this.Reader)));
+
+				this.Data[key] = this.Reader.Value;
+			}
+		}
 	}
 }
