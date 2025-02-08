944a3ad3b00982f9bdec26eac6eb661b0575d875
PopeyeZhong <9555843@qq.com>
2025-02-08 16:31:12 +0800

    重构异常过滤器。 💼

--- Zongsoft.Web/src/Filters/ExceptionFilter.cs
+++ Zongsoft.Web/src/Filters/ExceptionFilter.cs
@@ -9,7 +9,7 @@
  * Authors:
  *   钟峰(Popeye Zhong) <zongsoft@qq.com>
  *
- * Copyright (C) 2010-2020 Zongsoft Studio <http://www.zongsoft.com>
+ * Copyright (C) 2010-2025 Zongsoft Studio <http://www.zongsoft.com>
  *
  * This file is part of Zongsoft.Web library.
  *
@@ -28,112 +28,122 @@
  */
 
 using System;
+using System.Collections.Generic;
 
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 using Zongsoft.Data;
 using Zongsoft.Common;
 using Zongsoft.Security;
 using Zongsoft.Security.Membership;
 
-namespace Zongsoft.Web.Filters
+namespace Zongsoft.Web.Filters;
+
+public class ExceptionFilter : IExceptionFilter
 {
-	public class ExceptionFilter : IExceptionFilter
+	public void OnException(ExceptionContext context)
 	{
-		public void OnException(ExceptionContext context)
+		switch(context.Exception)
 		{
-			ValidationProblemDetails problem;
+			case AuthenticationException authentication:
+				context.HttpContext.Response.Headers.Append("X-Security-Error", authentication.Reason);
+				context.Result = new ObjectResult(GetProblem(context, StatusCodes.Status401Unauthorized, authentication.Reason));
+				break;
+			case SecurityException securityException:
+				context.HttpContext.Response.Headers.Append("X-Security-Error", securityException.Reason);
+				context.Result = new ObjectResult(GetProblem(context, StatusCodes.Status403Forbidden, securityException.Reason));
+				break;
+			case OperationException operationException when operationException.IsArgument:
+				context.Result = new ObjectResult(GetProblem(context, StatusCodes.Status400BadRequest));
+				break;
+			case OperationException operationException when operationException.IsUnfound:
+				context.Result = new ObjectResult(GetProblem(context, StatusCodes.Status404NotFound));
+				break;
+			case OperationException operationException when operationException.IsUnsatisfied:
+				context.Result = new ObjectResult(GetProblem(context, StatusCodes.Status412PreconditionFailed));
+				break;
+			case OperationException operationException when operationException.IsUnprocessed:
+				context.Result = new ObjectResult(GetProblem(context, StatusCodes.Status406NotAcceptable));
+				break;
+			case OperationException operationException when operationException.IsUnsupported:
+				context.Result = new ObjectResult(GetProblem(context, StatusCodes.Status405MethodNotAllowed));
+				break;
+			case DataArgumentException argumentException:
+				if(!string.IsNullOrEmpty(argumentException.Name))
+					context.ModelState.AddModelError(argumentException.Name, argumentException.Message);
 
-			switch(context.Exception)
-			{
-				case AuthenticationException authentication:
-					context.HttpContext.Response.Headers.Append("X-Security-Error", authentication.Reason.ToString());
-					context.Result = new ObjectResult(new ValidationProblemDetails(context.ModelState)
-					{
-						Title = authentication.Reason,
-						Detail = authentication.Message,
-						Status = StatusCodes.Status401Unauthorized,
-					});
+				context.Result = new ObjectResult(GetProblem(context, StatusCodes.Status400BadRequest));
+				break;
+			case DataConflictException conflictException:
+				if(!string.IsNullOrEmpty(conflictException.Key))
+					context.ModelState.AddModelError(conflictException.Key, conflictException.Message);
 
-					break;
-				case SecurityException securityException:
-					context.HttpContext.Response.Headers.Append("X-Security-Error", securityException.Reason.ToString());
-					context.Result = new ObjectResult(new ValidationProblemDetails(context.ModelState)
-					{
-						Title = securityException.Reason,
-						Detail = securityException.Message,
-						Status = StatusCodes.Status403Forbidden,
-					});
+				var problem = GetProblem(context, StatusCodes.Status409Conflict);
 
-					break;
-				case OperationException operationException when operationException.IsArgument:
-					context.Result = new BadRequestResult();
-					break;
-				case OperationException operationException when operationException.IsUnfound:
-					context.Result = new NotFoundResult();
-					break;
-				case OperationException operationException when operationException.IsUnsatisfied:
-					context.Result = new StatusCodeResult(StatusCodes.Status412PreconditionFailed);
-					break;
-				case OperationException operationException when operationException.IsUnprocessed:
-					context.Result = new StatusCodeResult(StatusCodes.Status406NotAcceptable);
-					break;
-				case OperationException operationException when operationException.IsUnsupported:
-					context.Result = new StatusCodeResult(StatusCodes.Status405MethodNotAllowed);
-					break;
-				case DataArgumentException argumentException:
-					if(!string.IsNullOrEmpty(argumentException.Name))
-						context.ModelState.AddModelError(argumentException.Name, argumentException.Message);
+				if(conflictException.Fields != null && conflictException.Fields.Length > 0)
+					problem.Extensions.Add(nameof(DataConflictException.Fields), conflictException.Fields);
 
-					context.Result = new BadRequestObjectResult(new ValidationProblemDetails(context.ModelState)
-					{
-						Detail = argumentException.Message
-					});
+				context.Result = new ObjectResult(problem);
+				break;
+			case DataConstraintException constraintException:
+				if(!string.IsNullOrEmpty(constraintException.Field))
+					context.ModelState.AddModelError(constraintException.Field, constraintException.Message);
 
-					break;
-				case DataConflictException conflictException:
-					if(!string.IsNullOrEmpty(conflictException.Key))
-						context.ModelState.AddModelError(conflictException.Key, conflictException.Message);
-
-					problem = new ValidationProblemDetails(context.ModelState)
-					{
-						Detail = conflictException.Message,
-					};
-
-					if(conflictException.Fields != null && conflictException.Fields.Length > 0)
-						problem.Extensions.Add(nameof(DataConflictException.Fields), conflictException.Fields);
-
-					context.Result = new ConflictObjectResult(problem);
-					break;
-				case DataConstraintException constraintException:
-					if(!string.IsNullOrEmpty(constraintException.Field))
-						context.ModelState.AddModelError(constraintException.Field, constraintException.Message);
-
-					context.Result = new ObjectResult(new ValidationProblemDetails(context.ModelState)
-					{
-						Status = StatusCodes.Status409Conflict,
-						Detail = constraintException.Message
-					});
-
-					break;
-				case DataOperationException operationException:
-					context.Result = new ObjectResult(new ValidationProblemDetails(context.ModelState)
-					{
-						Status = StatusCodes.Status412PreconditionFailed,
-						Detail = operationException.Message,
-					});
-
-					break;
-				case NotSupportedException:
-				case NotImplementedException:
-					context.Result = new StatusCodeResult(StatusCodes.Status501NotImplemented);
-					break;
-				default:
-					Zongsoft.Diagnostics.Logger.GetLogger(this).Error(context.Exception);
-					break;
-			}
+				context.Result = new ObjectResult(GetProblem(context, StatusCodes.Status409Conflict));
+				break;
+			case DataOperationException:
+				context.Result = new ObjectResult(GetProblem(context, StatusCodes.Status412PreconditionFailed));
+				break;
+			case NotSupportedException:
+			case NotImplementedException:
+				context.Result = new ObjectResult(GetProblem(context, StatusCodes.Status501NotImplemented));
+				break;
+			default:
+				context.Result = new ObjectResult(GetProblem(context, StatusCodes.Status500InternalServerError));
+				Zongsoft.Diagnostics.Logger.GetLogger(this).Error(context.Exception);
+				break;
 		}
+	}
+
+	private static ProblemDetails GetProblem(ExceptionContext context, int status, params ReadOnlySpan<KeyValuePair<string, object>> extras) => GetProblem(context, status, null, extras);
+	private static ProblemDetails GetProblem(ExceptionContext context, int status, string title, params ReadOnlySpan<KeyValuePair<string, object>> extras)
+	{
+		var errors = context.ModelState;
+		var factory = (ProblemDetailsFactory)context.HttpContext.RequestServices.GetService(typeof(ProblemDetailsFactory));
+
+		var result = factory != null ?
+		(
+			errors == null || errors.IsValid ?
+			factory.CreateProblemDetails(context.HttpContext, status, title, context.Exception.GetType().Name, context.Exception.Message) :
+			factory.CreateValidationProblemDetails(context.HttpContext, errors, status, title, context.Exception.GetType().Name, context.Exception.Message)
+		):
+		(
+			errors == null || errors.IsValid ?
+			new ProblemDetails()
+			{
+				Status = status,
+				Title = title,
+				Type = context.Exception.GetType().Name,
+				Detail = context.Exception.Message,
+			} :
+			new ValidationProblemDetails(errors)
+			{
+				Status = status,
+				Title = title,
+				Type = context.Exception.GetType().Name,
+				Detail = context.Exception.Message,
+			}
+		);
+
+		if(extras != null && extras.Length > 0)
+		{
+			foreach(var extra in extras)
+				result.Extensions.Add(extra.Key, extra.Value);
+		}
+
+		return result;
 	}
 }
