479703eaca90bb19e7caa31106e734cc25844d8f
PopeyeZhong <9555843@qq.com>
2025-03-12 10:09:19 +0800

    添加 Zongsoft.Web.OpenApi 项目。 🐷

---
+++ Zongsoft.Web/api/Transformers/DocumentTransformer.cs
@@ -0,0 +1,101 @@
+﻿/*
+ *   _____                                ______
+ *  /_   /  ____  ____  ____  _________  / __/ /_
+ *    / /  / __ \/ __ \/ __ \/ ___/ __ \/ /_/ __/
+ *   / /__/ /_/ / / / / /_/ /\_ \/ /_/ / __/ /_
+ *  /____/\____/_/ /_/\__  /____/\____/_/  \__/
+ *                   /____/
+ *
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@qq.com>
+ *
+ * Copyright (C) 2020-2025 Zongsoft Studio <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.Web.OpenApi library.
+ *
+ * The Zongsoft.Web.OpenApi is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3.0 of the License,
+ * or (at your option) any later version.
+ *
+ * The Zongsoft.Web.OpenApi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Zongsoft.Web.OpenApi library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Zongsoft.Web.OpenApi.Transformers;
+
+public class DocumentTransformer : IOpenApiDocumentTransformer
+{
+	public Task TransformAsync(OpenApiDocument document, OpenApiDocumentTransformerContext context, CancellationToken cancellation)
+	{
+		var server = new OpenApiServer()
+		{
+			Url = "http://{host}:{port}",
+			Description = "for development."
+		};
+
+		server.Variables.Add("host", new OpenApiServerVariable()
+		{
+			Default = "localhost",
+			Enum = ["localhost", "127.0.0.1"],
+			Description = "",
+		});
+
+		server.Variables.Add("port", new OpenApiServerVariable()
+		{
+			Default = "8069",
+			Enum = ["8069", "80", "81", "82", "83", "84", "85"],
+			Description = "",
+		});
+
+		document.Servers.Add(server);
+
+		server = new OpenApiServer()
+		{
+			Url = "http://{site}.{host}:{port}",
+			Description = "for stage or production."
+		};
+
+		server.Variables.Add("site", new OpenApiServerVariable()
+		{
+			Default = "b",
+			Enum = ["a", "b", "c", "gateway", "iot"],
+			Description = "",
+		});
+
+		server.Variables.Add("host", new OpenApiServerVariable()
+		{
+			Default = "localhost",
+			Enum = ["localhost", "127.0.0.1"],
+			Description = "",
+		});
+
+		server.Variables.Add("port", new OpenApiServerVariable()
+		{
+			Default = "80",
+			Enum = ["80", "88", "8080", "8088"],
+			Description = "",
+		});
+
+		document.Servers.Add(server);
+
+		return Task.CompletedTask;
+	}
+}
---
+++ Zongsoft.Web/api/WebInitializer.cs
@@ -0,0 +1,144 @@
+﻿/*
+ *   _____                                ______
+ *  /_   /  ____  ____  ____  _________  / __/ /_
+ *    / /  / __ \/ __ \/ __ \/ ___/ __ \/ /_/ __/
+ *   / /__/ /_/ / / / / /_/ /\_ \/ /_/ / __/ /_
+ *  /____/\____/_/ /_/\__  /____/\____/_/  \__/
+ *                   /____/
+ *
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@qq.com>
+ *
+ * Copyright (C) 2020-2025 Zongsoft Studio <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.Web.OpenApi library.
+ *
+ * The Zongsoft.Web.OpenApi is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3.0 of the License,
+ * or (at your option) any later version.
+ *
+ * The Zongsoft.Web.OpenApi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Zongsoft.Web.OpenApi library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Schema;
+using System.Text.Json.Serialization;
+using System.Text.Json.Serialization.Metadata;
+
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using Scalar.AspNetCore;
+
+using Zongsoft.Common;
+using Zongsoft.Services;
+using Zongsoft.Components;
+
+namespace Zongsoft.Web.OpenApi;
+
+[Service<IApplicationInitializer<IApplicationBuilder>>]
+public class WebInitializer : IApplicationInitializer<IApplicationBuilder>
+{
+	#region 初始方法
+	public void Initialize(IApplicationBuilder builder)
+	{
+		if(builder is IEndpointRouteBuilder app)
+		{
+			app.MapOpenApi();
+			app.MapScalarApiReference(options => { });
+		}
+	}
+	#endregion
+
+	#region 服务注册
+	public sealed class Registration : IServiceRegistration
+	{
+		public void Register(IServiceCollection services, IConfiguration configuration)
+		{
+			services.Configure<JsonOptions>(options =>
+			{
+				options.JsonSerializerOptions.TypeInfoResolverChain.Insert(0, new JsonTypeResolver());
+				options.JsonSerializerOptions.MaxDepth = 10;
+				options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+			});
+
+			services.AddOpenApi(options =>
+			{
+				options.ShouldInclude = OnShouldInclude;
+				options.CreateSchemaReferenceId = CreateReference;
+				options.AddSchemaTransformer(new Transformers.SchemaTransformer());
+				options.AddDocumentTransformer(new Transformers.DocumentTransformer());
+				options.AddOperationTransformer(new Transformers.OperationTransformer());
+			});
+		}
+
+		private static string CreateReference(JsonTypeInfo info)
+		{
+			if(!info.Options.IsReadOnly)
+			{
+				info.Options.MaxDepth = 32;
+				info.Options.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+			}
+
+			return info.Kind == JsonTypeInfoKind.Enumerable ? info.ElementType.GetAlias() : info.Type.GetAlias();
+		}
+
+		private static bool OnShouldInclude(ApiDescription api)
+		{
+			if(api.ActionDescriptor is Microsoft.AspNetCore.Mvc.Controllers.ControllerActionDescriptor action)
+			{
+				if(action.MethodInfo.ReturnType.IsGenericType)
+				{
+					if(action.MethodInfo.ReturnType.GetGenericTypeDefinition() == typeof(IAsyncEnumerable<>) || action.MethodInfo.ReturnType.GetGenericTypeDefinition() == typeof(IEnumerable<>))
+					{
+						if(action.MethodInfo.ReturnType.GenericTypeArguments[0].IsAbstract)
+							return false;
+					}
+					else if(action.MethodInfo.ReturnType.GetGenericTypeDefinition() == typeof(IDictionary<,>))
+					{
+						if(action.MethodInfo.ReturnType.GenericTypeArguments[1].IsAbstract)
+							return false;
+					}
+				}
+			}
+
+			if(api.ActionDescriptor != null && api.ActionDescriptor.Parameters != null)
+			{
+				foreach(var parameter in api.ActionDescriptor.Parameters)
+				{
+					if(parameter.ParameterType.IsAbstract)
+						return false;
+				}
+			}
+
+			return true;
+		}
+
+		public class JsonTypeResolver : IJsonTypeInfoResolver
+		{
+			public JsonTypeInfo GetTypeInfo(Type type, JsonSerializerOptions options)
+			{
+				options.MaxDepth = 10;
+				options.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+				return JsonTypeInfo.CreateJsonTypeInfo(type, options);
+			}
+		}
+	}
+	#endregion
+}
