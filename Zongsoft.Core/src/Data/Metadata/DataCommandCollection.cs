b9f2a7d90b50aa4dcd46a5be0fa5becf64c09bb3
PopeyeZhong <9555843@qq.com>
2025-06-23 20:03:49 +0800

    为数据实体集合和数据命令集合添加反向查找的功能。 🐀

--- Zongsoft.Core/src/Data/Metadata/DataCommandCollection.cs
+++ Zongsoft.Core/src/Data/Metadata/DataCommandCollection.cs
@@ -28,12 +28,17 @@
  */
 
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 
 namespace Zongsoft.Data.Metadata;
 
 public class DataCommandCollection() : KeyedCollection<string, IDataCommand>(StringComparer.OrdinalIgnoreCase)
 {
+	#region 成员字段
+	private readonly Dictionary<string, IDataCommand[]> _aliases = new();
+	#endregion
+
 	#region 公共属性
 	public IDataCommand this[string name, string @namespace = null] => base[GetKey(name, @namespace)];
 	#endregion
@@ -54,10 +59,83 @@ public class DataCommandCollection() : KeyedCollection<string, IDataCommand>(Str
 	public bool Contains(string name, string @namespace = null) => base.Contains(GetKey(name, @namespace));
 	public bool Remove(string name, string @namespace = null) => base.Remove(GetKey(name, @namespace));
 	public bool TryGetValue(string name, string @namespace, out IDataCommand value) => base.TryGetValue(GetKey(name, @namespace), out value);
+
+	/// <summary>根据别名查找对应的数据命令。</summary>
+	/// <param name="alias">指定要查找的数据命令别名。</param>
+	/// <returns>返回找到的数据命令数组，如果为空则表示查找失败。</returns>
+	public IDataCommand[] Find(string alias)
+	{
+		if(string.IsNullOrEmpty(alias))
+			return null;
+
+		return _aliases.TryGetValue(alias, out var commands) ? commands : base.TryGetValue(alias, out var command) ? [command] : null;
+	}
 	#endregion
 
 	#region 重写方法
 	protected override string GetKeyForItem(IDataCommand command) => command.QualifiedName;
+
+	protected override void ClearItems()
+	{
+		base.ClearItems();
+		_aliases.Clear();
+	}
+
+	protected override void InsertItem(int index, IDataCommand command)
+	{
+		if(command == null)
+			return;
+
+		base.InsertItem(index, command);
+
+		if(!string.IsNullOrEmpty(command.Alias))
+		{
+			if(!_aliases.TryAdd(command.Alias, [command]))
+			{
+				if(_aliases.TryGetValue(command.Alias, out var entities))
+					_aliases[command.Alias] = [.. entities, command];
+				else
+					_aliases[command.Alias] = [command];
+			}
+		}
+	}
+
+	protected override void RemoveItem(int index)
+	{
+		var item = this.Items[index];
+
+		base.RemoveItem(index);
+
+		if(item != null && !string.IsNullOrEmpty(item.Alias))
+			_aliases.Remove(item.Alias);
+	}
+
+	protected override void SetItem(int index, IDataCommand command)
+	{
+		if(command == null)
+			return;
+
+		if(index >= 0 && index < this.Items.Count)
+		{
+			var item = this.Items[index];
+
+			if(item != null && !string.IsNullOrEmpty(item.Alias))
+				_aliases.Remove(item.Alias);
+		}
+
+		base.SetItem(index, command);
+
+		if(!string.IsNullOrEmpty(command.Alias))
+		{
+			if(!_aliases.TryAdd(command.Alias, [command]))
+			{
+				if(_aliases.TryGetValue(command.Alias, out var entities))
+					_aliases[command.Alias] = [.. entities, command];
+				else
+					_aliases[command.Alias] = [command];
+			}
+		}
+	}
 	#endregion
 
 	#region 私有方法
--- Zongsoft.Core/src/Data/Metadata/DataEntityCollection.cs
+++ Zongsoft.Core/src/Data/Metadata/DataEntityCollection.cs
@@ -28,12 +28,17 @@
  */
 
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 
 namespace Zongsoft.Data.Metadata;
 
 public class DataEntityCollection() : KeyedCollection<string, IDataEntity>(StringComparer.OrdinalIgnoreCase)
 {
+	#region 成员字段
+	private readonly Dictionary<string, IDataEntity[]> _aliases = new();
+	#endregion
+
 	#region 公共属性
 	public IDataEntity this[string name, string @namespace = null] => base[GetKey(name, @namespace)];
 	#endregion
@@ -54,10 +59,83 @@ public class DataEntityCollection() : KeyedCollection<string, IDataEntity>(Strin
 	public bool Contains(string name, string @namespace = null) => base.Contains(GetKey(name, @namespace));
 	public bool Remove(string name, string @namespace = null) => base.Remove(GetKey(name, @namespace));
 	public bool TryGetValue(string name, string @namespace, out IDataEntity value) => base.TryGetValue(GetKey(name, @namespace), out value);
+
+	/// <summary>根据别名查找对应的数据实体。</summary>
+	/// <param name="alias">指定要查找的数据实体别名。</param>
+	/// <returns>返回找到的数据实体数组，如果为空则表示查找失败。</returns>
+	public IDataEntity[] Find(string alias)
+	{
+		if(string.IsNullOrEmpty(alias))
+			return null;
+
+		return _aliases.TryGetValue(alias, out var entities) ? entities : base.TryGetValue(alias, out var entity) ? [entity] : null;
+	}
 	#endregion
 
 	#region 重写方法
 	protected override string GetKeyForItem(IDataEntity entity) => entity.QualifiedName;
+
+	protected override void ClearItems()
+	{
+		base.ClearItems();
+		_aliases.Clear();
+	}
+
+	protected override void InsertItem(int index, IDataEntity entity)
+	{
+		if(entity == null)
+			return;
+
+		base.InsertItem(index, entity);
+
+		if(!string.IsNullOrEmpty(entity.Alias))
+		{
+			if(!_aliases.TryAdd(entity.Alias, [entity]))
+			{
+				if(_aliases.TryGetValue(entity.Alias, out var entities))
+					_aliases[entity.Alias] = [.. entities, entity];
+				else
+					_aliases[entity.Alias] = [entity];
+			}
+		}
+	}
+
+	protected override void RemoveItem(int index)
+	{
+		var item = this.Items[index];
+
+		base.RemoveItem(index);
+
+		if(item != null && !string.IsNullOrEmpty(item.Alias))
+			_aliases.Remove(item.Alias);
+	}
+
+	protected override void SetItem(int index, IDataEntity entity)
+	{
+		if(entity == null)
+			return;
+
+		if(index >= 0 && index < this.Items.Count)
+		{
+			var item = this.Items[index];
+
+			if(item != null && !string.IsNullOrEmpty(item.Alias))
+				_aliases.Remove(item.Alias);
+		}
+
+		base.SetItem(index, entity);
+
+		if(!string.IsNullOrEmpty(entity.Alias))
+		{
+			if(!_aliases.TryAdd(entity.Alias, [entity]))
+			{
+				if(_aliases.TryGetValue(entity.Alias, out var entities))
+					_aliases[entity.Alias] = [.. entities, entity];
+				else
+					_aliases[entity.Alias] = [entity];
+			}
+		}
+	}
 	#endregion
 
 	#region 私有方法
