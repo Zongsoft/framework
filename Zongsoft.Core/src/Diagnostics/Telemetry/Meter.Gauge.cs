179420ccf0774dc6d30f88c8cce7afd893902276
PopeyeZhong <9555843@qq.com>
2025-07-15 15:10:17 +0800

    为 Meter 添加了一些扩展方法。 :metal:

---
+++ Zongsoft.Core/src/Diagnostics/Telemetry/Meter.Gauge.cs
@@ -0,0 +1,65 @@
+﻿/*
+ *   _____                                ______
+ *  /_   /  ____  ____  ____  _________  / __/ /_
+ *    / /  / __ \/ __ \/ __ \/ ___/ __ \/ /_/ __/
+ *   / /__/ /_/ / / / / /_/ /\_ \/ /_/ / __/ /_
+ *  /____/\____/_/ /_/\__  /____/\____/_/  \__/
+ *                   /____/
+ *
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@qq.com>
+ *
+ * Copyright (C) 2020-2025 Zongsoft Studio <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.Core library.
+ *
+ * The Zongsoft.Core is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3.0 of the License,
+ * or (at your option) any later version.
+ *
+ * The Zongsoft.Core is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Zongsoft.Core library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Reflection;
+using System.Diagnostics.Metrics;
+using System.Collections.Generic;
+
+namespace Zongsoft.Diagnostics.Telemetry;
+
+#if NET9_0_OR_GREATER
+public static partial class MeterExtension
+{
+	private static readonly MethodInfo _GaugeMethod_ = typeof(Meter).GetMethod(
+		nameof(Meter.CreateGauge),
+		1,
+		BindingFlags.Public | BindingFlags.Instance,
+		null,
+		[typeof(string), typeof(string), typeof(string), typeof(IEnumerable<KeyValuePair<string, object>>)],
+		null);
+
+	public static Instrument CreateGauge(this Meter meter, string name, Type type) => CreateGauge(meter, name, type, null, null, null);
+	public static Instrument CreateGauge(this Meter meter, string name, Type type, string unit, string description = null) => CreateGauge(meter, name, type, unit, description, null);
+	public static Instrument CreateGauge(this Meter meter, string name, Type type, string unit, IEnumerable<KeyValuePair<string, object>> tags = null) => CreateGauge(meter, name, type, unit, null, tags);
+	public static Instrument CreateGauge(this Meter meter, string name, Type type, string unit, string description, IEnumerable<KeyValuePair<string, object>> tags = null)
+	{
+		if(meter == null)
+			throw new ArgumentNullException(nameof(meter));
+		if(string.IsNullOrWhiteSpace(name))
+			throw new ArgumentNullException(nameof(name));
+		if(type == null)
+			throw new ArgumentNullException(nameof(type));
+		if(!type.IsValueType)
+			throw new ArgumentException("The type must be a value type.", nameof(type));
+
+		return (Instrument)_GaugeMethod_.MakeGenericMethod(type).Invoke(meter, [name, unit, description, tags]);
+	}
+}
+#endif
---
+++ Zongsoft.Core/test/Diagnostics/MeterTest.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Linq;
+using System.Diagnostics.Metrics;
+using System.Collections.Generic;
+
+using Xunit;
+
+namespace Zongsoft.Diagnostics.Telemetry.Tests;
+
+public class MeterTest
+{
+	[Fact]
+	public void TestCreateCounter()
+	{
+		var meter = new Meter("TestMeter");
+
+		var counter = meter.CreateCounter("Int32Counter", typeof(int), "units", "description");
+		Assert.NotNull(counter);
+		Assert.NotNull(counter.Meter);
+		Assert.Same(meter, counter.Meter);
+		Assert.IsType<Counter<int>>(counter);
+		Assert.Equal("Int32Counter", counter.Name);
+		Assert.Equal("units", counter.Unit);
+		Assert.Equal("description", counter.Description);
+
+		#if NET8_0_OR_GREATER
+		counter = meter.CreateCounter("DoubleCounter", typeof(double), "units", "description", [new KeyValuePair<string, object>("Tag1", "Value1")]);
+		Assert.NotNull(counter);
+		Assert.NotNull(counter.Meter);
+		Assert.Same(meter, counter.Meter);
+		Assert.IsType<Counter<double>>(counter);
+		Assert.Equal("DoubleCounter", counter.Name);
+		Assert.Equal("units", counter.Unit);
+		Assert.Equal("description", counter.Description);
+		Assert.Single(counter.Tags);
+		Assert.Equal("Tag1", counter.Tags.FirstOrDefault().Key);
+		Assert.Equal("Value1", counter.Tags.FirstOrDefault().Value);
+		#endif
+	}
+
+	[Fact]
+	public void TestCreateHistogram()
+	{
+		var meter = new Meter("TestMeter");
+
+		var histogram = meter.CreateHistogram("Int32Histogram", typeof(int), "units", "description");
+		Assert.NotNull(histogram);
+		Assert.NotNull(histogram.Meter);
+		Assert.Same(meter, histogram.Meter);
+		Assert.IsType<Histogram<int>>(histogram);
+		Assert.Equal("Int32Histogram", histogram.Name);
+		Assert.Equal("units", histogram.Unit);
+		Assert.Equal("description", histogram.Description);
+
+		#if NET8_0_OR_GREATER
+		histogram = meter.CreateHistogram("DoubleHistogram", typeof(double), "units", "description", [new KeyValuePair<string, object>("Tag1", "Value1")]);
+		Assert.NotNull(histogram);
+		Assert.NotNull(histogram.Meter);
+		Assert.Same(meter, histogram.Meter);
+		Assert.IsType<Histogram<double>>(histogram);
+		Assert.Equal("DoubleHistogram", histogram.Name);
+		Assert.Equal("units", histogram.Unit);
+		Assert.Equal("description", histogram.Description);
+		Assert.Single(histogram.Tags);
+		Assert.Equal("Tag1", histogram.Tags.FirstOrDefault().Key);
+		Assert.Equal("Value1", histogram.Tags.FirstOrDefault().Value);
+		#endif
+	}
+
+	#if NET8_0_OR_GREATER
+	[Fact]
+	public void TestCreateUpDownCounter()
+	{
+		var meter = new Meter("TestMeter");
+
+		var counter = meter.CreateUpDownCounter("Int32Counter", typeof(int), "units", "description");
+		Assert.NotNull(counter);
+		Assert.NotNull(counter.Meter);
+		Assert.Same(meter, counter.Meter);
+		Assert.IsType<UpDownCounter<int>>(counter);
+		Assert.Equal("Int32Counter", counter.Name);
+		Assert.Equal("units", counter.Unit);
+		Assert.Equal("description", counter.Description);
+
+		counter = meter.CreateUpDownCounter("DoubleCounter", typeof(double), "units", "description", [new KeyValuePair<string, object>("Tag1", "Value1")]);
+		Assert.NotNull(counter);
+		Assert.NotNull(counter.Meter);
+		Assert.Same(meter, counter.Meter);
+		Assert.IsType<UpDownCounter<double>>(counter);
+		Assert.Equal("DoubleCounter", counter.Name);
+		Assert.Equal("units", counter.Unit);
+		Assert.Equal("description", counter.Description);
+		Assert.Single(counter.Tags);
+		Assert.Equal("Tag1", counter.Tags.FirstOrDefault().Key);
+		Assert.Equal("Value1", counter.Tags.FirstOrDefault().Value);
+	}
+	#endif
+
+	#if NET9_0_OR_GREATER
+	[Fact]
+	public void TestCreateGauge()
+	{
+		var meter = new Meter("TestMeter");
+
+		var gauge = meter.CreateGauge("Int32Gauge", typeof(int), "units", "description");
+		Assert.NotNull(gauge);
+		Assert.NotNull(gauge.Meter);
+		Assert.Same(meter, gauge.Meter);
+		Assert.IsType<Gauge<int>>(gauge);
+		Assert.Equal("Int32Gauge", gauge.Name);
+		Assert.Equal("units", gauge.Unit);
+		Assert.Equal("description", gauge.Description);
+
+		gauge = meter.CreateGauge("DoubleGauge", typeof(double), "units", "description", [new KeyValuePair<string, object>("Tag1", "Value1")]);
+		Assert.NotNull(gauge);
+		Assert.NotNull(gauge.Meter);
+		Assert.Same(meter, gauge.Meter);
+		Assert.IsType<Gauge<double>>(gauge);
+		Assert.Equal("DoubleGauge", gauge.Name);
+		Assert.Equal("units", gauge.Unit);
+		Assert.Equal("description", gauge.Description);
+		Assert.Single(gauge.Tags);
+		Assert.Equal("Tag1", gauge.Tags.FirstOrDefault().Key);
+		Assert.Equal("Value1", gauge.Tags.FirstOrDefault().Value);
+	}
+	#endif
+}
