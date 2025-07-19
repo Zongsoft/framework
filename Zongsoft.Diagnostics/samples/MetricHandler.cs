249c3fff1d815a567897acf987628ff48fb825ca
PopeyeZhong <9555843@qq.com>
2025-07-19 20:22:48 +0800

    添加 Diagnostics 项目的范例程序。 🎼

---
+++ Zongsoft.Diagnostics/samples/MetricHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using Zongsoft.Components;
+using Zongsoft.Collections;
+
+namespace Zongsoft.Diagnostics.Samples;
+
+public class MetricHandler : HandlerBase<IEnumerable<Zongsoft.Diagnostics.Telemetry.Metrics.Meter>>
+{
+	protected override ValueTask OnHandleAsync(IEnumerable<Telemetry.Metrics.Meter> meters, Parameters parameters, CancellationToken cancellation)
+	{
+		foreach(var meter in meters)
+			CommandOutletDumper.Dump(meter);
+
+		return ValueTask.CompletedTask;
+	}
+}
