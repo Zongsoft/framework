c947cc5b7a95cfd785f22d7f04500090ec118960
PopeyeZhong <9555843@qq.com>
2025-07-22 18:30:07 +0800

    完善安全模块的身份验证事件表。 🌿

--- Zongsoft.Security/src/Module.cs
+++ Zongsoft.Security/src/Module.cs
@@ -33,38 +33,62 @@ using Zongsoft.Components;
 
 [assembly: ApplicationModule(Zongsoft.Security.Module.NAME)]
 
-namespace Zongsoft.Security
+namespace Zongsoft.Security;
+
+public sealed class Module : ApplicationModule<Module.EventRegistry>
 {
-	public sealed class Module : ApplicationModule<Module.EventRegistry>
+	#region 常量定义
+	/// <summary>表示安全模块的名称常量值。</summary>
+	public const string NAME = nameof(Security);
+	#endregion
+
+	#region 单例字段
+	/// <summary>表示安全模块的单例字段。</summary>
+	public static readonly Module Current = new();
+	#endregion
+
+	#region 私有构造
+	private Module() : base(NAME, Zongsoft.Security.Properties.Resources.Security_Title, Zongsoft.Security.Properties.Resources.Security_Description) { }
+	#endregion
+
+	#region 公共属性
+	private IDataAccess _accessor;
+	public IDataAccess Accessor => _accessor ??= this.Services.ResolveRequired<IDataAccessProvider>().GetAccessor(this.Name);
+	#endregion
+
+	#region 嵌套子类
+	[System.Reflection.DefaultMember(nameof(Events))]
+	public sealed class EventRegistry : EventRegistryBase
 	{
-		#region 常量定义
-		/// <summary>表示安全模块的名称常量值。</summary>
-		public const string NAME = nameof(Security);
-		#endregion
+		#region 构造函数
+		public EventRegistry() : base(NAME)
+		{
+			this.Event(AuthenticationEvent.Authenticated);
+			this.Event(AuthenticationEvent.Authenticating);
 
-		#region 单例字段
-		/// <summary>表示安全模块的单例字段。</summary>
-		public static readonly Module Current = new();
-		#endregion
-
-		#region 私有构造
-		private Module() : base(NAME, Zongsoft.Security.Properties.Resources.Security_Title, Zongsoft.Security.Properties.Resources.Security_Description) { }
+			this.Authentication = new(this);
+		}
 		#endregion
 
 		#region 公共属性
-		private IDataAccess _accessor;
-		public IDataAccess Accessor => _accessor ??= this.Services.ResolveRequired<IDataAccessProvider>().GetAccessor(this.Name);
+		public AuthenticationEvent Authentication { get; }
 		#endregion
 
-		#region 嵌套子类
-		public sealed class EventRegistry : EventRegistryBase
+		public class AuthenticationEvent
 		{
+			#region 静态字段
+			public static readonly EventDescriptor<Privileges.AuthenticatedEventArgs> Authenticated = new($"{nameof(Privileges.Authentication)}.{nameof(Privileges.Authentication.Authenticated)}");
+			public static readonly EventDescriptor<Privileges.AuthenticatingEventArgs> Authenticating = new($"{nameof(Privileges.Authentication)}.{nameof(Privileges.Authentication.Authenticating)}");
+			#endregion
+
+			#region 成员字段
+			private readonly EventRegistry _registry;
+			#endregion
+
 			#region 构造函数
-			public EventRegistry() : base(NAME)
-			{
-			}
+			internal AuthenticationEvent(EventRegistry registry) => _registry = registry;
 			#endregion
 		}
-		#endregion
 	}
+	#endregion
 }
