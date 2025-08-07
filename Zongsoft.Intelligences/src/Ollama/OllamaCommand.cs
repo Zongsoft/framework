96a2eaf417ec7343c93cbb802da39ca42b67f811
PopeyeZhong <9555843@qq.com>
2025-08-07 19:43:48 +0800

    初始化 AI 项目。 🚃

---
+++ Zongsoft.Intelligences/src/Ollama/OllamaCommand.cs
@@ -0,0 +1,60 @@
+﻿/*
+ *   _____                                ______
+ *  /_   /  ____  ____  ____  _________  / __/ /_
+ *    / /  / __ \/ __ \/ __ \/ ___/ __ \/ /_/ __/
+ *   / /__/ /_/ / / / / /_/ /\_ \/ /_/ / __/ /_
+ *  /____/\____/_/ /_/\__  /____/\____/_/  \__/
+ *                   /____/
+ *
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@qq.com>
+ *
+ * Copyright (C) 2025-2025 Zongsoft Studio <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.Intelligences library.
+ *
+ * The Zongsoft.Intelligences is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3.0 of the License,
+ * or (at your option) any later version.
+ *
+ * The Zongsoft.Intelligences is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Zongsoft.Intelligences library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using OllamaSharp;
+
+using Zongsoft.Components;
+
+namespace Zongsoft.Intelligences.Ollama;
+
+public class OllamaCommand : CommandBase<CommandContext>
+{
+	#region 构造函数
+	public OllamaCommand() : base("Ollama") { }
+	public OllamaCommand(string name) : base(name) { }
+	#endregion
+
+	#region 公共属性
+	public IOllamaApiClient Client { get; set; }
+	#endregion
+
+	#region 重写方法
+	protected override ValueTask<object> OnExecuteAsync(CommandContext context, CancellationToken cancellation)
+	{
+		if(context.Value is IOllamaApiClient client)
+			this.Client = client;
+
+		return ValueTask.FromResult<object>(this.Client);
+	}
+	#endregion
+}
---
+++ Zongsoft.Intelligences/src/Ollama/OllamaListCommand.cs
@@ -0,0 +1,68 @@
+﻿/*
+ *   _____                                ______
+ *  /_   /  ____  ____  ____  _________  / __/ /_
+ *    / /  / __ \/ __ \/ __ \/ ___/ __ \/ /_/ __/
+ *   / /__/ /_/ / / / / /_/ /\_ \/ /_/ / __/ /_
+ *  /____/\____/_/ /_/\__  /____/\____/_/  \__/
+ *                   /____/
+ *
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@qq.com>
+ *
+ * Copyright (C) 2025-2025 Zongsoft Studio <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.Intelligences library.
+ *
+ * The Zongsoft.Intelligences is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3.0 of the License,
+ * or (at your option) any later version.
+ *
+ * The Zongsoft.Intelligences is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Zongsoft.Intelligences library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using Zongsoft.Components;
+
+namespace Zongsoft.Intelligences.Ollama;
+
+public class OllamaListCommand() : CommandBase<CommandContext>("List")
+{
+	protected override async ValueTask<object> OnExecuteAsync(CommandContext context, CancellationToken cancellation)
+	{
+		var client = context.Find<OllamaCommand>(true)?.Client;
+		if(client == null)
+			throw new CommandException("The Ollama API client is not found.");
+
+		if(context.Expression.Options.Contains("running"))
+		{
+			var models = await client.ListRunningModelsAsync(cancellation);
+			Dump(context.Output, models);
+			return models;
+		}
+		else
+		{
+			var models = await client.ListLocalModelsAsync(cancellation);
+			Dump(context.Output, models);
+			return models;
+		}
+	}
+
+	private static void Dump(ICommandOutlet outlet, IEnumerable<OllamaSharp.Models.Model> models)
+	{
+		foreach(var model in models)
+		{
+			outlet.Dump(model);
+		}
+	}
+}
