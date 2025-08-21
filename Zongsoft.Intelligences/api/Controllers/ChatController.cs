0a255e7835273903a2ed62949bd1c6800f641864
PopeyeZhong <9555843@qq.com>
2025-08-21 17:48:20 +0800

    添加AI的 Web APID 库。 👿

---
+++ Zongsoft.Intelligences/api/Controllers/ChatController.cs
@@ -0,0 +1,55 @@
+﻿/*
+ *   _____                                ______
+ *  /_   /  ____  ____  ____  _________  / __/ /_
+ *    / /  / __ \/ __ \/ __ \/ ___/ __ \/ /_/ __/
+ *   / /__/ /_/ / / / / /_/ /\_ \/ /_/ / __/ /_
+ *  /____/\____/_/ /_/\__  /____/\____/_/  \__/
+ *                   /____/
+ *
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@qq.com>
+ *
+ * Copyright (C) 2010-2020 Zongsoft Studio <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.Intelligences.Web library.
+ *
+ * The Zongsoft.Intelligences.Web is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3.0 of the License,
+ * or (at your option) any later version.
+ *
+ * The Zongsoft.Intelligences.Web is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Zongsoft.Intelligences.Web library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
+
+using Zongsoft.Web;
+using Zongsoft.Web.Http;
+using Zongsoft.Services;
+
+namespace Zongsoft.Intelligences.Web.Controllers;
+
+[Area("AI")]
+[ControllerName("Chats")]
+public class ChatController : ControllerBase
+{
+	#region 公共方法
+	[HttpPost("{id?}")]
+	public ValueTask<IActionResult> Chat(string id, CancellationToken cancellation = default)
+	{
+		throw new NotImplementedException();
+	}
+	#endregion
+}
