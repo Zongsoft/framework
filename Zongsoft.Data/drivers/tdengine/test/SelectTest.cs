4aa90dd56986d137ffa72e94ecf026097331cc99
Popeye Zhong <zongsoft@qq.com>
2025-10-29 21:43:56 +0800

    完善单元测试。 😾

--- Zongsoft.Data/drivers/tdengine/test/SelectTest.cs
+++ Zongsoft.Data/drivers/tdengine/test/SelectTest.cs
@@ -22,9 +22,15 @@ public class SelectTest
 		Assert.NotEmpty(Mapping.Entities);
 		Assert.True(Mapping.Entities.Contains("GatewayHistory"));
 
+		accessor.Delete("GatewayHistory", null);
+		Assert.Equal(0, accessor.Count("GatewayHistory"));
+
+		var count = accessor.Insert("GatewayHistory", new Models.GatewayHistory(100, 10001, 123.56, null, DateTime.Now));
+		Assert.Equal(1, count);
+
 		var models = accessor.Select<Models.GatewayHistory>();
 		Assert.NotNull(models);
-		Assert.NotEmpty(models);
+		Assert.Empty(models);
 
 		var model = models.FirstOrDefault();
 		Assert.True(model.GatewayId > 0);
@@ -43,6 +49,12 @@ public class SelectTest
 		Assert.NotNull(Mapping.Entities);
 		Assert.NotEmpty(Mapping.Entities);
 		Assert.True(Mapping.Entities.Contains("GatewayHistory"));
+
+		await accessor.DeleteAsync("GatewayHistory", null);
+		Assert.Equal(0, accessor.Count("GatewayHistory"));
+
+		var count = await accessor.InsertAsync("GatewayHistory", new Models.GatewayHistory(100, 10001, 123.56, null, DateTime.Now));
+		Assert.Equal(1, count);
 
 		var models = accessor.SelectAsync<Models.GatewayHistory>();
 		Assert.NotNull(models);
