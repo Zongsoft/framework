8f56269017d640e3f4f4c5729c95a848f5e3db00
PopeyeZhong <9555843@qq.com>
2025-10-30 16:59:59 +0800

    格式化初始化SQL脚本文件。 ✊

--- Zongsoft.Data/drivers/tdengine/test/script/init.sql
+++ Zongsoft.Data/drivers/tdengine/test/script/init.sql
@@ -4,5 +4,5 @@ CREATE DATABASE IF NOT EXISTS zongsoft;
  * 注意：TDengine 初始化脚本内的SQL语句不支持换行！
  */
 
-CREATE STABLE IF NOT EXISTS zongsoft.`GatewayHistory`(`Timestamp` timestamp, `Value` double, `Text` varchar(100), `FailureCode` int, `FailureMessage` nchar(500)) TAGS (`GatewayId` int unsigned, `MetricId`  bigint unsigned);
+CREATE STABLE IF NOT EXISTS zongsoft.`GatewayHistory`(`Timestamp` timestamp, `Value` double, `Text` varchar(100), `FailureCode` int, `FailureMessage` nchar(500)) TAGS (`GatewayId` int unsigned, `MetricId` bigint unsigned);
 CREATE STABLE IF NOT EXISTS zongsoft.`DeviceHistory`(`Timestamp` timestamp, `Value` double, `Text` varchar(100), `FailureCode` int, `FailureMessage` nchar(500)) TAGS (`DeviceId` bigint unsigned, `MetricId` bigint unsigned);
