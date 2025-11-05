2ab906db33b007fcba0ab5181e0d173e5dd7256b
PopeyeZhong <9555843@qq.com>
2025-11-05 10:46:40 +0800

    完善MySQL数据驱动的单元测试。 ☔

---
+++ Zongsoft.Data/drivers/mysql/test/ExportTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Xunit;
+
+using Zongsoft.Data.MySql.Tests.Models;
+
+namespace Zongsoft.Data.MySql.Tests;
+
+[Collection("Database")]
+public class ExportTest(DatabaseFixture database)
+{
+	private readonly DatabaseFixture _database = database;
+
+}
---
+++ Zongsoft.Data/drivers/mysql/test/ImportTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Xunit;
+
+using Zongsoft.Data.MySql.Tests.Models;
+
+namespace Zongsoft.Data.MySql.Tests;
+
+[Collection("Database")]
+public class ImportTest(DatabaseFixture database)
+{
+	private readonly DatabaseFixture _database = database;
+
+	[Fact]
+	public async Task ImportAsync()
+	{
+		const int COUNT = 1000;
+
+		if(!Global.IsTestingEnabled)
+			return;
+
+		IDataAccess accessor = _database.Accessor;
+
+		await accessor.DeleteAsync<UserModel>(Condition.GreaterThanEqual(nameof(UserModel.UserId), 1000));
+
+		var users = Model.Build<UserModel>(COUNT, (model, index) =>
+		{
+			model.UserId = (uint)(1000 + index);
+			model.Name = $"${Zongsoft.Common.Randomizer.GenerateString()}";
+		});
+
+		var count = await accessor.ImportAsync(users);
+		Assert.Equal(COUNT, count);
+
+		count = await accessor.DeleteAsync<UserModel>(Condition.Between(nameof(UserModel.UserId), 1000, 1000 + COUNT));
+		Assert.Equal(COUNT, count);
+	}
+}
