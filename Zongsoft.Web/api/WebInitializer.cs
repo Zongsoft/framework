1b15a54e3fd9b6035890f4ca4036a8bf83a1b3a9
PopeyeZhong <9555843@qq.com>
2025-11-26 17:13:53 +0800

    更新版本。 🎮

--- Zongsoft.Web/api/WebInitializer.cs
+++ Zongsoft.Web/api/WebInitializer.cs
@@ -28,22 +28,11 @@
  */
 
 using System;
-using System.Collections.Generic;
-using System.Text.Json;
-using System.Text.Json.Serialization;
-using System.Text.Json.Serialization.Metadata;
 
-using Microsoft.AspNetCore.OpenApi;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Routing;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ApiExplorer;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 
 using Scalar.AspNetCore;
-
-using Zongsoft.Common;
 using Zongsoft.Services;
 
 namespace Zongsoft.Web.OpenApi;
@@ -62,82 +51,6 @@ public class WebInitializer : IApplicationInitializer<IApplicationBuilder>
 				options.WithDefaultHttpClient(ScalarTarget.Http, ScalarClient.Http);
 				options.Authentication = new() { PreferredSecuritySchemes = ["Credential"] };
 			});
-		}
-	}
-	#endregion
-
-	#region 服务注册
-	public sealed class Registration : IServiceRegistration
-	{
-		public void Register(IServiceCollection services, IConfiguration configuration)
-		{
-			//services.Configure<JsonOptions>(options =>
-			//{
-			//	options.JsonSerializerOptions.TypeInfoResolverChain.Insert(0, new JsonTypeResolver());
-			//	options.JsonSerializerOptions.MaxDepth = 10;
-			//	options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
-			//});
-
-			//services.AddOpenApi(options =>
-			//{
-			//	options.ShouldInclude = OnShouldInclude;
-			//	options.CreateSchemaReferenceId = CreateReference;
-			//	options.AddSchemaTransformer(new Transformers.SchemaTransformer());
-			//	options.AddDocumentTransformer(new Transformers.DocumentTransformer());
-			//	options.AddOperationTransformer(new Transformers.OperationTransformer());
-			//});
-		}
-
-		private static string CreateReference(JsonTypeInfo info)
-		{
-			if(!info.Options.IsReadOnly)
-			{
-				info.Options.MaxDepth = 32;
-				info.Options.ReferenceHandler = ReferenceHandler.IgnoreCycles;
-			}
-
-			return info.Kind == JsonTypeInfoKind.Enumerable ? info.ElementType.GetAlias() : info.Type.GetAlias();
-		}
-
-		private static bool OnShouldInclude(ApiDescription api)
-		{
-			if(api.ActionDescriptor is Microsoft.AspNetCore.Mvc.Controllers.ControllerActionDescriptor action)
-			{
-				if(action.MethodInfo.ReturnType.IsGenericType)
-				{
-					if(action.MethodInfo.ReturnType.GetGenericTypeDefinition() == typeof(IAsyncEnumerable<>) || action.MethodInfo.ReturnType.GetGenericTypeDefinition() == typeof(IEnumerable<>))
-					{
-						if(action.MethodInfo.ReturnType.GenericTypeArguments[0].IsAbstract)
-							return false;
-					}
-					else if(action.MethodInfo.ReturnType.GetGenericTypeDefinition() == typeof(IDictionary<,>))
-					{
-						if(action.MethodInfo.ReturnType.GenericTypeArguments[1].IsAbstract)
-							return false;
-					}
-				}
-			}
-
-			if(api.ActionDescriptor != null && api.ActionDescriptor.Parameters != null)
-			{
-				foreach(var parameter in api.ActionDescriptor.Parameters)
-				{
-					if(parameter.ParameterType.IsAbstract)
-						return false;
-				}
-			}
-
-			return true;
-		}
-
-		public class JsonTypeResolver : IJsonTypeInfoResolver
-		{
-			public JsonTypeInfo GetTypeInfo(Type type, JsonSerializerOptions options)
-			{
-				options.MaxDepth = 10;
-				options.ReferenceHandler = ReferenceHandler.IgnoreCycles;
-				return JsonTypeInfo.CreateJsonTypeInfo(type, options);
-			}
 		}
 	}
 	#endregion
