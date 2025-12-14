61c38bd426fc8cdc5155d0dedf247ef42b5d13c2
Popeye Zhong <zongsoft@qq.com>
2025-12-14 23:09:57 +0800

    完善多个导航属性的删除测试。 🍄

--- Zongsoft.Data/drivers/mysql/test/DeleteTest.cs
+++ Zongsoft.Data/drivers/mysql/test/DeleteTest.cs
@@ -36,7 +36,7 @@ public class DeleteTest(DatabaseFixture database)
 	}
 
 	[Fact]
-	public async Task DeleteAsync_Cascading()
+	public async Task DeleteAsync_Cascading1()
 	{
 		if(!Global.IsTestingEnabled)
 			return;
@@ -59,5 +59,52 @@ public class DeleteTest(DatabaseFixture database)
 		Assert.Equal(2, count);
 		Assert.False(await accessor.ExistsAsync<RoleModel>(Condition.Equal(nameof(RoleModel.RoleId), 100)));
 		Assert.False(await accessor.ExistsAsync<MemberModel>(Condition.Equal(nameof(MemberModel.RoleId), 100)));
+	}
+
+	[Fact]
+	public async Task DeleteAsync_Cascading2()
+	{
+		const uint TenantId = 1U;
+		const uint BranchId = 0x01_00_00_00;
+
+		if(!Global.IsTestingEnabled)
+			return;
+
+		var accessor = _database.Accessor;
+		await accessor.InsertAsync(Model.Build<Branch>(model => {
+			model.TenantId = TenantId;
+			model.BranchId = BranchId;
+			model.BranchNo = "B01";
+			model.Name = "Branch#1";
+			model.CreatedTime = DateTime.UtcNow;
+		}), DataInsertOptions.SuppressSequence().IgnoreConstraint());
+
+		await accessor.InsertAsync(Model.Build<Department>(model =>
+		{
+			model.TenantId = TenantId;
+			model.BranchId = BranchId;
+			model.DepartmentId = 1;
+			model.DepartmentNo = "D01";
+			model.Name = "Department#1";
+		}), DataInsertOptions.SuppressSequence().IgnoreConstraint());
+
+		await accessor.InsertAsync(Model.Build<Team>(model =>
+		{
+			model.TenantId = TenantId;
+			model.BranchId = BranchId;
+			model.TeamId = 1;
+			model.TeamNo = "T01";
+			model.Name = "Team#1";
+		}), DataInsertOptions.SuppressSequence().IgnoreConstraint());
+
+		var count = await accessor.DeleteAsync<Branch>(
+			Condition.Equal(nameof(Branch.TenantId), TenantId) &
+			Condition.Equal(nameof(Branch.BranchId), BranchId),
+			$"{nameof(Branch.Departments)},{nameof(Branch.Teams)}");
+
+		Assert.Equal(3, count);
+		Assert.False(await accessor.ExistsAsync<Branch>(Condition.Equal(nameof(Branch.TenantId), TenantId) & Condition.Equal(nameof(Branch.BranchId), BranchId)));
+		Assert.False(await accessor.ExistsAsync<Team>(Condition.Equal(nameof(Team.TenantId), TenantId) & Condition.Equal(nameof(Team.BranchId), BranchId)));
+		Assert.False(await accessor.ExistsAsync<Department>(Condition.Equal(nameof(Department.TenantId), TenantId) & Condition.Equal(nameof(Department.BranchId), BranchId)));
 	}
 }
