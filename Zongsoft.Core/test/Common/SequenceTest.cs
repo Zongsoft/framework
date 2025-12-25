cbee1a6f6a10587b3f4605d18665b71b9d74de20
PopeyeZhong <9555843@qq.com>
2025-12-25 18:26:03 +0800

    完善序号器的单元测试。 🐍

--- Zongsoft.Core/test/Common/SequenceTest.cs
+++ Zongsoft.Core/test/Common/SequenceTest.cs
@@ -17,11 +17,12 @@ public class SequenceTest
 		Parallel.For(0, 1000, _ => mocker.Increase("Integer", 1, 1));
 		Assert.Equal(1001, mocker.Increase("Integer", 1));
 
+		mocker.Latency = TimeSpan.FromMilliseconds(100);
 		var sequence = Sequence.Variate(mocker);
 		Assert.Equal(1, sequence.Increase("Var"));
 
-		Parallel.For(1, 10000, _ => sequence.Increase("Var"));
-		Assert.Equal(10001, sequence.Increase("Var"));
+		Parallel.For(1, 1000, _ => sequence.Increase("Var"));
+		Assert.Equal(1001, sequence.Increase("Var"));
 
 		sequence.Reset("Var");
 		Assert.Equal(1, sequence.Increase("Var"));
@@ -37,12 +38,13 @@ public class SequenceTest
 		Parallel.For(0, 1000, async _ => await mocker.IncreaseAsync("Integer", 1, 1));
 		Assert.Equal(1001, await mocker.IncreaseAsync("Integer", 1));
 
+		mocker.Latency = TimeSpan.FromMilliseconds(100);
 		var sequence = Sequence.Variate(mocker);
 		Assert.Equal(1, await sequence.IncreaseAsync("Var"));
 
 		#if NET8_0_OR_GREATER
-		await Parallel.ForAsync(1, 10000, async (_, cancellation) => await sequence.IncreaseAsync("Var", cancellation: cancellation));
-		Assert.Equal(10001, await sequence.IncreaseAsync("Var"));
+		await Parallel.ForAsync(1, 1000, async (_, cancellation) => await sequence.IncreaseAsync("Var", cancellation: cancellation));
+		Assert.Equal(1001, await sequence.IncreaseAsync("Var"));
 		#endif
 
 		sequence.Reset("Var");
@@ -52,15 +54,19 @@ public class SequenceTest
 	}
 
 	#region 嵌套子类
-	public sealed class SequenceMocker : Zongsoft.Common.ISequenceBase
+	public sealed class SequenceMocker(TimeSpan? latency = null) : Zongsoft.Common.ISequenceBase
 	{
 		private readonly Dictionary<string, long> _dictionary = new();
+		public TimeSpan Latency { get; set; } = latency ?? TimeSpan.Zero;
 
 		public long Decrease(string key, int interval = 1, int seed = 0) => this.Increase(key, -interval, seed);
 		public ValueTask<long> DecreaseAsync(string key, int interval = 1, int seed = 0, CancellationToken cancellation = default) => this.IncreaseAsync(key, -interval, seed, cancellation);
 
 		public long Increase(string key, int interval = 1, int seed = 0)
 		{
+			if(this.Latency > TimeSpan.Zero)
+				Thread.Sleep(this.Latency);
+
 			lock(this)
 			{
 				if(_dictionary.TryAdd(key, seed))
@@ -70,22 +76,39 @@ public class SequenceTest
 			}
 		}
 
-		public ValueTask<long> IncreaseAsync(string key, int interval = 1, int seed = 0, CancellationToken cancellation = default) => ValueTask.FromResult(this.Increase(key, interval, seed));
+		public async ValueTask<long> IncreaseAsync(string key, int interval = 1, int seed = 0, CancellationToken cancellation = default)
+		{
+			if(this.Latency > TimeSpan.Zero)
+				await Task.Delay(this.Latency, cancellation);
+
+			lock(this)
+			{
+				if(_dictionary.TryAdd(key, seed))
+					return seed;
+
+				return _dictionary[key] += interval;
+			}
+		}
 
 		public void Reset(string key, int value = 0)
 		{
+			if(this.Latency > TimeSpan.Zero)
+				Thread.Sleep(this.Latency);
+
 			lock(this)
 			{
 				_dictionary[key] = value;
 			}
 		}
 
-		public ValueTask ResetAsync(string key, int value = 0, CancellationToken cancellation = default)
+		public async ValueTask ResetAsync(string key, int value = 0, CancellationToken cancellation = default)
 		{
+			if(this.Latency > TimeSpan.Zero)
+				await Task.Delay(this.Latency, cancellation);
+
 			lock(this)
 			{
 				_dictionary[key] = value;
-				return ValueTask.CompletedTask;
 			}
 		}
 	}
