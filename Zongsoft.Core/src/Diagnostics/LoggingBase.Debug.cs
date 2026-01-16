eecef14f72332727feae91d692731360f2ff58ca
PopeyeZhong <9555843@qq.com>
2026-01-16 19:06:39 +0800

    添加 LoggingBase<TLog> 日志泛型基类。 🍳

---
+++ Zongsoft.Core/src/Diagnostics/LoggingBase.Debug.cs
@@ -0,0 +1,81 @@
+﻿/*
+ *   _____                                ______
+ *  /_   /  ____  ____  ____  _________  / __/ /_
+ *    / /  / __ \/ __ \/ __ \/ ___/ __ \/ /_/ __/
+ *   / /__/ /_/ / / / / /_/ /\_ \/ /_/ / __/ /_
+ *  /____/\____/_/ /_/\__  /____/\____/_/  \__/
+ *                   /____/
+ *
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@qq.com>
+ *
+ * Copyright (C) 2020-2025 Zongsoft Studio <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.Core library.
+ *
+ * The Zongsoft.Core is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3.0 of the License,
+ * or (at your option) any later version.
+ *
+ * The Zongsoft.Core is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Zongsoft.Core library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Zongsoft.Diagnostics;
+
+partial class LoggingBase<TLog>
+{
+	public void Debug(Exception exception, object data = null, [System.Runtime.CompilerServices.CallerMemberName] string action = null) => Logging.Log(this.CreateLog(LogLevel.Debug, exception, data, null, action));
+	public void Debug(string message, object data = null, [System.Runtime.CompilerServices.CallerMemberName] string action = null) => Logging.Log(this.CreateLog(LogLevel.Debug, message, data, null, action));
+	public void Debug(string message, Exception exception, object data = null, [System.Runtime.CompilerServices.CallerMemberName] string action = null) => Logging.Log(this.CreateLog(LogLevel.Debug, message, exception, data, null, action));
+
+	public void Debug<TSource>(Exception exception, object data = null, [System.Runtime.CompilerServices.CallerMemberName] string action = null) => Logging.Log(this.CreateLog(LogLevel.Debug, exception, data, this.GetSource(typeof(TSource)), action));
+	public void Debug<TSource>(string message, object data = null, [System.Runtime.CompilerServices.CallerMemberName] string action = null) => Logging.Log(this.CreateLog(LogLevel.Debug, message, data, this.GetSource(typeof(TSource)), action));
+	public void Debug<TSource>(string message, Exception exception, object data = null, [System.Runtime.CompilerServices.CallerMemberName] string action = null) => Logging.Log(this.CreateLog(LogLevel.Debug, message, exception, data, this.GetSource(typeof(TSource)), action));
+
+	public void Debug(Type source, Exception exception, object data = null, [System.Runtime.CompilerServices.CallerMemberName] string action = null) => Logging.Log(this.CreateLog(LogLevel.Debug, exception, data, this.GetSource(source), action));
+	public void Debug(Type source, string message, object data = null, [System.Runtime.CompilerServices.CallerMemberName] string action = null) => Logging.Log(this.CreateLog(LogLevel.Debug, message, data, this.GetSource(source), action));
+	public void Debug(Type source, string message, Exception exception, object data = null, [System.Runtime.CompilerServices.CallerMemberName] string action = null) => Logging.Log(this.CreateLog(LogLevel.Debug, message, exception, data, this.GetSource(source), action));
+
+	public void Debug(object source, Exception exception, object data = null, [System.Runtime.CompilerServices.CallerMemberName] string action = null) => Logging.Log(this.CreateLog(LogLevel.Debug, exception, data, this.GetSource(source?.GetType()), action));
+	public void Debug(object source, string message, object data = null, [System.Runtime.CompilerServices.CallerMemberName] string action = null) => Logging.Log(this.CreateLog(LogLevel.Debug, message, data, this.GetSource(source?.GetType()), action));
+	public void Debug(object source, string message, Exception exception, object data = null, [System.Runtime.CompilerServices.CallerMemberName] string action = null) => Logging.Log(this.CreateLog(LogLevel.Debug, message, exception, data, this.GetSource(source?.GetType()), action));
+
+	public ValueTask DebugAsync(Exception exception, CancellationToken cancellation = default, [System.Runtime.CompilerServices.CallerMemberName] string action = null) => Logging.LogAsync(this.CreateLog(LogLevel.Debug, exception, null, null, action), cancellation);
+	public ValueTask DebugAsync(Exception exception, object data, CancellationToken cancellation = default, [System.Runtime.CompilerServices.CallerMemberName] string action = null) => Logging.LogAsync(this.CreateLog(LogLevel.Debug, exception, data, null, action), cancellation);
+	public ValueTask DebugAsync(string message, CancellationToken cancellation = default, [System.Runtime.CompilerServices.CallerMemberName] string action = null) => Logging.LogAsync(this.CreateLog(LogLevel.Debug, message, null, null, action), cancellation);
+	public ValueTask DebugAsync(string message, object data, CancellationToken cancellation = default, [System.Runtime.CompilerServices.CallerMemberName] string action = null) => Logging.LogAsync(this.CreateLog(LogLevel.Debug, message, data, null, action), cancellation);
+	public ValueTask DebugAsync(string message, Exception exception, CancellationToken cancellation = default, [System.Runtime.CompilerServices.CallerMemberName] string action = null) => Logging.LogAsync(this.CreateLog(LogLevel.Debug, message, exception, null, null, action), cancellation);
+	public ValueTask DebugAsync(string message, Exception exception, object data, CancellationToken cancellation = default, [System.Runtime.CompilerServices.CallerMemberName] string action = null) => Logging.LogAsync(this.CreateLog(LogLevel.Debug, message, exception, data, null, action), cancellation);
+
+	public ValueTask DebugAsync<TSource>(Exception exception, CancellationToken cancellation = default, [System.Runtime.CompilerServices.CallerMemberName] string action = null) => Logging.LogAsync(this.CreateLog(LogLevel.Debug, exception, null, this.GetSource(typeof(TSource)), action), cancellation);
+	public ValueTask DebugAsync<TSource>(Exception exception, object data, CancellationToken cancellation = default, [System.Runtime.CompilerServices.CallerMemberName] string action = null) => Logging.LogAsync(this.CreateLog(LogLevel.Debug, exception, data, this.GetSource(typeof(TSource)), action), cancellation);
+	public ValueTask DebugAsync<TSource>(string message, CancellationToken cancellation = default, [System.Runtime.CompilerServices.CallerMemberName] string action = null) => Logging.LogAsync(this.CreateLog(LogLevel.Debug, message, null, this.GetSource(typeof(TSource)), action), cancellation);
+	public ValueTask DebugAsync<TSource>(string message, object data, CancellationToken cancellation = default, [System.Runtime.CompilerServices.CallerMemberName] string action = null) => Logging.LogAsync(this.CreateLog(LogLevel.Debug, message, data, this.GetSource(typeof(TSource)), action), cancellation);
+	public ValueTask DebugAsync<TSource>(string message, Exception exception, CancellationToken cancellation = default, [System.Runtime.CompilerServices.CallerMemberName] string action = null) => Logging.LogAsync(this.CreateLog(LogLevel.Debug, message, exception, null, this.GetSource(typeof(TSource)), action), cancellation);
+	public ValueTask DebugAsync<TSource>(string message, Exception exception, object data, CancellationToken cancellation = default, [System.Runtime.CompilerServices.CallerMemberName] string action = null) => Logging.LogAsync(this.CreateLog(LogLevel.Debug, message, exception, data, this.GetSource(typeof(TSource)), action), cancellation);
+
+	public ValueTask DebugAsync(Type source, Exception exception, CancellationToken cancellation = default, [System.Runtime.CompilerServices.CallerMemberName] string action = null) => Logging.LogAsync(this.CreateLog(LogLevel.Debug, exception, null, this.GetSource(source), action), cancellation);
+	public ValueTask DebugAsync(Type source, Exception exception, object data, CancellationToken cancellation = default, [System.Runtime.CompilerServices.CallerMemberName] string action = null) => Logging.LogAsync(this.CreateLog(LogLevel.Debug, exception, data, this.GetSource(source), action), cancellation);
+	public ValueTask DebugAsync(Type source, string message, CancellationToken cancellation = default, [System.Runtime.CompilerServices.CallerMemberName] string action = null) => Logging.LogAsync(this.CreateLog(LogLevel.Debug, message, null, this.GetSource(source), action), cancellation);
+	public ValueTask DebugAsync(Type source, string message, object data, CancellationToken cancellation = default, [System.Runtime.CompilerServices.CallerMemberName] string action = null) => Logging.LogAsync(this.CreateLog(LogLevel.Debug, message, data, this.GetSource(source), action), cancellation);
+	public ValueTask DebugAsync(Type source, string message, Exception exception, CancellationToken cancellation = default, [System.Runtime.CompilerServices.CallerMemberName] string action = null) => Logging.LogAsync(this.CreateLog(LogLevel.Debug, message, exception, null, this.GetSource(source), action), cancellation);
+	public ValueTask DebugAsync(Type source, string message, Exception exception, object data, CancellationToken cancellation = default, [System.Runtime.CompilerServices.CallerMemberName] string action = null) => Logging.LogAsync(this.CreateLog(LogLevel.Debug, message, exception, data, this.GetSource(source), action), cancellation);
+
+	public ValueTask DebugAsync(object source, Exception exception, CancellationToken cancellation = default, [System.Runtime.CompilerServices.CallerMemberName] string action = null) => Logging.LogAsync(this.CreateLog(LogLevel.Debug, exception, null, this.GetSource(source?.GetType()), action), cancellation);
+	public ValueTask DebugAsync(object source, Exception exception, object data, CancellationToken cancellation = default, [System.Runtime.CompilerServices.CallerMemberName] string action = null) => Logging.LogAsync(this.CreateLog(LogLevel.Debug, exception, data, this.GetSource(source?.GetType()), action), cancellation);
+	public ValueTask DebugAsync(object source, string message, CancellationToken cancellation = default, [System.Runtime.CompilerServices.CallerMemberName] string action = null) => Logging.LogAsync(this.CreateLog(LogLevel.Debug, message, null, this.GetSource(source?.GetType()), action), cancellation);
+	public ValueTask DebugAsync(object source, string message, object data, CancellationToken cancellation = default, [System.Runtime.CompilerServices.CallerMemberName] string action = null) => Logging.LogAsync(this.CreateLog(LogLevel.Debug, message, data, this.GetSource(source?.GetType()), action), cancellation);
+	public ValueTask DebugAsync(object source, string message, Exception exception, CancellationToken cancellation = default, [System.Runtime.CompilerServices.CallerMemberName] string action = null) => Logging.LogAsync(this.CreateLog(LogLevel.Debug, message, exception, null, this.GetSource(source?.GetType()), action), cancellation);
+	public ValueTask DebugAsync(object source, string message, Exception exception, object data, CancellationToken cancellation = default, [System.Runtime.CompilerServices.CallerMemberName] string action = null) => Logging.LogAsync(this.CreateLog(LogLevel.Debug, message, exception, data, this.GetSource(source?.GetType()), action), cancellation);
+}
---
+++ Zongsoft.Core/src/Diagnostics/LoggingBase.cs
@@ -0,0 +1,57 @@
+﻿/*
+ *   _____                                ______
+ *  /_   /  ____  ____  ____  _________  / __/ /_
+ *    / /  / __ \/ __ \/ __ \/ ___/ __ \/ /_/ __/
+ *   / /__/ /_/ / / / / /_/ /\_ \/ /_/ / __/ /_
+ *  /____/\____/_/ /_/\__  /____/\____/_/  \__/
+ *                   /____/
+ *
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@qq.com>
+ *
+ * Copyright (C) 2020-2025 Zongsoft Studio <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.Core library.
+ *
+ * The Zongsoft.Core is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3.0 of the License,
+ * or (at your option) any later version.
+ *
+ * The Zongsoft.Core is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Zongsoft.Core library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Zongsoft.Common;
+using Zongsoft.Services;
+
+namespace Zongsoft.Diagnostics;
+
+public abstract partial class LoggingBase<TLog> where TLog : ILog
+{
+	protected virtual string GetSource(Type type)
+	{
+		if(type == null)
+			return null;
+
+		var attribute = ApplicationModuleAttribute.Find(type);
+
+		if(attribute == null || string.IsNullOrEmpty(attribute.Name) || attribute.Name == "_")
+			return TypeAlias.GetAlias(type, true);
+		else
+			return $"{attribute.Name}:{TypeAlias.GetAlias(type, true)}";
+	}
+
+	protected virtual TLog CreateLog(LogLevel level, string message, object data, string source, string action) => this.CreateLog(level, message, null, data, source, action);
+	protected virtual TLog CreateLog(LogLevel level, Exception exception, object data, string source, string action) => this.CreateLog(level, null, exception, data, source, action);
+	protected abstract TLog CreateLog(LogLevel level, string message, Exception exception, object data, string source, string action);
+}
