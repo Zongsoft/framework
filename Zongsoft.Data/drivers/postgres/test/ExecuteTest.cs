c1502697679536613cd43b9e50a3b0ccd6745743
Popeye Zhong <zongsoft@qq.com>
2026-01-31 01:19:13 +0800

    完善数据命令执行方法的单元测试。 👂

--- Zongsoft.Data/drivers/postgres/test/ExecuteTest.cs
+++ Zongsoft.Data/drivers/postgres/test/ExecuteTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -10,7 +11,7 @@ using Zongsoft.Data.Tests.Models;
 namespace Zongsoft.Data.PostgreSql.Tests;
 
 [Collection("Database")]
-public class CommandTest(DatabaseFixture database) : IDisposable
+public class ExecuteTest(DatabaseFixture database) : IDisposable
 {
 	private readonly DatabaseFixture _database = database;
 
@@ -21,23 +22,30 @@ public class CommandTest(DatabaseFixture database) : IDisposable
 			return;
 
 		var accessor = _database.Accessor;
-		var log = Model.Build<Log>(log =>
-		{
-			log.UserId = 1;
-			log.Target = "MyTarget";
-			log.Action = "MyAction";
-			log.TenantId = 1;
-			log.BranchId = 0;
-			log.Timestamp = DateTime.Now;
-			log.Severity = LogSeverity.Info;
-		});
+		var timestamp = DateTime.Today.AddSeconds(Random.Shared.Next(1, 86399));
 
-		var count = await accessor.InsertAsync(log);
+		var script = "INSERT INTO \"Log\" (\"UserId\", \"Target\", \"Action\", \"TenantId\", \"BranchId\", \"Severity\", \"Timestamp\") VALUES (@UserId, @Target, @Action, @TenantId, @BranchId, @Severity, @Timestamp)";
+		var command = Mapping.Commands.Script(PostgreSqlDriver.NAME, DataCommandMutability.Insert, script)
+			.Parameter("@UserId", DataType.UInt32)
+			.Parameter("@TenantId", DataType.UInt32)
+			.Parameter("@BranchId", DataType.UInt32)
+			.Parameter("@Severity", DataType.Byte)
+			.Parameter("@Target", DataType.AnsiString, 100)
+			.Parameter("@Action", DataType.AnsiString, 100)
+			.Parameter("@Timestamp", DataType.DateTime);
+
+		var count = await accessor.ExecuteAsync(command.QualifiedName, [
+			new Parameter("@UserId", 1U),
+			new Parameter("@TenantId", 1U),
+			new Parameter("@BranchId", 0U),
+			new Parameter("@Target", "MyTarget"),
+			new Parameter("@Action", "MyAction"),
+			new Parameter("@Severity", LogSeverity.Info),
+			new Parameter("@Timestamp", timestamp)]);
 		Assert.Equal(1, count);
-		Assert.True(log.LogId > 0);
 
-		var script = "SELECT COUNT(*) FROM \"Log\" WHERE \"Severity\"=@Severity AND \"Timestamp\" BETWEEN @Timestamp1 AND @Timestamp2";
-		var command = Mapping.Commands.Script(PostgreSqlDriver.NAME, script)
+		script = "SELECT COUNT(*) FROM \"Log\" WHERE \"Severity\"=@Severity AND \"Timestamp\" BETWEEN @Timestamp1 AND @Timestamp2";
+		command = Mapping.Commands.Script(PostgreSqlDriver.NAME, script)
 			.Parameter("@Severity", DataType.Byte)
 			.Parameter("@Timestamp1", DataType.DateTime)
 			.Parameter("@Timestamp2", DataType.DateTime);
@@ -45,11 +53,26 @@ public class CommandTest(DatabaseFixture database) : IDisposable
 		var value = await accessor.ExecuteScalarAsync(command.QualifiedName, [
 			new Parameter("@Severity", LogSeverity.Info),
 			new Parameter("@Timestamp1", Range.Timing.Today().Minimum),
-			new Parameter("@Timestamp2", Range.Timing.Today().Maximum),
-		]);
+			new Parameter("@Timestamp2", Range.Timing.Today().Maximum)]);
 
 		Assert.NotNull(value);
 		Assert.Equal(1L, value);
+
+		command = Mapping.Commands.Script(PostgreSqlDriver.NAME, "SELECT * FROM \"Log\"");
+		var logs = accessor.ExecuteAsync<Log>(command.QualifiedName);
+
+		#if NET10_0_OR_GREATER
+		var log = await logs.SingleOrDefaultAsync();
+
+		Assert.NotNull(log);
+		Assert.True(log.LogId > 0);
+		Assert.Equal(1U, log.UserId);
+		Assert.Equal(1U, log.TenantId);
+		Assert.Equal(0U, log.BranchId);
+		Assert.Equal("MyTarget", log.Target);
+		Assert.Equal("MyAction", log.Action);
+		Assert.Equal(timestamp, log.Timestamp);
+		#endif
 	}
 
 	void IDisposable.Dispose()
