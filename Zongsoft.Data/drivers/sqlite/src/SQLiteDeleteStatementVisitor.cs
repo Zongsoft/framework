a5a19dd3d70ee6b8020180ac128a9ebe6dc86f6e
PopeyeZhong <9555843@qq.com>
2026-03-02 15:45:46 +0800

    修复 SQLite 数据驱动的 DELETE 语句的生成错误。 📅

--- Zongsoft.Data/drivers/sqlite/src/SQLiteDeleteStatementVisitor.cs
+++ Zongsoft.Data/drivers/sqlite/src/SQLiteDeleteStatementVisitor.cs
@@ -35,6 +35,21 @@ using Zongsoft.Data.Common.Expressions;
 
 namespace Zongsoft.Data.SQLite;
 
+/*
+ * 由于 SQLite 的删除语句不支持类似 PostgreSQL 中的 USING 子句，因此必须将删除语句中的连接、查询子句转换为 WITH 子句；
+ * 并且 SQLite 的删除语句的 WHERE 条件还不支持直接 WITH 子句表名，因此必须使用 EXISTS 将原 WHERE 过滤条件进行包裹。
+ *
+ * 下面的 PostgreSQL 删除语句：
+ * DELETE FROM DepartmentMember AS M
+ * USING Department AS D
+ * WHERE M.DepartmentId = D.DepartmentId AND 
+ *       D.Name LIKE '研发部';
+ *
+ * 对应的 SQLite 删除语句：
+ * WITH D AS (SELECT * FROM Department WHERE DepartmentId = M.DepartmentId)
+ * DELETE FROM DepartmentMember AS M
+ * WHERE EXISTS (SELECT * FROM D WHERE D.Name LIKE '研发部' LIMIT 1);
+ */
 public class SQLiteDeleteStatementVisitor : DeleteStatementVisitor
 {
 	#region 单例字段
@@ -46,53 +61,72 @@ public class SQLiteDeleteStatementVisitor : DeleteStatementVisitor
 	#endregion
 
 	#region 重写方法
-	protected override void OnVisiting(ExpressionVisitorContext context, DeleteStatement statement) { }
 	protected override void VisitTables(ExpressionVisitorContext context, DeleteStatement statement, IList<TableIdentifier> tables) { }
-	protected override void VisitJoin(ExpressionVisitorContext context, DeleteStatement statement, JoinClause joining, int index)
+	protected override void VisitWith(ExpressionVisitorContext context, DeleteStatement statement, CommonTableExpressionCollection expressions)
 	{
-		if(index == 0)
-			context.Write(" USING ");
+		/*
+		 * 注意：SQLite 的删除语句不支持类似 PostgreSQL 中的 USING 子句，
+		 * 因此在这里需要将删除语句中的连接、查询子句转换为 WITH 子句，以便在 WHERE 子句中使用这些表进行条件过滤。
+		 */
 
-		if(index > 0)
-			context.Write(',');
-
-		switch(joining.Target)
+		if(statement.HasFrom)
 		{
-			case TableIdentifier table:
-				context.Visit(table);
-				break;
-			case SelectStatement subquery:
-				context.Write('(');
+			List<ISource> removables = [];
 
-				//递归生成子查询语句
-				context.Visit(subquery);
+			foreach(var item in statement.From)
+			{
+				if(item is JoinClause joining)
+				{
+					expressions ??= [];
+					expressions.Add(new(joining.Alias, new SelectStatement(joining.Target, joining.Alias)
+					{ Where = joining.Conditions }));
 
-				if(string.IsNullOrEmpty(subquery.Alias))
-					context.WriteLine(")");
-				else
-					context.WriteLine($") AS {subquery.Alias}");
+					removables.Add(joining);
+				}
+				else if(item is SelectStatement select)
+				{
+					expressions ??= [];
+					expressions.Add(new(select.Alias, select));
 
-				break;
+					removables.Add(select);
+				}
+			}
+
+			foreach(var removable in removables)
+				statement.From.Remove(removable);
 		}
 
-		context.WriteLine("WHERE");
-		context.Visit(joining.Conditions);
+		base.VisitWith(context, statement, statement.With = expressions);
 	}
 
 	protected override void VisitWhere(ExpressionVisitorContext context, DeleteStatement statement, IExpression where)
 	{
-		var conditions = ConditionExpression.And(where);
+		/*
+		 * 注意：SQLite 的删除语句的 WHERE 子句不支持直接引用 WITH 子句定义的表名，而只能通过子查询来引用，
+		 * 因此在这里需要将原 WHERE 条件放在一个新的查询子句中并使用 EXISTS 操作包裹。
+		 */
 
-		if(statement.From.Count > 1)
+		if(statement.With != null && statement.With.Count > 0)
 		{
-			for(int i = 1; i < statement.From.Count; i++)
+			context.WriteLine();
+			context.Write("WHERE EXISTS (SELECT * FROM ");
+
+			int index = 0;
+			foreach(var item in statement.With)
 			{
-				if(statement.From[i] is JoinClause joining)
-					conditions.Add(joining.Conditions);
+				if(index++ > 0)
+					context.Write(',');
+
+				context.Write(item.Name);
 			}
+
+			base.VisitWhere(context, statement, where);
+
+			context.WriteLine(" LIMIT 1)");
+			return;
 		}
 
-		base.VisitWhere(context, statement, conditions);
+		base.VisitWhere(context, statement, where);
 	}
 	#endregion
 }
