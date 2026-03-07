6291f33a8474c1d3d27d932a6268f3c4023b7d10
Popeye Zhong <zongsoft@qq.com>
2026-03-07 21:57:04 +0800

    完善 DataType 的类型转换器。 💒

---
+++ Zongsoft.Core/src/Data/DataType.Converter.cs
@@ -0,0 +1,80 @@
+﻿/*
+ *   _____                                ______
+ *  /_   /  ____  ____  ____  _________  / __/ /_
+ *    / /  / __ \/ __ \/ __ \/ ___/ __ \/ /_/ __/
+ *   / /__/ /_/ / / / / /_/ /\_ \/ /_/ / __/ /_
+ *  /____/\____/_/ /_/\__  /____/\____/_/  \__/
+ *                   /____/
+ *
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@qq.com>
+ *
+ * Copyright (C) 2020-2026 Zongsoft Studio <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.Core library.
+ *
+ * The Zongsoft.Core is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3.0 of the License,
+ * or (at your option) any later version.
+ *
+ * The Zongsoft.Core is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Zongsoft.Core library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.ComponentModel;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Zongsoft.Data;
+
+[TypeConverter(typeof(TypeConverter))]
+[JsonConverter(typeof(JsonConverter))]
+partial class DataType
+{
+	private sealed class TypeConverter : System.ComponentModel.TypeConverter
+	{
+		public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType) => sourceType == typeof(string) || base.CanConvertFrom(context, sourceType);
+		public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType) => destinationType == typeof(string) || base.CanConvertTo(context, destinationType);
+
+		public override object ConvertFrom(ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value)
+		{
+			if(value is string text)
+				return DataType.Get(text);
+
+			return base.ConvertFrom(context, culture, value);
+		}
+
+		public override object ConvertTo(ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value, Type destinationType)
+		{
+			if(value is DataType type && destinationType == typeof(string))
+				return type.ToString();
+
+			return base.ConvertTo(context, culture, value, destinationType);
+		}
+	}
+
+	private sealed class JsonConverter : System.Text.Json.Serialization.JsonConverter<DataType>
+	{
+		public override DataType Read(ref Utf8JsonReader reader, Type type, JsonSerializerOptions options) => reader.TokenType switch
+		{
+			JsonTokenType.Null => null,
+			JsonTokenType.String => DataType.Get(reader.GetString()),
+			_ => throw new JsonException($"Unable to convert the JSON value to a {typeof(DataType).FullName} object.")
+		};
+
+		public override void Write(Utf8JsonWriter writer, DataType value, JsonSerializerOptions options)
+		{
+			if(value == null)
+				writer.WriteNullValue();
+			else
+				writer.WriteStringValue(value.ToString());
+		}
+	}
+}
