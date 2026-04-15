dbbcd653de1bc183b9e94a7baabdc3d457fd41bc
PopeyeZhong <9555843@qq.com>
2026-04-15 20:21:25 +0800

    添加框架开发的容器文件。 🐸

---
+++ framework-start.cmd
@@ -0,0 +1,30 @@
+@echo off
+
+REM 获取ESC字符
+for /F "delims=#" %%a in ('"prompt #$E# & echo on & for %%b in (1) do rem"') do set "ESC=%%a"
+
+REM 定义颜色代码
+set "RED=%ESC%[91m"
+set "GREEN=%ESC%[92m"
+set "YELLOW=%ESC%[93m"
+set "BLUE=%ESC%[94m"
+set "MAGENTA=%ESC%[95m"
+set "CYAN=%ESC%[96m"
+
+set "DARK_RED=%ESC%[31m"
+set "DARK_GREEN=%ESC%[32m"
+set "DARK_YELLOW=%ESC%[33m"
+set "DARK_BLUE=%ESC%[34m"
+set "DARK_MAGENTA=%ESC%[35m"
+set "DARK_CYAN=%ESC%[36m"
+
+set "RESET=%ESC%[0m"
+
+SET identifier=
+SET /p identifier=Please enter the platform architecture for the framework(%GREEN%linux-x64/linux-arm64%RESET%): 
+
+if "%identifier%"=="" (
+	SET identifier=linux-x64
+)
+
+podman kube play --replace .\framework.%identifier%.yaml
