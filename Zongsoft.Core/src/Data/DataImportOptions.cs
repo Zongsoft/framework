d63c123d7d25719446cef25684998533372b37c1
PopeyeZhong <9555843@qq.com>
2026-08-11 19:03:30 +0800

    为数据导入选项增加禁用事务的开关。 🍲

--- Zongsoft.Core/src/Data/DataImportOptions.cs
+++ Zongsoft.Core/src/Data/DataImportOptions.cs
@@ -39,6 +39,9 @@ public interface IDataImportOptions : IDataOptions
 {
 	/// <summary>获取或设置一个值，指示是否忽略写操作中的数据库约束（主键、唯一索引、外键约束等）。</summary>
 	bool ConstraintIgnored { get; set; }
+
+	/// <summary>获取或设置一个值，指示是否禁用当前导入操作关联环境事务，默认不禁用。</summary>
+	bool TransactionSuppressed { get; set; }
 }
 
 /// <summary>
@@ -55,12 +58,19 @@ public class DataImportOptions : DataOptionsBase, IDataImportOptions
 	#region 公共属性
 	/// <inheritdoc />
 	public bool ConstraintIgnored { get; set; }
+
+	/// <inheritdoc />
+	public bool TransactionSuppressed { get; set; }
 	#endregion
 
 	#region 静态方法
 	/// <summary>创建一个忽略数据库约束的导入选项构建器。</summary>
 	/// <returns>返回创建的<see cref="Builder"/>构建器对象。</returns>
 	public static Builder IgnoreConstraint() => new(null) { ConstraintIgnored = true };
+
+	/// <summary>创建一个禁用关联环境事务的导入选项构建器。</summary>
+	/// <returns>返回创建的<see cref="Builder"/>构建器对象。</returns>
+	public static Builder SuppressTransaction() => new(null) { TransactionSuppressed = true };
 
 	/// <summary>创建一个带参数的数据操作选项构建器。</summary>
 	/// <param name="name">指定的参数名称。</param>
@@ -89,6 +99,8 @@ public class DataImportOptions : DataOptionsBase, IDataImportOptions
 		#region 公共属性
 		/// <summary>获取或设置一个值，指示是否忽略写操作中的数据库约束（主键、唯一索引、外键约束等）。</summary>
 		public bool ConstraintIgnored { get; set; }
+		/// <summary>获取或设置一个值，指示是否禁用当前导入操作关联环境事务，默认不禁用。</summary>
+		public bool TransactionSuppressed { get; set; }
 		#endregion
 
 		#region 设置方法
@@ -97,12 +109,15 @@ public class DataImportOptions : DataOptionsBase, IDataImportOptions
 		public Builder Parameter(IEnumerable<KeyValuePair<string, object>> parameters) { this.Parameters.SetValue(parameters); return this; }
 		public Builder IgnoreConstraint() { this.ConstraintIgnored = true; return this; }
 		public Builder UnignoreConstraint() { this.ConstraintIgnored = false; return this; }
+		public Builder SuppressTransaction() { this.TransactionSuppressed = true; return this; }
+		public Builder UnsuppressTransaction() { this.TransactionSuppressed = false; return this; }
 		#endregion
 
 		#region 构建方法
 		public override DataImportOptions Build() => new DataImportOptions(this.Parameters)
 		{
 			ConstraintIgnored = this.ConstraintIgnored,
+			TransactionSuppressed = this.TransactionSuppressed,
 		};
 		#endregion
 
