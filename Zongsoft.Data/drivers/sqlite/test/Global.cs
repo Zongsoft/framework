672c112f10be75d20102a74775e3136168ae853c
PopeyeZhong <9555843@qq.com>
2026-08-19 11:44:42 +0800

    统一单元测试是否启用的判断。 🐴

--- Zongsoft.Data/drivers/sqlite/test/Global.cs
+++ Zongsoft.Data/drivers/sqlite/test/Global.cs
@@ -5,6 +5,17 @@ namespace Zongsoft.Data.SQLite.Tests;
 public static class Global
 {
 	#region 静态属性
-	public static bool IsTestingEnabled => true;
+	public static bool IsTestingEnabled => System.Diagnostics.Debugger.IsAttached || IsEnabled("ZONGSOFT_DATA_TESTS");
+	#endregion
+
+	#region 私有方法
+	private static bool IsEnabled(string name)
+	{
+		var value = Environment.GetEnvironmentVariable(name);
+
+		return string.Equals(value, "1", StringComparison.OrdinalIgnoreCase) ||
+		       string.Equals(value, "yes", StringComparison.OrdinalIgnoreCase) ||
+		       string.Equals(value, "true", StringComparison.OrdinalIgnoreCase);
+	}
 	#endregion
 }
